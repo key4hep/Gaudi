fda2e6925585178e58b846bf422f83e409eb6ade
hmd <hmd@53ecefc3-a64d-0410-9bdf-b8581fa3287e>
2009-11-03 18:53:54 +0000

    added a switch on the StripPath script. if GAUDI_NO_STRIP_PATH is set in the environment, no path stripping will occur at setup time.
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@6028 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- GaudiPolicy/scripts/StripPath.bat
+++ GaudiPolicy/scripts/StripPath.bat
@@ -1,5 +1,7 @@
 @echo off
 
+if not "%GAUDI_NO_STRIP_PATH%" == "" goto :eof
+
 set tmpfile="%TEMP%\StripPath_tmpsetup.bat"
 
 python %GAUDIPOLICYROOT%\scripts\PathStripper.py --shell=bat --output=%tmpfile% -e PATH -e PYTHONPATH -e JOBOPTSEARCHPATH -e HPATH
