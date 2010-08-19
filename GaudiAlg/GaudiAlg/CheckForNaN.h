d7371e3585d3295bb84ff98aed8708605f1bba8d
marcocle <marcocle@53ecefc3-a64d-0410-9bdf-b8581fa3287e>
2010-08-19 17:58:04 +0000

    Marco Clemencic  - Fix for ICC in GaudiAlg/CheckForNaN.h.
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@6270 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- GaudiAlg/GaudiAlg/CheckForNaN.h
+++ GaudiAlg/GaudiAlg/CheckForNaN.h
@@ -1,27 +1,34 @@
 // $Id: CheckForNaN.h,v 1.5 2007/10/19 10:18:23 marcocle Exp $
 // ============================================================================
-#ifndef GAUDIALG_CHECKFORNAN_H 
+#ifndef GAUDIALG_CHECKFORNAN_H
 #define GAUDIALG_CHECKFORNAN_H 1
 // ============================================================================
 // Include files
 // ============================================================================
-#ifndef _WIN32
-#include <cmath>
-namespace 
-{
-  inline int lfin ( double x ) { return  std::isfinite ( x ) ; }
-  inline int lnan ( double x ) { return  std::isnan  ( x ) ; }
-}
-#else
+#if defined(_WIN32)
 #include <float.h>
-namespace 
+namespace
 {
   inline int lfin ( double x ) { return  _finite ( x ) ; }
   inline int lnan ( double x ) { return  _isnan  ( x ) ; }
 }
+#elif defined(__ICC)
+#include <mathimf.h>
+namespace
+{
+  inline int lfin ( double x ) { return  isfinite ( x ) ; }
+  inline int lnan ( double x ) { return  isnan  ( x ) ; }
+}
+#else // GCC
+#include <cmath>
+namespace
+{
+  inline int lfin ( double x ) { return  std::isfinite ( x ) ; }
+  inline int lnan ( double x ) { return  std::isnan  ( x ) ; }
+}
 #endif
 // ============================================================================
-// The END 
+// The END
 // ============================================================================
 #endif // GAUDIALG_CHECKFORNAN_H
 // ============================================================================
