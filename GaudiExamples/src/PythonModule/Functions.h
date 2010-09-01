5c84219e4f85b982e09b29f550a071239dd22445
marcocle <marcocle@53ecefc3-a64d-0410-9bdf-b8581fa3287e>
2010-09-01 16:15:04 +0000

    Marco Clemencic  - Added an example of a Python binary module in src/PythonModule (plus test).
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@6289 53ecefc3-a64d-0410-9bdf-b8581fa3287e

---
+++ GaudiExamples/src/PythonModule/Functions.h
@@ -0,0 +1,14 @@
+/*
+ * Functions.h
+ *
+ *  Created on: Sep 1, 2010
+ *      Author: marcocle
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+/// Simple C++ function
+char const* greet();
+
+#endif /* FUNCTIONS_H_ */
