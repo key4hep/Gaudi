164f240c7326da21222807c5abb5193577c65de3
hmd <hmd@53ecefc3-a64d-0410-9bdf-b8581fa3287e>
2011-01-12 14:45:56 +0000

    merged with the trunk
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/branches/GAUDI/GAUDI_nativedep@6382 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- GaudiPython/GaudiPython/PyROOTPickle.h
+++ GaudiPython/GaudiPython/PyROOTPickle.h
@@ -1,55 +1,10 @@
-// This file's extension implies that it's C, but it's really -*- C++ -*-.
-
-/**
- * @file GaudiPython/PyROOTPickle.h
- * @author Wim Lavrijsen
- * @date Apr 2008
- * @brief Port pickling functionality while awaiting newer release.
- *
- * PyROOT does not implement direct pickling of ObjectProxy's until release
- * 5.20 of ROOT; this code brings it forward for use in ATLAS.
- *
- * The code here will make the following changes to PyROOT's behavior:
- *
- *  - Add a method "_ObjectProxy__expand__" to libPyROOT that is stored by
- *     name by the pickler used by the unpickler. The expand function takes
- *     a class name and a python string, which are turned into an object
- *     using TBufferFile.
- *
- *  - Add a "__reduce__" method to ObjectProxy, which turns ObjectProxy's
- *     into python strings through serialization done by TBufferFile. The
- *     python string is subsequently pickled.
- */
-
-
-#ifndef GAUDIPYTHON_PYROOTPICKLE_H
-#define GAUDIPYTHON_PYROOTPICKLE_H
-
-#include "GaudiKernel/Kernel.h"
-
-#ifdef _POSIX_C_SOURCE
-# undef _POSIX_C_SOURCE
-#endif
-#include "Python.h"
-
-
+// FIXME: remove obsolete header
+#include <GaudiMP/PyROOTPickle.h>
 namespace GaudiPython {
-
-
-class GAUDI_API PyROOTPickle
-{
-public:
-  /**
-   * @brief Install the pickling of ObjectProxy's functionality.
-   * @param libpyroot_pymodule The libPyROOT python module
-   * @param objectproxy_pytype The ObjectProxy python type
-   */
-  static void Initialize (PyObject* libpyroot_pymodule,
-                          PyObject* objectproxy_pytype);
-};
-
-
-} // namespace GaudiPython
-
-
-#endif // not GAUDIPYTHON_PYROOTPICKLE_H
+  using namespace GaudiMP;
+}
+#if WIN32
+#pragma message ( "Warning: obsolete header file, use GaudiMP/PyROOTPickle.h" )
+#else
+#warning obsolete header file, use GaudiMP/PyROOTPickle.h
+#endif
