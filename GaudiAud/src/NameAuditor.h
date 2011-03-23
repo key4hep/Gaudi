9a37c6a157c8f9ce19066db0179d5e9990af5e10
Marco Clemencic <marco.clemencic@cern.ch>
2011-03-23 13:52:44 +0100

    migrated NameAuditor to use CommonAuditor

--- GaudiAud/src/NameAuditor.h
+++ GaudiAud/src/NameAuditor.h
@@ -1,58 +1,22 @@
 #ifndef ATLASAUDITOR_NAMEAUDITOR_H
 #define ATLASAUDITOR_NAMEAUDITOR_H
 
-#include "GaudiKernel/Auditor.h"
-#include "GaudiKernel/Property.h"
+#include "CommonAuditor.h"
 
 /**
  * @brief Prints the name of each algorithm before entering the algorithm and after leaving it
  * @author M. Shapiro, LBNL
- *
+ * @author Marco Clemencic
  */
-class NameAuditor:public  Auditor {
-private:
-  enum Action { BEFORE, AFTER };
-
+class NameAuditor:public CommonAuditor {
 public:
   NameAuditor(const std::string& name, ISvcLocator* pSvcLocator);
-  virtual ~NameAuditor();
-
-  /// \name "before" Auditor hooks
-  //@{
-  virtual void before(StandardEventType evt, const std::string& caller);
-
-  virtual void before(StandardEventType evt, INamedInterface* caller) {
-    if (caller) before(evt, caller->name());
-  }
-  virtual void before(CustomEventTypeRef evt, const std::string& caller) {
-    i_doAudit(evt, caller, BEFORE);
-  }
-  virtual void before(CustomEventTypeRef evt, INamedInterface* caller) {
-    if (caller) before(evt, caller->name());
-  }
-  //@}
-
-  /// \name "after" Auditor hooks
-  //@{
-  virtual void after(StandardEventType evt, const std::string& caller, const StatusCode& sc);
-
-  virtual void after(StandardEventType evt, INamedInterface* caller, const StatusCode& sc) {
-    if (caller) after(evt, caller->name(), sc);
-  }
-  virtual void after(CustomEventTypeRef evt, const std::string& caller, const StatusCode&) {
-    i_doAudit(evt, caller, AFTER);
-  }
-  virtual void after(CustomEventTypeRef evt, INamedInterface* caller, const StatusCode& sc) {
-    if (caller) after(evt, caller->name(), sc);
-  }
-  //@}
 
 private:
-  /// Implementation of the auditor
-  void i_doAudit(const std::string& evt, const std::string& caller, Action action);
-
-private:
-  StringArrayProperty m_types;   ///< Filter for custom event types
+  /// Print a message on "before".
+  virtual void i_before(CustomEventTypeRef evt, const std::string& caller);
+  /// Print a message on "after".
+  virtual void i_after(CustomEventTypeRef evt, const std::string& caller, const StatusCode& sc);
 
 };
 
