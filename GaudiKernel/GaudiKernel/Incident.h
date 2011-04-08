295b55693c756e39f6b33b62bbb30034d5adb252
Marco Clemencic <marco.clemencic@cern.ch>
2011-04-08 19:02:18 +0200

    Introduced two new incidents: BeginProcessing and EndProcessing. They are needed to reintroduce the signals around the processing loop as it was the case for BeginEvent and EndEvent before GaudiSvc v17r0.

--- GaudiKernel/GaudiKernel/Incident.h
+++ GaudiKernel/GaudiKernel/Incident.h
@@ -64,7 +64,7 @@ namespace IncidentType
   const std::string EndStream  = "EndStream";  ///< Processing of the stream has finished
 
   const std::string AbortEvent = "AbortEvent"; ///< Stop processing the current event and pass to te next one
-  
+
   //Added by R. Lambert 2009-09-03, for summary services
   //define a preprocessor macro to allow backward-compatibility
 #define GAUDI_FILE_INCIDENTS
@@ -73,11 +73,15 @@ namespace IncidentType
   const std::string FailOutputFile = "FailOutputFile"; ///< could not create or write to this file
   const std::string WroteToOutputFile = "WroteToOutputFile"; ///< the output file was written to in this event
   const std::string EndOutputFile   = "EndOutputFile";   ///< an output file has been finished
-  
+
   const std::string BeginInputFile = "BeginInputFile"; ///< a new input file has been started
   const std::string FailInputFile = "FailInputFile"; ///< could not open or read from this file
   const std::string EndInputFile   = "EndInputFile";   ///< an input file has been finished
 
+  /// Incident raised just before entering loop over the algorithms.
+  const std::string BeginProcessing = "BeginProcessing";
+  /// Incident raised just after the loop over the algorithms (note: before the execution of OutputStreams).
+  const std::string EndProcessing = "EndProcessing";
 }
 
 #endif //GAUDI_INCIDENT_H
