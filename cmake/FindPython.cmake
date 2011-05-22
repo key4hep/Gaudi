c087ee5b54a90cd321debeef1c8863b08e46224f
Marco Clemencic <marco.clemencic@cern.ch>
2011-05-22 20:45:29 +0200

    Replaced 'dl' with ${CMAKE_DL_LIBS}

--- cmake/FindPython.cmake
+++
@@ -1,8 +0,0 @@
-# Simplified wrapper for the standard Python cmake configuration files
-
-find_package(PythonInterp)
-find_package(PythonLibs)
-
-set(PYTHON_FOUND 1)
-
-set(Python_INCLUDE_DIRS ${PYTHON_INCLUDE_DIR})
