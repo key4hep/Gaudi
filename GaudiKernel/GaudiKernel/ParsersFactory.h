b37c073f8da8c85c7506b4c32a320d09ede2f8fc
Marco Clemencic <marco.clemencic@cern.ch>
2011-07-25 17:56:14 +0200

    Patch #4891: fixed indentation

--- GaudiKernel/GaudiKernel/ParsersFactory.h
+++ GaudiKernel/GaudiKernel/ParsersFactory.h
@@ -21,33 +21,35 @@
 #include "GaudiKernel/StatusCode.h"
 #include "GaudiKernel/Grammars.h"
 // ============================================================================
-namespace Gaudi { namespace Parsers {
-// ============================================================================
+namespace Gaudi {
+  namespace Parsers {
+    // ========================================================================
     typedef std::string::const_iterator IteratorT;
     //typedef boost::spirit::ascii::space_type Skipper;
     typedef SkipperGrammar<IteratorT> Skipper;
-// ============================================================================
-template<typename ResultT> inline StatusCode
-parse_(ResultT& result, const std::string& input){
-	Skipper skipper;
-	typename Grammar_<IteratorT, ResultT, Skipper>::Grammar g;
-	IteratorT iter = input.begin(), end = input.end();
-	return qi::phrase_parse( iter, end, g, skipper , result) && (iter==end);
-}
-//==============================================================================
-template<> inline StatusCode
-parse_(std::string& result, const std::string& input){
-	Skipper skipper;
-    Grammar_<IteratorT, std::string, Skipper>::Grammar g;
-    IteratorT iter = input.begin(), end = input.end();
-    if (!(qi::phrase_parse( iter, end, g, skipper, result) && (iter==end))){
-        result = input;
+    // ========================================================================
+    template<typename ResultT> inline StatusCode
+    parse_(ResultT& result, const std::string& input){
+      Skipper skipper;
+      typename Grammar_<IteratorT, ResultT, Skipper>::Grammar g;
+      IteratorT iter = input.begin(), end = input.end();
+      return qi::phrase_parse( iter, end, g, skipper , result) && (iter==end);
     }
-    //@attention always
-    return true;
-}
-//==============================================================================
-}/* Parsers */ } /*  Gaudi */
-//==============================================================================
+    //=========================================================================
+    template<> inline StatusCode
+    parse_(std::string& result, const std::string& input){
+      Skipper skipper;
+      Grammar_<IteratorT, std::string, Skipper>::Grammar g;
+      IteratorT iter = input.begin(), end = input.end();
+      if (!(qi::phrase_parse( iter, end, g, skipper, result) && (iter==end))){
+        result = input;
+      }
+      //@attention always
+      return true;
+    }
+    //=========================================================================
+  }/* Parsers */
+} /*  Gaudi */
+//=============================================================================
 #endif // GAUDIPROPERTYPARSERS_PARSERSGENERATOR_H
 
