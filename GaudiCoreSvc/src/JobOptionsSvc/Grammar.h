122e50cb81415bb611e64850e51e93a3d82cef90
Alexander Mazurov <alexander.mazurov@cern.ch>
2011-12-13 14:52:21 +0000

    Patch #5166: Allow "::" in property and component names.
    https://savannah.cern.ch/patch/?5166
    
    From: Alexander Mazurov <alexander.mazurov@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@6752 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- GaudiCoreSvc/src/JobOptionsSvc/Grammar.h
+++ GaudiCoreSvc/src/JobOptionsSvc/Grammar.h
@@ -80,11 +80,14 @@ struct IdentifierGrammar: qi::grammar<Iterator, Node(), Skipper> {
 //-----------------------------------------------------------------------------
   IdentifierGrammar() : IdentifierGrammar::base_type(ident) {
       ident = rep::qi::iter_pos[op(qi::_val, qi::_1)] >> str[op(qi::_val, qi::_1)][op(qi::_val, Node::kIdentifier)];
-      str = qi::lexeme[qi::alpha >> *(qi::alnum | '_')];
+      str = -qi::lit("::")[qi::_val += "::"] >> inner[qi::_val += qi::_1] 
+        >> *(qi::lit("::") >> inner[qi::_val += ("::"+qi::_1)]);
+      inner = qi::alpha >> *(qi::alnum | '_');
   }
 // ----------------------------------------------------------------------------
   qi::rule<Iterator, Node(), Skipper> ident;
   qi::rule<Iterator, std::string(), Skipper> str;
+  qi::rule<Iterator, std::string()> inner;
   ph::function<NodeOperations> op;
 };
 // ============================================================================
@@ -185,9 +188,8 @@ struct FileGrammar: qi::grammar<Iterator, Node(), Skipper> {
                       >> value[op(qi::_val, qi::_1)]
                       >> ';';
       property = rep::qi::iter_pos[op(qi::_val, qi::_1)]
-                         >> (gidentifier[op(qi::_val, qi::_1)]
-                         >> -(gidentifier[op(qi::_val, qi::_1)]) % '.')
-                    [op(qi::_val, Node::kProperty)];
+                         >> (gidentifier[op(qi::_val, qi::_1)] % '.')
+                        [op(qi::_val, Node::kProperty)];
       property_ref %= -qi::lit('@') >>
           property[op(qi::_val, Node::kPropertyRef)];
       oper = rep::qi::iter_pos[op(qi::_val, qi::_1)]
