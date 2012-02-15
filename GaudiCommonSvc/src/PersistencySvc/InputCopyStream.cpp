d0417d90898356ba5ba33e345edbb5fe9399afe9
Marco Clemencic <marco.clemencic@cern.ch>
2012-02-15 14:42:03 +0000

    Add a "TESVetoList" property to InputCopyStream
    https://savannah.cern.ch/task/?25591
    
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@6804 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- GaudiCommonSvc/src/PersistencySvc/InputCopyStream.cpp
+++ GaudiCommonSvc/src/PersistencySvc/InputCopyStream.cpp
@@ -17,46 +17,81 @@
 DECLARE_ALGORITHM_FACTORY(InputCopyStream)
 
 // Standard Constructor
-InputCopyStream::InputCopyStream(const std::string& name, ISvcLocator* pSvcLocator)
- : OutputStream(name, pSvcLocator), m_leavesTool(0)
+  InputCopyStream::InputCopyStream(const std::string& name, ISvcLocator* pSvcLocator)
+    : OutputStream  ( name, pSvcLocator),
+      m_leavesTool  ( NULL             ),
+      m_tesVetoList (                  )
 {
   m_doPreLoad      = false;
   m_doPreLoadOpt   = false;
+  declareProperty( "TESVetoList", m_tesVetoList );
 }
 
 // Standard Destructor
-InputCopyStream::~InputCopyStream()   {
-}
+InputCopyStream::~InputCopyStream() { }
 
 // Initialize the instance.
-StatusCode InputCopyStream::initialize() {
+StatusCode InputCopyStream::initialize()
+{
   StatusCode sc = OutputStream::initialize();
-  if (sc.isFailure()) return sc;
+  if ( sc.isFailure() ) return sc;
 
   sc = toolSvc()->retrieveTool("DataSvcFileEntriesTool", "InputCopyStreamTool",
                                m_leavesTool);
-  if (sc.isFailure()) return sc;
 
-  return StatusCode::SUCCESS;
+  return sc;
 }
 
 // Finalize the instance.
-StatusCode InputCopyStream::finalize() {
+StatusCode InputCopyStream::finalize()
+{
   toolSvc()->releaseTool(m_leavesTool).ignore();
-  m_leavesTool = 0;
-
+  m_leavesTool = NULL;
   return OutputStream::finalize();
 }
 
 /// Collect all objects to be written to the output stream
-StatusCode InputCopyStream::collectObjects()   {
-  MsgStream log(msgSvc(), name());
-
+StatusCode InputCopyStream::collectObjects()
+{
   // Get the objects in the same file as the root node
-  try {
+  try
+  {
+    // Get all the leaves on the input stream
     const IDataStoreLeaves::LeavesList & leaves = m_leavesTool->leaves();
-    m_objects.assign(leaves.begin(), leaves.end());
-  } catch (GaudiException &e) {
+
+    // Do we need to veto anything ?
+    if ( UNLIKELY( !m_tesVetoList.empty() ) )
+    {
+      // Veto out locations
+      IDataStoreLeaves::LeavesList filteredLeaves;
+      filteredLeaves.reserve( leaves.size() );
+      for ( IDataStoreLeaves::LeavesList::const_iterator iL = leaves.begin();
+            iL != leaves.end(); ++iL )
+      {
+        if ( *iL && (*iL)->registry() )
+        {
+          if ( std::find( m_tesVetoList.begin(),
+                          m_tesVetoList.end(),
+                          (*iL)->registry()->identifier() ) == m_tesVetoList.end() )
+          {
+            filteredLeaves.push_back( (*iL) );
+          }
+        }
+      }
+
+      // saved the veto'ed list
+      m_objects.assign( filteredLeaves.begin(), filteredLeaves.end() );
+
+    }
+    else
+    {
+      // no veto'ing, so save everything
+      m_objects.assign( leaves.begin(), leaves.end() );
+    }
+  }
+  catch ( const GaudiException & e )
+  {
+    MsgStream log(msgSvc(), name());
     log << MSG::ERROR << e.message() << endmsg;
     return StatusCode::FAILURE;
   }
