aec1a9fe86538cb09f9fe3d565f79942a087f4b2
Marco Clemencic <marco.clemencic@cern.ch>
2012-02-16 16:34:22 +0100

    improved FindQMtest.cmake to find the 'qm' module on the system path

--- cmake/modules/FindQMtest.cmake
+++ cmake/modules/FindQMtest.cmake
@@ -1,7 +1,8 @@
 # - Find QMTest.
 #
 # This module will define the following variables:
-#  QMTEST_EXECUTABLE - the qmtest main script
+#  QMTEST_EXECUTABLE  - the qmtest main script
+#  QMTEST_PYTHON_PATH - directory containing the Python module 'qm'
 
 find_program(QMTEST_EXECUTABLE
   NAMES qmtest
@@ -18,6 +19,17 @@ find_path(QMTEST_PYTHON_PATH
    ${QMTEST_PREFIX_PATH}/lib/python${Python_config_version_twodigit}/site-packages
    ${QMTEST_PREFIX_PATH}/Lib/site-packages
 )
+
+if (NOT QMTEST_PYTHON_PATH)
+  # let's try to find the qm module in the standard environment
+  find_package(PythonInterp)
+  execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import os, qm; print os.path.dirname(os.path.dirname(qm.__file__))"
+                  OUTPUT_VARIABLE QMTEST_PYTHON_PATH OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+  if (QMTEST_PYTHON_PATH)
+    # we found it, so no need to extend the PYTHONPATH
+    set(QMTEST_PYTHON_PATH ${QMTEST_PYTHON_PATH} CACHE "" "" FORCE)
+  endif()
+endif()
 
 # handle the QUIETLY and REQUIRED arguments and set COOL_FOUND to TRUE if
 # all listed variables are TRUE
