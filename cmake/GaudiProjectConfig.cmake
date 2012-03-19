7dd22df2f6387180873c7e9e9244f194c373a2bd
Marco Clemencic <marco.clemencic@cern.ch>
2012-03-19 18:36:41 +0100

    removed cmake_policy calls (not needed with minimum version 2.8.5)

--- cmake/GaudiProjectConfig.cmake
+++ cmake/GaudiProjectConfig.cmake
@@ -6,9 +6,6 @@
 # Authors: Pere Mato, Marco Clemencic
 
 cmake_minimum_required(VERSION 2.8.5)
-cmake_policy(SET CMP0003 NEW) # See "cmake --help-policy CMP0003" for more details
-cmake_policy(SET CMP0011 NEW) # See "cmake --help-policy CMP0011" for more details
-cmake_policy(SET CMP0009 NEW) # See "cmake --help-policy CMP0009" for more details
 
 # Add the directory containing this file to the modules search path
 set(CMAKE_MODULE_PATH ${GaudiProject_DIR} ${CMAKE_MODULE_PATH})
