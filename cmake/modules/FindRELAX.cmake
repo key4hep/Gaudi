cd29a3b5d2c43e9f72647828ac96fe27f130599c
Marco Clemencic <marco.clemencic@cern.ch>
2012-08-22 11:02:32 +0200

    Improvements to FindRELAX.cmake.
    Modified to set the variable RELAX_ROOTMAPS to contain the rootmap files
    corresponding to the components required.

--- cmake/modules/FindRELAX.cmake
+++ cmake/modules/FindRELAX.cmake
@@ -4,7 +4,9 @@
 #  RELAX_FOUND
 #  RELAX_<component>_LIBRARY
 #  RELAX_<component>_ROOTMAP
+#  RELAX_ROOTMAPS (not cached) list of rootmap files required
 #  RELAX_LIBRARY_DIRS (not cached)
+#  RELAX_FOUND_COMPONENTS (not cached)
 
 # Enforce a minimal list if none is explicitly requested
 if(NOT RELAX_FIND_COMPONENTS)
@@ -13,16 +15,21 @@ endif()
 
 set(RELAX_ROOTMAP_SUFFIX .pamtoor)
 
+set(RELAX_ROOTMAPS)
+
 foreach(component ${RELAX_FIND_COMPONENTS})
+
   find_library(RELAX_${component}_LIBRARY NAMES ${component}Rflx
                HINTS $ENV{RELAX_ROOT_DIR}/lib ${RELAX_ROOT_DIR}/lib )
   mark_as_advanced(RELAX_${component}_LIBRARY)
+
   if(RELAX_${component}_LIBRARY)
     list(APPEND RELAX_FOUND_COMPONENTS ${component})
     get_filename_component(libdir ${RELAX_${component}_LIBRARY} PATH)
     list(APPEND RELAX_LIBRARY_DIRS ${libdir})
     # deduce the name of the rootmap file for the library
     string(REPLACE "${CMAKE_SHARED_MODULE_SUFFIX}" "${RELAX_ROOTMAP_SUFFIX}" RELAX_${component}_ROOTMAP "${RELAX_${component}_LIBRARY}")
+    list(APPEND RELAX_ROOTMAPS ${RELAX_${component}_ROOTMAP})
   endif()
 endforeach()
 list(REMOVE_DUPLICATES RELAX_LIBRARY_DIRS)
@@ -30,6 +37,6 @@ list(REMOVE_DUPLICATES RELAX_LIBRARY_DIRS)
 # handle the QUIETLY and REQUIRED arguments and set RELAX_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(RELAX DEFAULT_MSG RELAX_LIBRARY_DIRS)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(RELAX DEFAULT_MSG RELAX_LIBRARY_DIRS RELAX_ROOTMAPS)
 
 mark_as_advanced(RELAX_FOUND)
