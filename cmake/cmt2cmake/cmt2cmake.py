f79a58b055b2f2f92919d00b40e4eaa9283e45fe
Marco Clemencic <marco.clemencic@cern.ch>
2012-10-19 12:00:44 +0200

    improved the default toolchain wrapper
    Store the actual toolchain file in the cache variable
    CMAKE_TOOLCHAIN_FILE, instead of the wrapper.

--- cmake/cmt2cmake/cmt2cmake.py
+++ cmake/cmt2cmake/cmt2cmake.py
@@ -673,17 +673,25 @@ class Package(object):
 toolchain_template = '''# Special wrapper to load the declared version of the heptools toolchain.
 set(heptools_version {0})
 
-find_file(toolchain_file
+# Remove the reference to this file from the cache.
+unset(CMAKE_TOOLCHAIN_FILE CACHE)
+
+# Find the actual toolchain file.
+find_file(CMAKE_TOOLCHAIN_FILE
           NAMES heptools-${{heptools_version}}.cmake
           HINTS ENV CMTPROJECTPATH
           PATHS ${{CMAKE_CURRENT_LIST_DIR}}/cmake/toolchain
           PATH_SUFFIXES toolchain)
 
-if(NOT toolchain_file)
+if(NOT CMAKE_TOOLCHAIN_FILE)
   message(FATAL_ERROR "Cannot find heptools-${{heptools_version}}.cmake.")
 endif()
 
-include(${{toolchain_file}})
+# Reset the cache variable to have proper documentation.
+set(CMAKE_TOOLCHAIN_FILE ${{CMAKE_TOOLCHAIN_FILE}}
+    CACHE FILEPATH "The CMake toolchain file" FORCE)
+
+include(${{CMAKE_TOOLCHAIN_FILE}})
 '''
 
 class Project(object):
