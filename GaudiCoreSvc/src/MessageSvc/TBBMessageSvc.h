9c3a869cd27f60fb6a7fe1ddfc8aaf9fd2cb4dd0
Marco Clemencic <marco.clemencic@cern.ch>
2012-10-21 19:28:40 +0200

    Modified TBBMessageSvc to use the new Gaudi::SerialTaskQueue.
    https://sft.its.cern.ch/jira/browse/CFHEP-2

--- GaudiCoreSvc/src/MessageSvc/TBBMessageSvc.h
+++ GaudiCoreSvc/src/MessageSvc/TBBMessageSvc.h
@@ -3,15 +3,10 @@
 // Include files
 #include "MessageSvc.h"
 
-// Include TBB headers
-#include <tbb/concurrent_queue.h>
-#include <tbb/task.h>
-// just needed for the thread id
-#include <tbb/compat/thread>
+#include "GaudiKernel/SerialTaskQueue.h"
 
-#if defined(__GXX_EXPERIMENTAL_CXX0X__) || __cplusplus >= 201103L
+// just needed for the thread id
 #include <thread>
-#endif
 
 /** @class TBMessageSvc TBMessageSvc.h MessageSvc/TBMessageSvc.h
   *
@@ -41,52 +36,24 @@ protected:
 private:
 
   /// Simple class to wrap the data needed to report a message.
-  struct MessageType {
-    MessageType(Message _msg, int _level):
-      msg(_msg), level(_level), sender(std::this_thread::get_id()) {}
-    Message msg;
-    int level;
-    std::thread::id sender;
-  };
-  typedef tbb::concurrent_queue<MessageType*> QueueType;
-
-  /// Helper class to report messages.
-  class ReportTask: public tbb::task {
+  class MessageTask: public Gaudi::SerialTaskQueue::WorkItem {
   public:
-    /// Create an instance of the task, bound to the TBBMessageSvc, which is
-    /// used to pop messages from the queue and actually print them.
-    ReportTask(TBBMessageSvc& svc): m_svc(svc) {}
-    tbb::task* execute() {
-      MessageType* msg = NULL;
-      if (m_svc.m_messageQueue.try_pop(msg)) {
-        // the queue, was not empty -> process the message
-        m_svc.MessageSvc::reportMessage(msg->msg, msg->level);
-        delete msg; msg = NULL;
-        // prepare continuation
-        tbb::task* t = new(this->allocate_continuation()) ReportTask(m_svc);
-        t->increment_ref_count();
-        return t;
-      }
-      // if the queue was empty, we stop the continuation
-      return NULL;
+    MessageTask(TBBMessageSvc& svc, Message msg, int level):
+      m_svc(svc),
+      m_msg(msg), m_level(level),
+      m_sender(std::this_thread::get_id()) {}
+
+    virtual void run() {
+      m_svc.MessageSvc::reportMessage(m_msg, m_level);
     }
   private:
     TBBMessageSvc& m_svc;
+    Message m_msg;
+    int m_level;
+    std::thread::id m_sender;
   };
 
-  friend class ReportTask;
-
-  inline void i_processMessages() {
-    if (m_barrier->ref_count() == 1) { // no running children
-      // start a new child
-      m_barrier->increment_ref_count();
-      tbb::task* t = new(m_barrier->allocate_child()) ReportTask(*this);
-      tbb::task::spawn(*t);
-    }
-  }
-
-  tbb::task* m_barrier;
-  QueueType m_messageQueue;
+  Gaudi::SerialTaskQueue m_messageQueue;
 };
 
 #endif // MESSAGESVC_TBBMESSAGESVC_H
