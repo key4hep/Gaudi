9e4e784d8e4623dc124cd2363a85ef325bfe43a6
Benedikt Hegner <benedikt.hegner@cern.ch>
2012-11-21 15:22:22 +0100

    add more graph analysis capability; new script to extract json from LHCb log

--- GaudiHive/options/graphanalysis.py
+++ GaudiHive/options/graphanalysis.py
@@ -1,11 +1,12 @@
-from pygraph.classes.graph import graph
+from pygraph.classes.digraph import digraph
 from pygraph.algorithms.cycles import find_cycle
 from pygraph.algorithms.accessibility import connected_components
+from pygraph.algorithms.critical import critical_path
 
 def read_graph_from_json(filename):
     data = open(filename).read()
     workflow = eval(data)
-    gr = graph()
+    gr = digraph()
     # create all nodes
     for algo in workflow["algorithms"]:
       name = algo["name"]+"_algo" #to differentiate algos from equally named products
@@ -15,13 +16,14 @@ def read_graph_from_json(filename):
         if not gr.has_node(input):         
           gr.add_nodes([input])
         if not gr.has_edge((input,name)):
-          gr.add_edge((input,name))
+          gr.add_edge((input,name),wt=0)
       for output in algo["outputs"]:
         if output == "":output ="dummy" 
         if not gr.has_node(output):  
           gr.add_nodes([output])
         if not gr.has_edge((name,output)):
-          gr.add_edge((name,output))
+          #the output edge gets the weigth corresponding to the runtime
+          gr.add_edge((name,output),wt=algo["runtimes_wall"][0]*100)
     return gr
 
 def analyze_and_fix_cycles(gr):
@@ -33,10 +35,10 @@ def analyze_and_fix_cycles(gr):
         has_loop = False
         continue
       n_cycles += 1
-      #print cycle
+      print cycle
       print "Removed loop by deleting edge (%s,%s)" %(cycle[-1],cycle[0])
       gr.del_edge((cycle[-1],cycle[0]))
-    print "\nREMOVED %i CYCLES\n" %(n_cycles)
+    print "\nIN TOTAL %i CYCLES\n" %(n_cycles)
 
 def analyze_connected_componets(gr):
     cc = connected_components(gr)
@@ -46,18 +48,40 @@ def analyze_connected_componets(gr):
     for k, v in cc.iteritems():
       cc_size[v] = cc_size[v] + 1
     print "Connected components have the following size:"
-    for k,v in cc_size.iteritems():
-      print "%i : %i" %(k,v)
+#    for k,v in cc_size.iteritems():
+#      print "%i : %i" %(k,v)
     print "NUMBER OF CONNECTED COMPONENTS: %i" %(len(cc_size.keys()))
 
 
+def analyze_critical_path(gr):
+    total_time = 0
+    critical_time = 0
+    cp = critical_path(gr)
+    # calculate total time
+    for edge in gr.edges():
+     total_time += gr.edge_weight(edge)
+    
+    # calculate time of critical path
+    edges = [tuple(cp[i:i+2]) for i in range(0, len(cp))]
+    for edge in edges:
+      critical_time += gr.edge_weight(edge)
+
+    print "Total time   : %s" %total_time
+    print "Critical path: %s" %critical_time
+    print "POSSIBLE SPEEDUP: %s" %(total_time/critical_time)
+ 
+
 ##########################  
 if __name__ == "__main__":
-  filename = "Athena.json"
+  filename = "lhcb.json"
   gr = read_graph_from_json(filename)
   
-  # now let's analysis and fix for loops:
+  # let's analysis and fix for loops:
   analyze_and_fix_cycles(gr)
 
-  # now see how many disconnected components are there
+  # see how many disconnected components are there
   analyze_connected_componets(gr)
+
+  # let's check the minimum time required
+  # and print achievable speedup
+  analyze_critical_path(gr)
