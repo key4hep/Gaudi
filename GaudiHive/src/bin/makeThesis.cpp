29484c25bd95e731bbf1185d705d915c642b85f4
Benedikt Hegner <hegner@cern.ch>
2013-03-20 23:29:33 +0100

    ops... how did that happen?

---
+++ GaudiHive/src/bin/makeThesis.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main ( int argc, char** argv )
+//-----------------------------------------------------------------------------
+{
+ std::cout << "Were you really expecting a working tool?" << std::endl; 
+ return 0; 
+}
