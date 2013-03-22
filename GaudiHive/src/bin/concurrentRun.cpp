f73e0220a96846b7f15ff94e5fb7c73426c0a51b
Danilo Piparo <dpiparo@cern.ch>
2013-03-22 08:26:31 +0100

    o Nicely print usage in case of unknown option instead of exception

--- GaudiHive/src/bin/concurrentRun.cpp
+++ GaudiHive/src/bin/concurrentRun.cpp
@@ -19,18 +19,26 @@ int main ( int argc, char** argv )
   // Declare the command line options and read them in
   options_description desc("Allowed options");
   desc.add_options()
-        ("help", "produce help message")
+        ("help,h", "produce help message")
         ("config", value<std::string>(), "the python configuration file")
   ;
   positional_options_description p;
   p.add("config", 1);
   variables_map vm;
-  store(command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+  try{
+    store(command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+  }
+  catch(boost::exception_detail::clone_impl<boost::exception_detail::error_info_injector<boost::program_options::unknown_option> >){
+    std::cerr << "Unknown option(s) detected." << std::endl
+              << "Usage:" << std::endl
+              << desc << std::endl;
+    return(1);
+  }
   notify(vm);
 
   std::string fileName;
   // handle the retrieved options
-  if (vm.count("help")) {
+  if (vm.count("help") or vm.count("h")) {
     std::cout << desc << std::endl;
     return 1;
   }
