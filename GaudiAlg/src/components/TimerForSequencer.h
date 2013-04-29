d3b6baaf4ff60cf5de379818606615a4cabefc06
Danilo Piparo <dpiparo@cern.ch>
2013-04-29 17:12:11 +0200

    Remove usage of doubles to accumulate statistic
    Use uint64_t-s instead: they will not suffer from problems linked
    to fp arithmetics, namely adding small numbers to a big one (the
    gap between two fp numbers may be to big to be crossed with the
    small number one wants to add). In addition, doubles are slow.
    Moreover, 64 bits unsigned integers can represent numbers up to
    ~10^19. The units are here microseconds, which would mean ~10^13
    seconds. A year has pi*10^7 seconds.

--- GaudiAlg/src/components/TimerForSequencer.h
+++ GaudiAlg/src/components/TimerForSequencer.h
@@ -3,12 +3,19 @@
 
 #include "GaudiKernel/MsgStream.h"
 #include "GaudiKernel/Timing.h"
+#include <inttypes.h> 
 
 /** Auxilliary class. Measure the time between start and stop, and compute
  *  average, min and max. In fact, measure the cpu time, and the elapsed time
  *  but givesmin/max only for elapsed.
  *
+ * Avoid usage of double precision floating point to cumulate counts and use
+ * unsigned long long integers instead. The total capacity is enough (~1.8e19)
+ * and there is no risk to loose counts when the available sum is big with
+ * respect to the added duration. The sigma 
+ * 
  * @author O.Callot
+ * @author D.Piparo
  */
 
 class TimerForSequencer {
@@ -16,20 +23,20 @@ class TimerForSequencer {
 public:
   /** Constructor. Specify the name, for later printing. */
 
-  TimerForSequencer( std::string name, double factor ) {
-    m_name   = name;
-    m_num    = 0L;
-    m_min    = 0.;
-    m_max    = 0.;
-    m_sum    = 0.;
-    m_sumCpu = 0.;
-    m_factor = factor;
-    m_lastTime = 0.;
-    m_lastCpu  = 0.;
-    m_startClock = 0LL;
-    m_startCpu = 0LL;
-  }
-
+  TimerForSequencer( std::string name, double factor ):
+  m_name(name),
+  m_factor(factor),
+  m_startClock(0ULL),
+  m_startCpu(0ULL),
+  m_num(0ULL),
+  m_lastTime(0ULL),
+  m_lastCpu(0ULL),
+  m_min(0ULL),
+  m_max(0ULL),
+  m_sum(0ULL),
+  m_sum2(0ULL),
+  m_sumCpu(0ULL){}
+    
   ~TimerForSequencer() {};
 
   /** Start a time measurement */
@@ -41,28 +48,27 @@ public:
   /** Stop time measurement and return the last elapsed time.
       @return Measured time in ms
   */
-  double stop() {
-    double cpuTime  =  double(System::cpuTime( System::microSec ) - m_startCpu );
-    double lastTime =  double(System::currentTime( System::microSec ) - m_startClock );
-
-    //== Change to normalized millisecond
-    cpuTime  *= m_factor;
-    lastTime *= m_factor;
+  uint64_t stop() {
+    uint64_t cpuTime  =  System::cpuTime( System::microSec ) - m_startCpu ;
+    uint64_t lastTime =  System::currentTime( System::microSec ) - m_startClock ;
 
     //== Update the counter
-    m_num    += 1;
+    m_num    += 1ULL;
     m_sum    += lastTime;
+    m_sum2   += lastTime*lastTime;
     m_sumCpu += cpuTime;
 
-    if ( 1 == m_num ) {
-      m_min = lastTime;
-      m_max = lastTime;
-    } else {
-      if ( lastTime < m_min ) m_min = lastTime;
-      if ( lastTime > m_max ) m_max = lastTime;
-    }
+    // Branchless update, only cast
+    bool  numIsFirst= (1ULL == m_num) ;
+    m_min += lastTime * numIsFirst;
+    m_max += lastTime * numIsFirst;
+
+    m_min = lastTime < m_min ? lastTime : m_min;
+    m_max = lastTime > m_max ? lastTime : m_max;
+
     m_lastTime = lastTime;
     m_lastCpu  = cpuTime;
+
     return lastTime;
   }
 
@@ -70,42 +76,54 @@ public:
   std::string name() const { return m_name; }
 
   /** returns the last measured time **/
-  double lastTime()   const { return m_lastTime; }
+  double lastTime()   const { return m_lastTime*m_factor; }
 
   /** returns the last measured time **/
-  double lastCpu()   const { return m_lastCpu; }
+  double lastCpu()   const { return m_lastCpu*m_factor; }
 
   /** returns the total elapsed time */
   double elapsedTotal() 
   {
-    return m_sum;
+    return m_sum*m_factor;
   }
 
   /** returns the toptal cpu time */
   double cpuTotal() 
   {
-    return m_sumCpu;
+    return m_sumCpu*m_factor;
   }
 
   /** Returns the number run count*/
-  double count() 
+  uint64_t count() 
   {
     return m_num;
   }
 
   /** Write measured time into the message stream. */
   MsgStream & fillStream(MsgStream & s) const {
-    double ave = 0.;
-    double cpu = 0.;
+    float ave = 0.f;
+    float cpu = 0.f;
 
-    if ( 0 != m_num ) {
+    if ( 0ULL != m_num ) {
       ave = m_sum    / m_num;
       cpu = m_sumCpu / m_num;
     }
+    
+    ave*=m_factor;
+    cpu*=m_factor;
+    float min = m_min * m_factor;
+    float max = m_max * m_factor;
+    float sum = m_sum * m_factor;
 
+    // Calculate the sigma with 2 momenta. ROOT histos call this quantity
+    // RMS but just to be consistent with paw.
+    // The division is by N-1 since one degree of freedom is used to calculate
+    // the average. See your favourite book for the proof!
+    float  sigma = m_num <= 1ULL ? 0.f : m_factor * sqrt( (m_sum2 - m_sum*m_sum/m_num)/(m_num-1) );
+  
     return s << m_name
-             << format( "| %9.3f | %9.3f | %8.3f %9.1f | %7d | %9.3f |",
-                        cpu, ave, m_min, m_max, m_num, m_sum * 0.001 );
+             << format( "| %9.3f | %9.3f | %8.3f %9.1f %8.2f | %7d | %9.3f |",
+                        cpu, ave, min, max, sigma, m_num, sum * 0.001f );
   }
 
   /** header matching the previous format **/
@@ -113,23 +131,25 @@ public:
     if ( size < 21 ) size = 21;
     std::string blank( size - 20, ' ' );
     std::string s = "Algorithm" + blank + "(millisec) |    <user> |   <clock> |";
-    s += "      min       max | entries | total (s) |";
+    s += "      min       max    sigma | entries | total (s) |";
     return s;
   }
 
 private:
   std::string m_name;
   double m_factor;
-  longlong m_startClock;
-  longlong m_startCpu;
+  uint64_t m_startClock;
+  uint64_t m_startCpu;
 
-  long m_num;
-  double m_lastTime;
-  double m_lastCpu;
-  double m_min;
-  double m_max;
-  double m_sum;
-  double m_sumCpu;
+  uint64_t m_num;
+  uint64_t m_lastTime;
+  uint64_t m_lastCpu;
+  uint64_t m_min;
+  uint64_t m_max;
+  uint64_t m_sum;
+  uint64_t m_sum2;  
+  uint64_t m_sumCpu;
+
 };
 
 inline MsgStream& operator<<(MsgStream& ms, const TimerForSequencer& count) {
