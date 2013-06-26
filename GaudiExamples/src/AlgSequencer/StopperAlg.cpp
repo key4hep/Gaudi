6580deef976c790c4778beae66249a14c55e5db4
Benedikt Hegner <hegner@cern.ch>
2013-06-26 20:53:16 +0200

    add status code check

--- GaudiExamples/src/AlgSequencer/StopperAlg.cpp
+++ GaudiExamples/src/AlgSequencer/StopperAlg.cpp
@@ -36,7 +36,10 @@ StatusCode StopperAlg::execute() {
   if ( ++count >= m_stopcount ) {
     log << MSG::INFO << "scheduling a event processing stop...." << endmsg;
     IEventProcessor* evt = svc<IEventProcessor>("ApplicationMgr");
-    evt->stopRun();
+    if (evt->stopRun().isFailure()) {
+      log << MSG::ERROR << "unable to schedule a stopRun" << endmsg;
+      return StatusCode::FAILURE;
+    }
     evt->release();
   }
 
