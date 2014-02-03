b12f6ed5e6ad4e99408d057bac37d6ebb5fbd65c
Marco Clemencic <marco.clemencic@cern.ch>
2014-02-03 18:57:43 +0100

    support backward compatibility with Reflex component names
    Allow factories for components to be found, with the new Plugin
    Service, using the names used in the Reflex one, for example, find
    'NS::Alg' as NS__Alg.

---
+++ GaudiExamples/src/PluginService/BackwardCompatibleAliases.cpp
@@ -0,0 +1,40 @@
+/// Test file used to check if the new Plugin Service is able to handle the
+/// strings used in the old one.
+/// For example the class PluginServiceTest::MyAlg was identified by the string
+/// "PluginServiceTest__MyAlg".
+
+#include "GaudiKernel/Algorithm.h"
+
+#include <string>
+#include <vector>
+
+namespace PluginServiceTest {
+  class MyAlg: public Algorithm {
+  public:
+    MyAlg(const std::string& name, ISvcLocator *svcloc):
+      Algorithm(name, svcloc) {}
+    virtual ~MyAlg() {}
+
+    virtual StatusCode execute() {
+      return StatusCode::SUCCESS;
+    }
+  };
+
+  template <class T1, class T2>
+  class MyTemplatedAlg: public Algorithm {
+  public:
+    MyTemplatedAlg(const std::string& name, ISvcLocator *svcloc):
+      Algorithm(name, svcloc) {}
+    virtual ~MyTemplatedAlg() {}
+
+    virtual StatusCode execute() {
+      return StatusCode::SUCCESS;
+    }
+  };
+}
+
+DECLARE_COMPONENT(PluginServiceTest::MyAlg)
+namespace {
+  typedef PluginServiceTest::MyTemplatedAlg<int&, double*> _dummy;
+}
+DECLARE_COMPONENT(_dummy)
