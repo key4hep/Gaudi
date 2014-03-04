5fc49a0edb3e44688dc52edf9f04f858d7a71f7c
Marco Clemencic <marco.clemencic@cern.ch>
2014-03-04 17:55:54 +0000

    use LCG 66 in cmake too
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@7413 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,5 +1,5 @@
 # Special wrapper to load the declared version of the heptools toolchain.
-set(heptools_version 67)
+set(heptools_version 66)
 
 # this check is needed because the toolchain is called when checking the
 # compiler (without the proper cache)
