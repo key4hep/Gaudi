fa3fa14c0e62500703bd3d1e5839a512b813cd7e
Daniel Funke <daniel.funke@cern.ch>
2014-05-26 15:00:28 +0200

    undoe changes to DeclareFactoryEntries until new Gaudi is merged

--- GaudiKernel/GaudiKernel/DeclareFactoryEntries.h
+++ GaudiKernel/GaudiKernel/DeclareFactoryEntries.h
@@ -3,36 +3,36 @@
 
 #ifdef GAUDI_NEW_PLUGIN_SERVICE
 #warning "obsolete empty header, please remove it"
-#else
-#include "GaudiKernel/Kernel.h"
-#include "GaudiKernel/Converter.h"
-#include "GaudiKernel/Service.h"
-#include "GaudiKernel/Algorithm.h"
-#include "GaudiKernel/Auditor.h"
-#include "GaudiKernel/AlgTool.h"
-#include "GaudiKernel/ObjectFactory.h"
-
-// Backwards compatibility dummies
-#define DECLARE_ALGORITHM(x)  /*dummy*/
-#define DECLARE_NAMESPACE_ALGORITHM(n,x) /*dummy*/
-
-#define DECLARE_AUDITOR(x)  /*dummy*/
-#define DECLARE_NAMESPACE_AUDITOR(n,x)  /*dummy*/
-
-#define DECLARE_GENERIC_CONVERTER(x) /* dummy */
-#define DECLARE_NAMESPACE_GENERIC_CONVERTER(n,x) /* dummy */
-#define DECLARE_CONVERTER(x) /*dummy*/
-#define DECLARE_NAMESPACE_CONVERTER(n,x)   /*dummy */
-
-#define DECLARE_SERVICE(x)  /*dummy*/
-#define DECLARE_NAMESPACE_SERVICE(n,x) /*dummy*/
-
-#define DECLARE_ALGTOOL(x)             /*dummy*/
-#define DECLARE_NAMESPACE_ALGTOOL(n,x) /*dummy*/
-#define DECLARE_TOOL(x)                /*dummy*/
-#define DECLARE_NAMESPACE_TOOL(n,x)    /*dummy*/
-
-#define DECLARE_FACTORY_ENTRIES(x) void x##_load()
+//#else
+//#include "GaudiKernel/Kernel.h"
+//#include "GaudiKernel/Converter.h"
+//#include "GaudiKernel/Service.h"
+//#include "GaudiKernel/Algorithm.h"
+//#include "GaudiKernel/Auditor.h"
+//#include "GaudiKernel/AlgTool.h"
+//#include "GaudiKernel/ObjectFactory.h"
+//
+//// Backwards compatibility dummies
+//#define DECLARE_ALGORITHM(x)  /*dummy*/
+//#define DECLARE_NAMESPACE_ALGORITHM(n,x) /*dummy*/
+//
+//#define DECLARE_AUDITOR(x)  /*dummy*/
+//#define DECLARE_NAMESPACE_AUDITOR(n,x)  /*dummy*/
+//
+//#define DECLARE_GENERIC_CONVERTER(x) /* dummy */
+//#define DECLARE_NAMESPACE_GENERIC_CONVERTER(n,x) /* dummy */
+//#define DECLARE_CONVERTER(x) /*dummy*/
+//#define DECLARE_NAMESPACE_CONVERTER(n,x)   /*dummy */
+//
+//#define DECLARE_SERVICE(x)  /*dummy*/
+//#define DECLARE_NAMESPACE_SERVICE(n,x) /*dummy*/
+//
+//#define DECLARE_ALGTOOL(x)             /*dummy*/
+//#define DECLARE_NAMESPACE_ALGTOOL(n,x) /*dummy*/
+//#define DECLARE_TOOL(x)                /*dummy*/
+//#define DECLARE_NAMESPACE_TOOL(n,x)    /*dummy*/
+//
+//#define DECLARE_FACTORY_ENTRIES(x) void x##_load()
 
 #endif
 
