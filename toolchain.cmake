535483b5b42c2819119896c95e62c51a2164f201
Marco Clemencic <marco.clemencic@cern.ch>
2014-06-02 11:13:55 +0000

    reverted to LCG 68 to prepare Gaudi v25r2
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@7467 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,5 +1,5 @@
 # Special wrapper to load the declared version of the heptools toolchain.
-set(heptools_version 67b)
+set(heptools_version 68)
 
 if(DEFINED ENV{HEPTOOLS_VERSION})
   set(heptools_version $ENV{HEPTOOLS_VERSION})
