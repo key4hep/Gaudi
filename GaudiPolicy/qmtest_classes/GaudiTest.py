ada83dbf7a7838dcac33353ba9dfd72cd09ac612
Marco Clemencic <marco.clemencic@cern.ch>
2014-07-02 10:54:37 +0000

    fix GAUDI-976: make GaudiTest.py resilient towards conversion failure
    https://its.cern.ch/jira/browse/GAUDI-976
    
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@7496 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- GaudiPolicy/qmtest_classes/GaudiTest.py
+++ GaudiPolicy/qmtest_classes/GaudiTest.py
@@ -15,6 +15,8 @@ import string
 import difflib
 import time
 import calendar
+import codecs
+
 from subprocess import Popen, PIPE, STDOUT
 
 try:
@@ -1727,7 +1729,7 @@ class GaudiExeTest(ExecTestBase):
         data["project"] = projectName.strip()
 
         # Template for the XML file, based on eclipse 3.4
-        xml = u"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+        xml_template = u"""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <launchConfiguration type="org.eclipse.cdt.launch.applicationLaunchType">
 <booleanAttribute key="org.eclipse.cdt.debug.mi.core.AUTO_SOLIB" value="true"/>
 <listAttribute key="org.eclipse.cdt.debug.mi.core.AUTO_SOLIB_LIST"/>
@@ -1775,11 +1777,17 @@ class GaudiExeTest(ExecTestBase):
 <listEntry value="org.eclipse.debug.ui.launchGroup.debug"/>
 </listAttribute>
 </launchConfiguration>
-""" % data
+"""
+        try:
+            # ensure the correct encoding of data values
+            for k in data:
+                data[k] = codecs.decode(data[k], 'utf-8')
+            xml = xml_template % data
 
-        # Write the output file
-        open(destfile, "w").write(xml)
-        #open(destfile + "_copy.xml", "w").write(xml)
+            # Write the output file
+            codecs.open(destfile, "w", encoding='utf-8').write(xml)
+        except:
+            print 'WARNING: problem generating Eclipse launcher'
 
 
 try:
