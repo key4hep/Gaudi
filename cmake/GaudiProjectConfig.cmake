bb03b6f87899cc58085808255af6a68bb4abb5c8
Marco Clemencic <marco.clemencic@cern.ch>
2014-07-10 08:22:55 +0200

    fixed dependency of the <dictionary>PCM target
    The custom command used for the <dictionary>PCM  target must depend on
    <dictionary>Gen (and not on the pcm file) to avoid that the genreflex
    command is called multiple times.

--- cmake/GaudiProjectConfig.cmake
+++ cmake/GaudiProjectConfig.cmake
@@ -1929,7 +1929,7 @@ function(gaudi_add_dictionary dictionary header selection)
     get_property(pcmname TARGET ${dictionary}Gen PROPERTY PCMFILE)
     add_custom_command(OUTPUT ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${pcmname}
                        COMMAND ${CMAKE_COMMAND} -E copy ${pcmname} ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${pcmname}
-                       DEPENDS ${pcmname})
+                       DEPENDS ${dictionary}Gen)
     add_custom_target(${dictionary}PCM ALL
                       DEPENDS ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${pcmname})
   endif()
