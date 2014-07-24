d05c22f6e71c94b752477a5e44353445ad866aed
Marco Clemencic <marco.clemencic@cern.ch>
2014-07-24 13:58:37 +0200

    factored out the default toolchain code and modified toolchain.cmake

---
+++ cmake/GaudiDefaultToolchain.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR} ${CMAKE_MODULE_PATH})
+
+# it should be found in the current directory
+include(GaudiToolchainMacros)
+
+init()
+find_projects(projects tools ${CMAKE_SOURCE_DIR}/CMakeLists.txt)
+
+if(heptools_version)
+  include(UseHEPTools)
+  use_heptools(${heptools_version})
+else()
+  include(InheritHEPTools)
+  inherit_heptools()
+endif()
+
+set_paths_from_projects(${tools} ${projects})
--- toolchain.cmake
+++ toolchain.cmake
@@ -8,12 +8,13 @@ endif()
 # this check is needed because the toolchain is called when checking the
 # compiler (without the proper cache)
 if(NOT CMAKE_SOURCE_DIR MATCHES "CMakeTmp")
-  set(CMAKE_PREFIX_PATH ${CMAKE_SOURCE_DIR}/cmake ${CMAKE_PREFIX_PATH})
-  find_file(use_heptools_module UseHEPTools.cmake)
-  if(use_heptools_module)
-    include(${use_heptools_module})
-    use_heptools(${heptools_version})
+
+  find_file(default_toolchain NAMES GaudiDefaultToolchain.cmake
+            HINTS ${CMAKE_SOURCE_DIR}/cmake)
+  if(default_toolchain)
+    include(${default_toolchain})
   else()
-    message(FATAL_ERROR "Cannot find UseHEPTools.cmake")
+    message(FATAL_ERROR "Cannot find GaudiDefaultToolchain.cmake")
   endif()
+
 endif()
