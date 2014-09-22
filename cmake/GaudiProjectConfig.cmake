key4hep/Gaudi

commit 3084f142f4d9cbecb60fafab91a7ef0998bb557a

avoid error when 'libs' is set to NOTFOUND

cmake/GaudiProjectConfig.cmake
@@ -1321,7 +1321,9 @@ function(gaudi_resolve_link_libraries variable)
                        PROPERTY RESOLVED_REQUIRED_LIBRARIES ${libs})
         endif()
       endif()
-      set(collected ${collected} ${package} ${libs})
+      if(libs)
+        set(collected ${collected} ${package} ${libs})
+      endif()
     elseif(EXISTS ${package}) # it's a real file
       #message(STATUS "${package} is a FILE")
       set(collected ${collected} ${package})

commit 19cdfb731a88d29b377ab68146c804806fc2142a

Fixed a problem introduced with commit 3084f142

cmake/GaudiProjectConfig.cmake
@@ -1324,11 +1324,13 @@ function(gaudi_resolve_link_libraries variable)
           gaudi_resolve_link_libraries(libs ${libs})
           set_property(TARGET ${package}
                        PROPERTY RESOLVED_REQUIRED_LIBRARIES ${libs})
+        else()
+          # this is to avoid that libs gets defined as libs-NOTFOUND
+          # (it may happen in some rare conditions)
+          set(libs)
         endif()
       endif()
-      if(libs)
-        set(collected ${collected} ${package} ${libs})
-      endif()
+      set(collected ${collected} ${package} ${libs})
     elseif(EXISTS ${package}) # it's a real file
       #message(STATUS "${package} is a FILE")
       set(collected ${collected} ${package})