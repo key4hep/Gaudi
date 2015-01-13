key4hep/Gaudi

commit f0b0a33136ad67095527f9a7d9328ff3dc2808f6

GAUDI-993: more speed improvements in DataSvc::retrieveEntry
From: Gerhard Raven <gerhard.raven@nikhef.nl>


git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@7618 53ecefc3-a64d-0410-9bdf-b8581fa3287e

GaudiKernel/doc/release.notes
@@ -2,6 +2,9 @@ Package : GaudiKernel
 Package manager : Marco Clemencic
 Commit Id: $Format:%H$
 
+! 2015-01-07 - Gerhard Raven
+ - GAUDI-993: more speed improvements in DataSvc::retrieveEntry
+
 ! 2015-01-07 - Marco Clemencic
  - Fixed g++ 4.9 warning (unused typedef).
 

GaudiKernel/GaudiKernel/DataSvc.h
@@ -417,10 +417,10 @@ protected:
     */
   DataObject* handleDataFault(IRegistry* pReg, const std::string& path="");
 private:
-  StatusCode _retrieveEntry(DataSvcHelpers::RegistryEntry* parentObj,
-                            boost::string_ref path,
-                            DataSvcHelpers::RegistryEntry*& pEntry) ;
-  DataObject* _handleDataFault(IRegistry* pReg, boost::string_ref path = boost::string_ref{});
+  StatusCode i_retrieveEntry(DataSvcHelpers::RegistryEntry* parentObj,
+                             boost::string_ref path,
+                             DataSvcHelpers::RegistryEntry*& pEntry) ;
+  DataObject* i_handleDataFault(IRegistry* pReg, boost::string_ref path = boost::string_ref{});
 };
 #endif // GAUDIKERNEL_DATASVC_H
 

GaudiKernel/GaudiKernel/RegistryEntry.h
@@ -74,8 +74,10 @@ namespace DataSvcHelpers {
     RegistryEntry* i_find ( boost::string_ref path )  const;
     /// Internal method to locate object entry
     RegistryEntry* i_find ( const DataObject* pObject )  const;
+    /// Internal method to create entries
+    RegistryEntry* i_create ( std::string name );
     /// Internal method to add entries
-    RegistryEntry* i_add ( const std::string& name );
+    long i_add ( RegistryEntry* entry );
     /// Set new parent pointer
     void setParent(RegistryEntry* pParent);
     /// Set the transient data store
@@ -104,7 +106,7 @@ namespace DataSvcHelpers {
     void makeSoft (IOpaqueAddress* pAddress);
   public:
     /// Standard Constructor
-    RegistryEntry(const std::string& path, RegistryEntry* parent = 0);
+    RegistryEntry(std::string path, RegistryEntry* parent = 0);
     /// Standard Destructor
     virtual ~RegistryEntry();
     /// IInterface implementation: Reference the object

GaudiKernel/src/Lib/RegistryEntry.cpp
@@ -42,35 +42,23 @@
 constexpr char SEPARATOR { '/' };
 
 namespace {
-  inline boost::string_ref::size_type find_( boost::string_ref s, char c, size_t o ) {
-        if (!s.empty()) s.remove_prefix(o);
-        auto r = s.find(c);
-        return (r == boost::string_ref::npos) ? r : (r + o);
-  }
   inline std::string operator+(std::string s, boost::string_ref p) {
-        return s.append(p.data(),p.size());
-  }
-  inline std::string to_string(boost::string_ref p) {
-        return { p.data(), p.size() };
+    return s.append(p.data(),p.size());
   }
 }
 /// Standard Constructor
-DataSvcHelpers::RegistryEntry::RegistryEntry(const std::string& path, RegistryEntry* parent)
+DataSvcHelpers::RegistryEntry::RegistryEntry(std::string path, RegistryEntry* parent)
 : m_refCount(0),
   m_isSoft(false),
-  m_path(path),
+  m_path(std::move(path)),
   m_pParent(parent),
-  m_pAddress(0),
-  m_pObject(0),
-  m_pDataProviderSvc(0)
+  m_pAddress(nullptr),
+  m_pObject(nullptr),
+  m_pDataProviderSvc(nullptr)
 {
   std::string::size_type sep = m_path.rfind(SEPARATOR);
-  if ( path[0] != SEPARATOR )   {
-    m_path.insert(m_path.begin(), SEPARATOR);
-  }
-  if ( sep != std::string::npos )    {
-    m_path.erase(0,sep);
-  }
+  if ( m_path.front() != SEPARATOR ) m_path.insert(0,1,SEPARATOR);
+  if ( sep != std::string::npos )  m_path.erase(0,sep);
   assemblePath(m_fullpath);
   addRef();
 }
@@ -78,12 +66,12 @@ DataSvcHelpers::RegistryEntry::RegistryEntry(const std::string& path, RegistryEn
 /// Standard destructor
 DataSvcHelpers::RegistryEntry::~RegistryEntry()  {
   deleteElements();
-  if ( 0 != m_pObject  )   {
-    if ( !m_isSoft ) m_pObject->setRegistry(0);
+  if ( m_pObject  )   {
+    if ( !m_isSoft ) m_pObject->setRegistry(nullptr);
     m_pObject->release();
   }
-  if ( 0 != m_pAddress )   {
-    if ( !m_isSoft ) m_pAddress->setRegistry(0);
+  if ( m_pAddress )   {
+    if ( !m_isSoft ) m_pAddress->setRegistry(nullptr);
     m_pAddress->release();
   }
 }
@@ -91,9 +79,7 @@ DataSvcHelpers::RegistryEntry::~RegistryEntry()  {
 /// Release entry
 unsigned long DataSvcHelpers::RegistryEntry::release()  {
   unsigned long cnt = --m_refCount;
-  if ( 0 == m_refCount )   {
-    delete this;
-  }
+  if ( !m_refCount ) delete this;
   return cnt;
 }
 
@@ -123,12 +109,8 @@ void DataSvcHelpers::RegistryEntry::makeSoft(IOpaqueAddress* pAddress)   {
 void DataSvcHelpers::RegistryEntry::makeHard(DataObject* pObject)   {
   makeSoft(pObject);
   m_isSoft = false;
-  if ( 0 != m_pObject )   {
-    m_pObject->setRegistry(this);
-  }
-  if ( 0 != m_pAddress )   {
-    m_pAddress->setRegistry(this);
-  }
+  if ( m_pObject )   m_pObject->setRegistry(this);
+  if ( m_pAddress )  m_pAddress->setRegistry(this);
 }
 
 /// Create hard link
@@ -139,21 +121,21 @@ void DataSvcHelpers::RegistryEntry::makeHard(IOpaqueAddress* pAddress)   {
 
 /// Update Opaque address of registry entry
 void DataSvcHelpers::RegistryEntry::setAddress( IOpaqueAddress* pAddress )    {
-  if ( 0 != pAddress  )   {
+  if ( pAddress )   {
     pAddress->addRef();
     pAddress->setRegistry(this);
   }
-  if ( 0 != m_pAddress ) m_pAddress->release();
+  if ( m_pAddress ) m_pAddress->release();
   m_pAddress = pAddress;
 }
 
 /// Set object pointer of data store item
 void DataSvcHelpers::RegistryEntry::setObject( DataObject* pObject )   {
-  if ( 0 != pObject  )  {
+  if ( pObject  )  {
     pObject->addRef();
     if ( !isSoft() ) pObject->setRegistry(this);
   }
-  if ( 0 != m_pObject ) m_pObject->release();
+  if ( m_pObject ) m_pObject->release();
   m_pObject = pObject;
 }
 
@@ -161,10 +143,10 @@ void DataSvcHelpers::RegistryEntry::setObject( DataObject* pObject )   {
 long DataSvcHelpers::RegistryEntry::remove  ( IRegistry* obj )    {
   try   {
     RegistryEntry* pEntry = dynamic_cast<RegistryEntry*>(obj);
-    Store::iterator i = std::remove(m_store.begin(), m_store.end(), pEntry);
-    if (i != m_store.end())   {
+    auto i = std::remove(std::begin(m_store), std::end(m_store), pEntry);
+    if (i != std::end(m_store))   {
       pEntry->release();
-      m_store.erase( i, m_store.end() );
+      m_store.erase( i, std::end(m_store) );
     }
   }
   catch ( ... )   {     }
@@ -173,11 +155,7 @@ long DataSvcHelpers::RegistryEntry::remove  ( IRegistry* obj )    {
 
 /// Remove entry from data store
 long DataSvcHelpers::RegistryEntry::remove ( const std::string& nam )  {
-  if ( nam[0] != SEPARATOR )   {
-    std::string path = nam;
-    path.insert(path.begin(), SEPARATOR);
-    return remove(path);
-  }
+  if ( nam.front() != SEPARATOR ) return remove( SEPARATOR+nam );
   // if this object is already present, this is an error....
   for (auto& i : m_store ) {
     if ( nam == i->name() )   {
@@ -189,27 +167,35 @@ long DataSvcHelpers::RegistryEntry::remove ( const std::string& nam )  {
 }
 
 /// Internal method to add entries
-DataSvcHelpers::RegistryEntry* DataSvcHelpers::RegistryEntry::i_add(const std::string& nam)    {
-  if ( nam[0] != SEPARATOR )   {
-    return i_add( SEPARATOR + nam );
-  }
+DataSvcHelpers::RegistryEntry* DataSvcHelpers::RegistryEntry::i_create(std::string nam)    {
+  if ( nam.front() != SEPARATOR ) nam.insert(0,1,SEPARATOR);
   // if this object is already present, this is an error....
-  for (auto& i : m_store ) {
-    if ( nam == i->name() )  {
-      return nullptr;
-    }
-  }
-  return new RegistryEntry( nam, this );
+  auto not_present = std::none_of( std::begin(m_store), std::end(m_store),
+                                   [&](IRegistry* i) { return nam == i->name(); 
+  } );
+  return not_present ? new RegistryEntry( std::move(nam), this ) : nullptr;
 }
 
 ///Add object to the container
-long DataSvcHelpers::RegistryEntry::add( IRegistry* obj )    {
-  try   {
+long DataSvcHelpers::RegistryEntry::add( IRegistry* obj ) {
     RegistryEntry* pEntry = CAST_REGENTRY(RegistryEntry*, obj);
+    return i_add(pEntry);
+}
+
+///Add object to the container
+long DataSvcHelpers::RegistryEntry::i_add( RegistryEntry* pEntry ) {
+//TODO: if this is the sole place where items are added to m_store, 
+//      and we know here that they must be RegisteryEntry, can we 
+//      drop the dynamic_cast every where else???
+//TODO: if so, can we also change m_store to be std::vector<RegistryEntry*> 
+//      instead
+//TODO: if so, can we not make it std::vector<RegistryEntry> instead?
+//TODO: if so, should make sure that a RegistryEntry can be std::move'ed
+  try {
     pEntry->setDataSvc(m_pDataProviderSvc);
-    m_store.push_back(pEntry);
+    m_store.push_back(pEntry); 
     pEntry->setParent(this);
-    if ( !pEntry->isSoft() && pEntry->address() != 0 )   {
+    if ( !pEntry->isSoft() && pEntry->address() )   {
       pEntry->address()->setRegistry(pEntry);
     }
   }
@@ -220,24 +206,20 @@ long DataSvcHelpers::RegistryEntry::add( IRegistry* obj )    {
 
 /// Add entry to the current data store item
 long DataSvcHelpers::RegistryEntry::add ( const std::string& name, DataObject* pObject, bool is_soft )  {
-  RegistryEntry* entry = i_add(name);
-  if ( 0 != entry )   {
-    ( is_soft ) ? entry->makeSoft(pObject) : entry->makeHard(pObject);
-    add( entry );
-    return StatusCode::SUCCESS;
-  }
-  return StatusCode::FAILURE;
+  RegistryEntry* entry = i_create(name);
+  if ( !entry ) return StatusCode::FAILURE;
+  ( is_soft ) ? entry->makeSoft(pObject) : entry->makeHard(pObject);
+  i_add( entry );
+  return StatusCode::SUCCESS;
 }
 
 /// Add entry to the current data store item
 long DataSvcHelpers::RegistryEntry::add ( const std::string& name, IOpaqueAddress* pAddress, bool is_soft )  {
-  RegistryEntry* entry = i_add(name);
-  if ( 0 != entry )   {
-    ( is_soft ) ? entry->makeSoft(pAddress) : entry->makeHard(pAddress);
-    add( entry );
-    return StatusCode::SUCCESS;
-  }
-  return StatusCode::FAILURE;
+  RegistryEntry* entry = i_create(name);
+  if ( !entry )  return StatusCode::FAILURE;
+  ( is_soft ) ? entry->makeSoft(pAddress) : entry->makeHard(pAddress);
+  i_add( entry );
+  return StatusCode::SUCCESS;
 }
 
 /// Delete recursively all elements pending from the current store item
@@ -261,68 +243,55 @@ IRegistry* DataSvcHelpers::RegistryEntry::i_find( const IRegistry* obj )  const 
 
 /// Find identified leaf in this registry node
 DataSvcHelpers::RegistryEntry* DataSvcHelpers::RegistryEntry::i_find(boost::string_ref path)   const    {
-  if ( path.front() != SEPARATOR )    return i_find(std::string{SEPARATOR}+path);
-  auto len  = path.size();
-  auto loc1 = find_(path,SEPARATOR,1);
-  auto len2 = (loc1 != boost::string_ref::npos) ? loc1 : len;
-  for (const auto& i : m_store )   {
+  if ( path.front() == SEPARATOR ) return i_find(path.substr(1)); // strip leading '/'
+  auto loc1 = path.find(SEPARATOR);
+  auto len2 = (loc1 != boost::string_ref::npos) ? loc1 : path.size();
+  for (const auto& i : m_store ) {
     RegistryEntry* regEnt = CAST_REGENTRY(RegistryEntry*, i);
-    const std::string& nam = regEnt->name();
-    // check that the first len2 chars of path are the same as nam
-    // (i.e. match {len2:3 nam:"/Ab" path:"/Ab/C"}
-    // but not {len2:3 nam:"/Abc" path:"/Ab/C"})
-    if ( path.substr(0, len2) == nam ) {
+    // check that the first len2 chars of path are the same as regEng->name()
+    // (i.e. match {len2:3 nam:"/Ab"  path:"/Ab/C"}
+    // but not     {len2:3 nam:"/Abc" path:"/Ab/C"})
+    if ( path.substr(0, len2) == boost::string_ref{regEnt->m_path}.substr(1) ) {
       try {
-        if ( loc1 != boost::string_ref::npos ) {
-          auto  search_path = path.substr(loc1, len);
-          IRegistry* pDir = regEnt->find(to_string(search_path));
-          if ( pDir )    {
-            return CAST_REGENTRY(RegistryEntry*, pDir);
-          }
-          return nullptr;
-        }
-        else  {
-          return CAST_REGENTRY(RegistryEntry*, i);
-        }
+        return ( loc1 == boost::string_ref::npos ) ? 
+                    regEnt : 
+                    regEnt->i_find(path.substr(loc1));
       }
-      catch (...)   {
-      }
+      catch (...)   { }
     }
   }
   // If this node is "/NodeA", this part allows to find "/NodeA/NodeB" as
   // our "/NodeB" child.
-  if ( path.substr(0, len2) == m_path ) {
-    if (len2 < len)   {
-      return i_find(path.substr(loc1, len));
-    }
+  if ( len2 < path.size() && path.substr(0, len2) == boost::string_ref{m_path}.substr(1) ) {
+     return i_find(path.substr(loc1));
   }
   return nullptr;
 }
 
 /// Find identified leaf in this registry node
 DataSvcHelpers::RegistryEntry* DataSvcHelpers::RegistryEntry::i_find(const DataObject* key)  const  {
-  if ( 0 != key )  {
+  if ( key )  {
     if ( key == m_pObject )    {
       return const_cast<RegistryEntry*>(this);
     }
     // Look in the immediate level:
     RegistryEntry *result = CAST_REGENTRY(RegistryEntry*, i_find(key->registry()));
-    if ( 0 != result ) return result;
+    if ( result ) return result;
     // Go levels down
-    for (Store::const_iterator i = m_store.begin(); i != m_store.end(); i++ )   {
+    for (const auto& i : m_store) {
       try   {
-        const RegistryEntry *entry = CAST_REGENTRY(RegistryEntry*, *i);
-        if( 0 != (result = entry->i_find(key)) )
-          return result;
+        const RegistryEntry *entry = CAST_REGENTRY(RegistryEntry*, i);
+        result = entry->i_find(key);
+        if( result ) return result;
       }
       catch ( ... )   {    }
     }
   }
-  return 0;
+  return nullptr;
 }
 
 // Traverse registry tree
-long DataSvcHelpers::RegistryEntry::traverseTree(IDataStoreAgent* pAgent, int level)    {
+long DataSvcHelpers::RegistryEntry::traverseTree(IDataStoreAgent* pAgent, int level) {
   bool go_down = pAgent->analyse(this, level);
   long status = StatusCode::SUCCESS;
   if ( go_down )    {
@@ -341,8 +310,6 @@ long DataSvcHelpers::RegistryEntry::traverseTree(IDataStoreAgent* pAgent, int le
 
 // Recursive helper to assemble the full path name of the entry
 void DataSvcHelpers::RegistryEntry::assemblePath(std::string& buffer)  const  {
-  if ( m_pParent )    {
-    m_pParent->assemblePath(buffer);
-  }
+  if ( m_pParent ) m_pParent->assemblePath(buffer);
   buffer += m_path;
 }

commit 261a413839d4a806577b8ed03b553252870b455c

Fixed regression introduced with commit f0b0a33
(GAUDI-993: more speed improvements in DataSvc::retrieveEntry)

From: Marco Clemencic <marco.clemencic@cern.ch>


git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@7630 53ecefc3-a64d-0410-9bdf-b8581fa3287e

GaudiKernel/doc/release.notes
@@ -3,6 +3,10 @@ Package manager : Marco Clemencic
 Commit Id: $Format:%H$
 
 ============================= GaudiKernel v31r3 ==============================
+! 2015-01-13 - Marco Clemencic
+ - Fixed regression introduced with commit f0b0a33
+   (GAUDI-993: more speed improvements in DataSvc::retrieveEntry)
+
 ! 2015-01-12 - Gerhard Raven
  - GAUDI-993: Avoid recursion in DataSvc::i_retrieveEntry.
 

GaudiKernel/src/Lib/DataSvc.cpp
@@ -267,7 +267,7 @@ StatusCode DataSvc::registerAddress(IRegistry* parentObj,
                                     IOpaqueAddress* pAddress)   {
   if ( !checkRoot() )  return INVALID_ROOT;
   if ( objPath.empty() )  return INVALID_OBJ_PATH;
-  
+
   if ( !parentObj )   {
     if ( objPath.front() != SEPARATOR )    {
       return registerAddress(m_root, objPath, pAddress);
@@ -285,7 +285,7 @@ StatusCode DataSvc::registerAddress(IRegistry* parentObj,
   }
   RegEntry* par_entry = CAST_REGENTRY(RegEntry*,parentObj);
   if ( !par_entry ) return INVALID_PARENT;
-  
+
   std::string::size_type sep = objPath.rfind(SEPARATOR);
   if ( sep > 0 && sep != std::string::npos )    {
     auto p_path = objPath.substr( 0, sep);
@@ -311,7 +311,7 @@ StatusCode DataSvc::registerAddress(IRegistry* parentObj,
 ///  IDataManagerSvc: Unregister object address from the data store.
 StatusCode DataSvc::unregisterAddress(const std::string& fullPath)    {
   if ( fullPath.empty() )  return INVALID_OBJ_PATH;
-  return unregisterAddress( fullPath.front() != SEPARATOR ? m_root  :  nullptr, 
+  return unregisterAddress( fullPath.front() != SEPARATOR ? m_root  :  nullptr,
                             fullPath );
 }
 
@@ -327,13 +327,13 @@ StatusCode DataSvc::unregisterAddress(IRegistry* pParent,
                                       const std::string& objPath)   {
   if ( !checkRoot() )  return INVALID_ROOT;
   if ( objPath.empty() )   return INVALID_OBJ_PATH;
-  
+
   if ( !pParent )   {
     if ( objPath.front() != SEPARATOR )    {
       return unregisterAddress(m_root, objPath);
     }
     std::string::size_type sep = objPath.find(SEPARATOR,1);
-    if ( sep != std::string::npos && 
+    if ( sep != std::string::npos &&
          objPath.compare( 0, sep, m_rootName) == 0 )   {
         return unregisterAddress(m_root, objPath.substr(sep));
     }
@@ -398,7 +398,7 @@ StatusCode DataSvc::registerObject(DataObject* parentObj,
                                    const std::string& objPath,
                                    DataObject* pObject)   {
   if ( !checkRoot() )  return INVALID_ROOT;
-  
+
   if ( !parentObj )   {
     if ( !objPath.empty() )   {
       if ( objPath.front() == SEPARATOR )    {
@@ -486,12 +486,12 @@ StatusCode DataSvc::unregisterObject(const std::string& fullPath)   {
   if ( status.isFailure() )    return status;
   RegEntry* pEntry = CAST_REGENTRY(RegEntry*,pObject->registry());
   if ( !pEntry )    return INVALID_ROOT;
-  
+
   if ( !pEntry->isEmpty() )    return DIR_NOT_EMPTY;
-  
+
   RegEntry* pParent = pEntry->parentEntry();
   if ( !pParent )   return INVALID_PARENT;
-  
+
   if ( pObject )    pObject->addRef();
   pParent->remove(pEntry);
   return StatusCode::SUCCESS;
@@ -517,8 +517,8 @@ StatusCode DataSvc::unregisterObject(DataObject* pObject)    {
   RegEntry* entry = m_root->findLeaf(pObject);
   if ( !entry )   return INVALID_OBJECT;
   RegEntry* parent = entry->parentEntry();
-  if ( parent )    return DIR_NOT_EMPTY;
-  if ( !entry->isEmpty() )    return INVALID_PARENT;
+  if ( !parent )     return INVALID_PARENT;
+  if ( !entry->isEmpty() )   return DIR_NOT_EMPTY;
   if ( entry->object() ) entry->object()->addRef();
   if ( parent )  parent->remove(entry);
   return SUCCESS;
@@ -626,7 +626,7 @@ StatusCode DataSvc::loadObject(IConversionSvc* pLoader, IRegistry* pRegistry) {
     m_incidentSvc->fireIncident(incident);
   }
   if ( !m_inhibitPathes.empty() )   {
-    auto inhibit = std::find(m_inhibitPathes.begin(), m_inhibitPathes.end(), 
+    auto inhibit = std::find(m_inhibitPathes.begin(), m_inhibitPathes.end(),
                              pRegistry->identifier());
     if ( inhibit != m_inhibitPathes.end() )   {
       return NO_ACCESS;
@@ -696,7 +696,7 @@ StatusCode DataSvc::i_retrieveEntry(RegEntry* parentObj,
   if ( !checkRoot() )  return StatusCode(INVALID_ROOT,true);
 
   static const auto empty = boost::string_ref{};
-  auto sep    = find(path,SEPARATOR,1); 
+  auto sep    = find(path,SEPARATOR,1);
   pEntry = nullptr;
 
   if ( !parentObj )    {
@@ -774,7 +774,7 @@ StatusCode DataSvc::i_retrieveEntry(RegEntry* parentObj,
     //DataIncident incident(name(), m_accessName, pEntry->identifier());
     //m_incidentSvc->fireIncident(incident);
     return SUCCESS;
-  } 
+  }
   return SUCCESS;
 }
 
@@ -942,7 +942,7 @@ StatusCode DataSvc::updateObject(DataObject* toUpdate)    {
     return NO_DATA_LOADER;                     // Data loader must be present
   }
   if ( !m_inhibitPathes.empty() )   {
-    auto inhibit = std::find( m_inhibitPathes.begin(), m_inhibitPathes.end(), 
+    auto inhibit = std::find( m_inhibitPathes.begin(), m_inhibitPathes.end(),
                               pRegistry->identifier());
     if ( inhibit != m_inhibitPathes.end() )   {
       return NO_ACCESS;
@@ -1150,7 +1150,7 @@ StatusCode DataSvc::addPreLoadItem(const std::string& itemPath)    {
 
 /// Remove an item from the preload list
 StatusCode DataSvc::removePreLoadItem(const DataStoreItem& item)   {
-  m_preLoads.erase(std::remove(std::begin(m_preLoads), std::end(m_preLoads), item), 
+  m_preLoads.erase(std::remove(std::begin(m_preLoads), std::end(m_preLoads), item),
                    std::end(m_preLoads));
   return StatusCode::SUCCESS;
 }