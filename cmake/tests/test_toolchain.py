bc1541337668b11792f5966c78efaaa34148656d
Marco Clemencic <marco.clemencic@cern.ch>
2015-01-29 15:01:08 +0000

    refactored CMake modules test helpers
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@7672 53ecefc3-a64d-0410-9bdf-b8581fa3287e

--- cmake/tests/test_toolchain.py
+++ cmake/tests/test_toolchain.py
@@ -3,25 +3,16 @@
 import os
 from os.path import join, dirname
 
-from cmake_coverage import cmake_script
+from cmake_test_utils import CMakeTestScripts
 
-base_dir = dirname(__file__)
+class Tests(CMakeTestScripts):
+    base_dir = dirname(__file__)
+    scripts_dir = join(base_dir, 'cmake_scripts')
 
-def setup():
-    os.environ['CMAKE_PREFIX_PATH'] = join(base_dir, 'data', 'toolchain')
-    os.environ['CMTCONFIG'] = 'x86_64-slc6-gcc48-opt'
+    def setup(self):
+        os.environ['CMAKE_PREFIX_PATH'] = join(self.base_dir, 'data', 'toolchain')
+        os.environ['CMTCONFIG'] = 'x86_64-slc6-gcc48-opt'
 
-def cmake_script_runner(name):
-    script_name = join(base_dir, 'cmake_scripts', 'test_%s.cmake' % name)
-    out, err, returncode = cmake_script(script_name, cwd=base_dir)
-    print "---------- stdout ----------"
-    print out
-    print "---------- stderr ----------"
-    print err
-    assert returncode == 0
-
-def test_script():
-    for test in ['toolchain_extensions_min',
-                 'toolchain_extensions',
-                 'toolchain_extensions_multi']:
-        yield cmake_script_runner, test
+    tests = ['toolchain_extensions_min',
+             'toolchain_extensions',
+             'toolchain_extensions_multi']
