947b2e2694004fbc4f3a4bf883dd0e5442c67b7c
Marco Clemencic <marco.clemencic@cern.ch>
2015-02-03 11:46:29 +0000

    added tool to report the CPU capabilities and a simple test
    The test uses /proc/cpuinfo for comparison.
    
    See GAUDI-999.
    
    From: Marco Clemencic <marco.clemencic@cern.ch>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/gaudi/Gaudi/trunk@7683 53ecefc3-a64d-0410-9bdf-b8581fa3287e

---
+++ GaudiKernel/tests/nose/test_instructionsetLevel.py
@@ -0,0 +1,20 @@
+import os
+from subprocess import Popen, PIPE
+
+def test():
+    out = Popen(['instructionsetLevel.exe', 'all'],
+                stdout=PIPE).communicate()[0]
+    known_flags = set(l.strip() for l in out.splitlines())
+
+    expected = set()
+    for l in open('/proc/cpuinfo'):
+        if l.startswith('flags'):
+            expected = set(l.strip().split()).intersection(known_flags)
+            break
+
+    out = Popen(['instructionsetLevel.exe'], stdout=PIPE).communicate()[0]
+    found = set(l.strip() for l in out.splitlines())
+    # FIXME: these seem not to be reported by Linux
+    found -= set(['sse3', 'avx512f'])
+    assert expected == found, ('expected: {0}, found: {1}'
+                               .format(sorted(expected), sorted(found)))
