a6ddd80a5e1189af387a7d2bb9974600bb66439a
Stan Thompson <alan.thompson@glasgow.ac.uk>
2015-06-02 16:54:41 +0200

    GAUDI-1038: CMAKE/CPACK additions to assist in rpm creation

--- cmake/GaudiProjectConfig.cmake
+++ cmake/GaudiProjectConfig.cmake
@@ -716,12 +716,22 @@ __path__ = [d for d in [os.path.join(d, '${pypack}') for d in sys.path if d]
   endforeach()
   set(CPACK_SYSTEM_NAME ${BINARY_TAG})
 
-  set(CPACK_GENERATOR "RPM")
+  set(CPACK_SOURCE_IGNORE_FILES "/InstallArea/;/build\\\\..*/;/\\\\.svn/;/\\\\.git/;/\\\\.settings/;\\\\..*project;\\\\.gitignore")
+
+  # for the binary
+  set(CPACK_INSTALL_PREFIX "usr/${project}/${version}/InstallArea/${BINARY_TAG}")
+  set(CPACK_INSTALL_SCRIPT "${GaudiProject_DIR}/cpack_install.cmake")
+
+  # for the source
+  set(CPACK_SOURCE_INSTALLED_DIRECTORIES "${CMAKE_SOURCE_DIR};/usr/${project}/${version}")
+
+  # for the RPMs
   set(CPACK_PACKAGE_DEFAULT_LOCATION "/usr")
+  set(CPACK_RPM_PACKAGE_VERSION "${version}")
   set(CPACK_SOURCE_GENERATOR "RPM")
   set(CPACK_SOURCE_RPM "ON")
-
-  set(CPACK_SOURCE_IGNORE_FILES "/InstallArea/;/build\\\\..*/;/\\\\.svn/;/\\\\.git/;/\\\\.settings/;\\\\..*project;\\\\.gitignore")
+  set(CPACK_SOURCE_RPM_PACKAGE_ARCHITECTURE "noarch")
+  set(CPACK_SOURCE_RPM_PACKAGE_NAME "${project}-source")
 
   include(CPack)
 
