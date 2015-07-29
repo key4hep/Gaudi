29250566cdd977b3b27cfd591696dd84d6e8d1c4
Benedikt Hegner <hegner@cern.ch>
2015-07-29 17:52:03 +0200

    remove deprecated headers

--- GaudiUtils/GaudiUtils/Range.h
+++
@@ -1,7 +0,0 @@
-// FIXME: remove obsolete header
-#include "GaudiKernel/Range.h"
-#if WIN32
-#pragma message ( "Warning: obsolete header file, use GaudiKernel/Range.h" )
-#else
-#warning obsolete header file, use GaudiKernel/Range.h
-#endif
