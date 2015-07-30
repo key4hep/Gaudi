key4hep/Gaudi

commit b5d0bbfc1ba43ef6b89639a0347fc9d071b556af

use nullptr and range based for loops

GaudiCommonSvc/src/CounterSvc.cpp
@@ -184,7 +184,7 @@ private:
   // find group/name for the counter:
   inline std::pair<std::string,std::string> _find ( const Counter* c ) const
   {
-    if ( c ) { 
+    if ( c ) {
         for ( const auto& i : m_counts ) {
           auto j = std::find_if( i.second.begin(), i.second.end(),
                                  [&](const NameMap::value_type& k) {
@@ -247,11 +247,11 @@ ICounterSvc::Counters CounterSvc::get ( const std::string& group ) const
 {
   ICounterSvc::Counters result ;
   auto i = m_counts.find  ( group ) ;
-  if (  i != m_counts.end() ) { 
+  if (  i != m_counts.end() ) {
     std::transform( i->second.begin(), i->second.end(),
                     std::back_inserter(result),
-                    [&](const NameMap::value_type& j) { 
-        return CountObject{ j.second, i->first, j.first }; 
+                    [&](const NameMap::value_type& j) {
+        return CountObject{ j.second, i->first, j.first };
     } );
   }
   return result ;
@@ -272,7 +272,7 @@ StatusCode CounterSvc::create
 {
   // try to find existing counter:
   refpCounter = get ( grp , nam ) ;
-  if ( 0 != refpCounter ) { return COUNTER_EXISTS ; }                // RETURN
+  if ( !refpCounter ) { return COUNTER_EXISTS ; }                // RETURN
   // create the new counter
   Counter* newc = new Counter() ;
   refpCounter = newc ;
@@ -300,7 +300,7 @@ CounterSvc::CountObject CounterSvc::create
   const std::string& name  ,
   longlong initial_value   )
 {
-  Counter* p = 0;
+  Counter* p = nullptr;
   StatusCode sc = create ( group, name, initial_value, p ) ;
   if ( sc.isSuccess() && 0 != p ) { return CountObject ( p , group , name ) ; }
   throw std::runtime_error("CounterSvc::Counter('"+group+"::"+name+"') exists already!");
@@ -415,7 +415,7 @@ StatusCode CounterSvc::print( Printout& printer ) const
   MsgStream log ( msgSvc() , name() ) ;
   // Force printing in alphabetical order
   std::map<std::pair<std::string,std::string>, Counter*> sorted_map;
-  for ( const auto& i : m_counts ) for ( const auto&  j : i.second) 
+  for ( const auto& i : m_counts ) for ( const auto&  j : i.second)
       sorted_map[ { i.first, j.first } ] = j.second;
   std::for_each(sorted_map.begin(), sorted_map.end(),
                 conditionalPrint(printer, log));

GaudiAlg/GaudiAlg/GaudiHistos_2DProfFixedBinning.icpp
@@ -158,7 +158,7 @@ AIDA::IProfile2D* GaudiHistos<PBASE>::profile2D
   const unsigned long binsY        ,
   const double        weight       ) const
 {
-  AIDA::IProfile2D * h(0);
+  AIDA::IProfile2D * h = nullptr;
   if ( produceHistos() )
   {
     // retrieve or book the histogram
@@ -189,7 +189,7 @@ AIDA::IProfile2D* GaudiHistos<PBASE>::profile2D
   const unsigned long binsY        ,
   const double        weight       ) const
 {
-  AIDA::IProfile2D * h(0);
+  AIDA::IProfile2D * h = nullptr;
   if ( produceHistos() )
   {
     // retrieve or book the histogram

GaudiExamples/src/IO/WriteAlg.h
@@ -27,7 +27,7 @@ class WriteAlg : public Algorithm {
  public:
   /// Constructor: A constructor of this form must be provided.
   WriteAlg(const std::string& name, ISvcLocator* pSvcLocator)
-  : Algorithm(name, pSvcLocator), m_recordSvc(0), m_evtCount(0) { }
+  : Algorithm(name, pSvcLocator), m_recordSvc(nullptr), m_evtCount(nullptr) { }
   /// Standard Destructor
   virtual ~WriteAlg() { }
   /// Initialize

GaudiMonitor/src/HistorySvc.cpp
@@ -74,10 +74,10 @@ HistorySvc::HistorySvc( const std::string& name, ISvcLocator* svc )
   : base_class( name, svc ),
     m_isInitialized(false),
     m_dump(false),
-    p_algCtxSvc(0),
-    m_jobHistory(0),
+    p_algCtxSvc(nullptr),
+    m_jobHistory(nullptr),
     m_outputFile(""),
-    m_incidentSvc(0),
+    m_incidentSvc(nullptr),
     m_log(msgSvc(), name ),
     m_outputFileTypeXML(false)
 
@@ -235,7 +235,7 @@ StatusCode HistorySvc::captureState() {
   /// Get all the Algorithms
 
   StatusCode sc;
-  IAlgManager* algMgr = 0;
+  IAlgManager* algMgr = nullptr;
   sc = Gaudi::svcLocator()->queryInterface( IAlgManager::interfaceID(),
                                             pp_cast<void>(&algMgr) );
   if (sc.isFailure()) {
@@ -245,7 +245,7 @@ StatusCode HistorySvc::captureState() {
     size_t count = 0;
     for (auto ialg: algMgr->getAlgorithms()) {
       Algorithm* alg = dynamic_cast<Algorithm*>(ialg);
-      if (alg == 0) {
+      if ( !alg ) {
 	m_log << MSG::WARNING << "Algorithm " << ialg->name()
 	      << " does not inherit from Algorithm. Not registering it."
 	      << endmsg;
@@ -376,7 +376,7 @@ HistorySvc::listProperties(const Algorithm &alg) const {
 
   AlgorithmHistory *hist = getAlgHistory( alg );
 
-  if (hist == 0) {
+  if ( !hist ) {
     return StatusCode::FAILURE;
   }
 
@@ -397,7 +397,7 @@ HistorySvc::dumpProperties(const Algorithm &alg, std::ofstream& ofs) const {
 
   AlgorithmHistory *hist = getAlgHistory( alg );
 
-  if (hist == 0) {
+  if ( !hist ) {
     return;
   }
 
@@ -417,7 +417,7 @@ HistorySvc::getAlgHistory(const Algorithm &alg) const {
   if ( itr == m_algs.end() ) {
     m_log << MSG::WARNING << "Algorithm " << alg.name() << " not registered"
           << endmsg;
-    return 0;
+    return nullptr;
   }
 
   map<const Algorithm*, AlgorithmHistory*>::const_iterator itr2;
@@ -548,10 +548,10 @@ HistorySvc::getJobHistory() const {
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 IAlgorithm*
 HistorySvc::getCurrentIAlg() const {
-  if (p_algCtxSvc == 0) {
+  if ( !p_algCtxSvc ) {
     m_log << MSG::WARNING << "trying to create DataHistoryObj before "
 	  << "HistorySvc has been initialized" << endmsg;
-    return 0;
+    return nullptr;
 
   } else {
     return p_algCtxSvc->currentAlg();
@@ -565,22 +565,22 @@ DataHistory*
 HistorySvc::createDataHistoryObj(const CLID& id, const std::string& key,
 				 const std::string& /* storeName */) {
 
-  if (!m_activate) return 0;
+  if (!m_activate) return nullptr;
 
 
-  AlgorithmHistory *algHist;
+  AlgorithmHistory *algHist = nullptr;
 
   IAlgorithm* ialg = getCurrentIAlg();
-  if (ialg == 0) {
+  if ( !ialg ) {
     ON_DEBUG
       m_log << MSG::DEBUG
 	    << "Could not discover current Algorithm:" << endl
 	    << "          object CLID: " << id << "  key: \"" << key
 	    << "\"" << endmsg;
-    algHist = 0;
+    algHist = nullptr;
   } else {
     Algorithm* alg = dynamic_cast<Algorithm*>(ialg);
-    if (alg != 0) {
+    if ( alg ) {
       algHist = getAlgHistory( *alg );
     } else {
       m_log << MSG::WARNING
@@ -588,7 +588,7 @@ HistorySvc::createDataHistoryObj(const CLID& id, const std::string& key,
             << endl
             << "          object CLID: " << id << "  key: \"" << key
             << "\"" << endmsg;
-      algHist = 0;
+      algHist = nullptr;
     }
   }
 
@@ -645,7 +645,7 @@ HistorySvc::getDataHistory(const CLID& id, const std::string& key,
   pair<DHMCitr,DHMCitr> mitr = m_datMap.equal_range(dhh);
 
   if(mitr.first == mitr.second) {
-    return 0;
+    return nullptr;
   }
 
   return mitr.first->second;
@@ -721,7 +721,7 @@ HistorySvc::getServiceHistory(const IService &svc) const {
   if ( itr == m_svcmap.end() ) {
     m_log << MSG::WARNING << "Service " << svc.name() << " not registered"
           << endmsg;
-    return 0;
+    return nullptr;
   }
 
   return ( itr->second );
@@ -743,7 +743,7 @@ HistorySvc::listProperties(const IService &svc) const {
 
   ServiceHistory *hist = getServiceHistory( svc );
 
-  if (hist == 0) {
+  if ( !hist ) {
     return StatusCode::FAILURE;
   }
 
@@ -767,7 +767,7 @@ HistorySvc::dumpProperties(const IService &svc, std::ofstream &ofs) const {
 
   ServiceHistory *hist = getServiceHistory( svc );
 
-  if (hist == 0) {
+  if ( !hist ) {
     return;
   }
 
@@ -783,7 +783,7 @@ StatusCode
 HistorySvc::registerAlgTool(const IAlgTool& ialg) {
 
   if (! m_isInitialized) {
-    if (p_algCtxSvc == 0) {
+    if ( !p_algCtxSvc ) {
       if ( service("AlgContextSvc",p_algCtxSvc,true).isFailure() ) {
 	m_log << MSG::ERROR << "unable to get the AlgContextSvc" << endmsg;
 	return StatusCode::FAILURE;
@@ -795,7 +795,7 @@ HistorySvc::registerAlgTool(const IAlgTool& ialg) {
   }
 
   const AlgTool *alg = dynamic_cast<const AlgTool*>( &ialg );
-  if ( alg == 0 ) {
+  if ( !alg ) {
     m_log << MSG::ERROR << "Could not dcast IAlgTool \"" << ialg.name()
 	  << "\" to an AlgTool" << endmsg;
     return StatusCode::FAILURE;
@@ -834,7 +834,7 @@ HistorySvc::listProperties(const IAlgTool& alg) const {
 
   AlgToolHistory *hist = getAlgToolHistory( alg );
 
-  if (hist == 0) {
+  if ( !hist ) {
     return StatusCode::FAILURE;
   }
 
@@ -855,7 +855,7 @@ HistorySvc::dumpProperties(const IAlgTool& alg, std::ofstream &ofs) const {
 
   AlgToolHistory *hist = getAlgToolHistory( alg );
 
-  if (hist == 0) {
+  if ( !hist ) {
     return;
   }
 
@@ -875,7 +875,7 @@ HistorySvc::getAlgToolHistory(const IAlgTool& alg) const {
   if ( itr == m_algtools.end() ) {
     m_log << MSG::WARNING << "AlgTool " << alg.name() << " not registered"
           << endmsg;
-    return 0;
+    return nullptr;
   }
 
   map<const AlgTool*, AlgToolHistory*>::const_iterator itr2;
@@ -1014,12 +1014,12 @@ HistorySvc::dumpState(std::ofstream& ofs) const {
 void
 HistorySvc::dumpState(const INamedInterface *in, std::ofstream& ofs) const {
 
-  HistoryObj *hist(0);
-  IVersHistoryObj *vhist(0);
+  HistoryObj *hist = nullptr;
+  IVersHistoryObj *vhist = nullptr;
 
-  const IService* is(0);
-  const Algorithm* ia(0);
-  const IAlgTool* it(0);
+  const IService*  is = nullptr;
+  const Algorithm* ia = nullptr;
+  const IAlgTool*  it = nullptr;
   if ( (is=dynamic_cast<const IService*>(in)) != 0) {
     ON_VERBOSE
       m_log << MSG::VERBOSE << in->name() << " is Service" << endmsg;

RootCnv/merge/extractEvt.C
@@ -27,7 +27,7 @@ namespace Gaudi {
   /** @struct RootRef RootRefs.h RootCnv/RootRefs.h
    *
    * Persistent reference object.
-   * 
+   *
    * @author  M.Frank
    * @version 1.0
    */
@@ -37,7 +37,7 @@ namespace Gaudi {
     /// Standard constructor
     RootRef()     {  this->reset(); }
     /// Copy constructor
-    RootRef(const RootRef& c) 
+    RootRef(const RootRef& c)
       : dbase(c.dbase),container(c.container),link(c.link),clid(c.clid),svc(c.svc),entry(c.entry)
     {
     }
@@ -66,7 +66,7 @@ namespace Gaudi {
    *
    * Persistent reference object containing all leafs and links
    * corresponding to a Gaudi DataObject.
-   * 
+   *
    * @author  M.Frank
    * @version 1.0
    */
@@ -75,7 +75,7 @@ namespace Gaudi {
     std::vector<int>       links;
     /// The references corresponding to the next layer of items in the data store
     std::vector<RootRef>   refs;
-    
+
     /// Default constructor
     RootObjectRefs() {}
     /// Copy constructor
@@ -85,7 +85,7 @@ namespace Gaudi {
     /// Assignment operator
     RootObjectRefs& operator=(const RootObjectRefs& r) {
       links = r.links;
-      refs = r.refs; 
+      refs = r.refs;
       return *this;
     }
   };
@@ -100,7 +100,7 @@ namespace Gaudi {
   struct RootNTupleDescriptor {
     /// Description string
     std::string   description;
-    /// Optional description 
+    /// Optional description
     std::string   optional;
     /// Identifier of description
     std::string   container;
@@ -113,9 +113,9 @@ namespace Gaudi {
   };
 
   typedef int ExtractStatus;
-  enum ExtractStatusEnum { 
-    EXTRACT_ERROR=0, 
-    EXTRACT_SUCCESS=1 
+  enum ExtractStatusEnum {
+    EXTRACT_ERROR=0,
+    EXTRACT_SUCCESS=1
   };
 
   struct RootEventExtractor {
@@ -159,7 +159,7 @@ namespace Gaudi {
     /// Add a given entry number to the list of events to be selected to the output file
     ExtractStatus select(int evt_num);
 
-    /// Extract all previously selected events from the input file and write it to the output 
+    /// Extract all previously selected events from the input file and write it to the output
     ExtractStatus extract();
   };
 
@@ -172,7 +172,7 @@ using namespace Gaudi;
 using namespace std;
 
 /// Default constructor
-RootEventExtractor::RootEventExtractor()     
+RootEventExtractor::RootEventExtractor()
   : m_in(0), m_evt_in(0), m_ref_in(0), m_out(0), m_evt_out(0), m_ref_out(0), m_localDB_id(0)
 {
 }
@@ -197,7 +197,7 @@ RootEventExtractor::~RootEventExtractor()    {
 
 /// Clear the list of event numbers to be extracted from the file.
 ExtractStatus RootEventExtractor::cancel() {
-  m_eventList.clear(); 
+  m_eventList.clear();
   ::printf("+++ Event list cleared.\n");
   return EXTRACT_SUCCESS;
 }
@@ -256,7 +256,7 @@ ExtractStatus RootEventExtractor::closeOutput() {
   return EXTRACT_SUCCESS;
 }
 
-/// Extract all previously selected events from the input file and write it to the output 
+/// Extract all previously selected events from the input file and write it to the output
 ExtractStatus RootEventExtractor::extract()   {
   char text[1024];
   bool new_output = false;
@@ -285,7 +285,7 @@ ExtractStatus RootEventExtractor::extract()   {
     br_in->SetAddress(text);
     br_out = m_ref_out->GetBranch("Databases");
     br_out->SetAddress(text);
-    for( int i=0; i<br_in->GetEntries(); ++i ) { 
+    for( int i=0; i<br_in->GetEntries(); ++i ) {
       br_in->GetEntry(i);
       br_out->Fill();
       if ( m_localDB_id<0 && strcmp(text,"<localDB>") == 0 ) {
@@ -297,7 +297,7 @@ ExtractStatus RootEventExtractor::extract()   {
     br_in->SetAddress(text);
     br_out = m_ref_out->GetBranch("Containers");
     br_out->SetAddress(text);
-    for( int i=0; i<br_in->GetEntries(); ++i ) { 
+    for( int i=0; i<br_in->GetEntries(); ++i ) {
       br_in->GetEntry(i);
       br_out->Fill();
     }
@@ -306,7 +306,7 @@ ExtractStatus RootEventExtractor::extract()   {
     br_in->SetAddress(text);
     br_out = m_ref_out->GetBranch("Links");
     br_out->SetAddress(text);
-    for( int i=0; i<br_in->GetEntries(); ++i ) { 
+    for( int i=0; i<br_in->GetEntries(); ++i ) {
       br_in->GetEntry(i);
       br_out->Fill();
     }
@@ -315,10 +315,10 @@ ExtractStatus RootEventExtractor::extract()   {
     br_in->SetAddress(text);
     br_out = m_ref_out->GetBranch("Params");
     br_out->SetAddress(text);
-    for( int i=0; i<br_in->GetEntries(); ++i ) { 
+    for( int i=0; i<br_in->GetEntries(); ++i ) {
       br_in->GetEntry(i);
       if ( strncmp(text,"PFN=",4) == 0 ) {  // Update PFN entry
-	::snprintf(text,sizeof(text),"PFN=%s",br_out->GetFile()->GetName());    
+	::snprintf(text,sizeof(text),"PFN=%s",br_out->GetFile()->GetName());
 	::printf("+++ PFN of the created output file is:%s\n",text);
       }
       else if ( strncmp(text,"FID=",4) == 0 ) {    // Create new FID for new file
@@ -349,8 +349,8 @@ ExtractStatus RootEventExtractor::extract()   {
       return EXTRACT_ERROR;
     }
     int         out_num_entries = br_out->GetEntries();
-    for(std::vector<int>::const_iterator i=m_eventList.begin(); i != m_eventList.end(); ++i) {
-      int   num_evt = *i;
+    for(const auto& i : m_eventList) {
+      int   num_evt = i;
       const char* br_type = "DataObject";
       void* pObject = br_class->New();
       br_in->SetAddress(&pObject);
@@ -362,8 +362,8 @@ ExtractStatus RootEventExtractor::extract()   {
       }
       if ( name.EndsWith("_R.") )   {
 	RootObjectRefs *refs=(RootObjectRefs*)pObject;
-	for(std::vector<RootRef>::iterator ir=refs->refs.begin(); ir!= refs->refs.end(); ++ir)     {
-	  RootRef& r = *ir;
+	for(auto& ir : refs->refs)     {
+	  RootRef& r = ir;
 	  if ( r.dbase == m_localDB_id ) r.entry = out_num_entries;
 	}
 	br_type = "*Index*";
@@ -372,7 +372,7 @@ ExtractStatus RootEventExtractor::extract()   {
       if ( num_wr < 0 ) {
 	::printf("+++ ERROR: Failed to write data to extraction branch:%s "
 		 "read:%d wrote:%d bytes [Length-mismatch]\n",name.Data(),num_rd,num_wr);
-	return EXTRACT_ERROR;	
+	return EXTRACT_ERROR;
       }
       ::printf("+++ Copied %8d bytes to %-10s branch %s(%d)\n",num_rd,br_type,name.Data(),out_num_entries);
       ++out_num_entries;

commit 7c27f7b498941a15ff3596257ce382408e75917c

fix bug introduced in commit b5d0bbfc

GaudiCommonSvc/src/CounterSvc.cpp
@@ -272,7 +272,7 @@ StatusCode CounterSvc::create
 {
   // try to find existing counter:
   refpCounter = get ( grp , nam ) ;
-  if ( !refpCounter ) { return COUNTER_EXISTS ; }                // RETURN
+  if ( refpCounter ) { return COUNTER_EXISTS ; }                // RETURN
   // create the new counter
   Counter* newc = new Counter() ;
   refpCounter = newc ;