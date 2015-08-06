2b8d63aae6115741d0b02b2ae3c238bc9355f38c
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2015-08-06 16:15:53 +0200

    Moderize RegisteryEntry
       - simplify constructor
       - qualify implementations as 'override' instead of 'virtual'

--- GaudiKernel/src/Lib/RegistryEntry.cpp
+++ GaudiKernel/src/Lib/RegistryEntry.cpp
@@ -47,14 +47,9 @@ namespace {
   }
 }
 /// Standard Constructor
-DataSvcHelpers::RegistryEntry::RegistryEntry(std::string path, RegistryEntry* parent)
-: m_refCount(0),
-  m_isSoft(false),
+DataSvcHelpers::RegistryEntry::RegistryEntry(std::string path, RegistryEntry* parent) :
   m_path(std::move(path)),
-  m_pParent(parent),
-  m_pAddress(nullptr),
-  m_pObject(nullptr),
-  m_pDataProviderSvc(nullptr)
+  m_pParent(parent)
 {
   std::string::size_type sep = m_path.rfind(SEPARATOR);
   if ( m_path.front() != SEPARATOR ) m_path.insert(0,1,SEPARATOR);
@@ -86,7 +81,7 @@ unsigned long DataSvcHelpers::RegistryEntry::release()  {
 /// Set new parent pointer
 void DataSvcHelpers::RegistryEntry::setParent(RegistryEntry* pParent)    {
   m_pParent = pParent;
-  m_fullpath = "";
+  m_fullpath.clear();
   assemblePath(m_fullpath);
 }
 
