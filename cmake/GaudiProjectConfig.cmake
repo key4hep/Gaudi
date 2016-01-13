2c9ee167f9be84ae42541fe08799773938bb3944
Attila Krasznahorkay <krasznaa@pb-d-128-141-37-81.cern.ch>
2016-01-13 15:23:36 +0100

    ATLAS CPack changes
    Changes necessary to build (seemingly) correct binary RPMs for ATLAS

--- cmake/GaudiProjectConfig.cmake
+++ cmake/GaudiProjectConfig.cmake
@@ -716,32 +716,34 @@ __path__ = [d for d in [os.path.join(d, '${pypack}') for d in sys.path if d]
   gaudi_generate_project_manifest(${CMAKE_CONFIG_OUTPUT_DIRECTORY}/manifest.xml ${ARGV})
   install(FILES ${CMAKE_CONFIG_OUTPUT_DIRECTORY}/manifest.xml DESTINATION .)
 
-  #--- CPack configuration
-  set(CPACK_PACKAGE_NAME ${project})
-  foreach(t MAJOR MINOR PATCH)
-    set(CPACK_PACKAGE_VERSION_${t} ${CMAKE_PROJECT_VERSION_${t}})
-  endforeach()
-  set(CPACK_SYSTEM_NAME ${BINARY_TAG})
+  if( NOT GAUDI_ATLAS )
+    #--- CPack configuration
+    set(CPACK_PACKAGE_NAME ${project})
+    foreach(t MAJOR MINOR PATCH)
+      set(CPACK_PACKAGE_VERSION_${t} ${CMAKE_PROJECT_VERSION_${t}})
+    endforeach()
+    set(CPACK_SYSTEM_NAME ${BINARY_TAG})
 
-  set(CPACK_SOURCE_IGNORE_FILES "/InstallArea/;/build\\\\..*/;/\\\\.svn/;/\\\\.git/;/\\\\.settings/;\\\\..*project;\\\\.gitignore")
+    set(CPACK_SOURCE_IGNORE_FILES "/InstallArea/;/build\\\\..*/;/\\\\.svn/;/\\\\.git/;/\\\\.settings/;\\\\..*project;\\\\.gitignore")
 
-  # for the binary
-  set(CPACK_INSTALL_PREFIX "usr/${project}/${version}/InstallArea/${BINARY_TAG}")
-  set(CPACK_INSTALL_SCRIPT "${GaudiProject_DIR}/cpack_install.cmake")
+    # for the binary
+    set(CPACK_INSTALL_PREFIX "usr/${project}/${version}/InstallArea/${BINARY_TAG}")
+    set(CPACK_INSTALL_SCRIPT "${GaudiProject_DIR}/cpack_install.cmake")
 
-  # for the source
-  set(CPACK_SOURCE_INSTALLED_DIRECTORIES "${CMAKE_SOURCE_DIR};/usr/${project}/${version}")
+    # for the source
+    set(CPACK_SOURCE_INSTALLED_DIRECTORIES "${CMAKE_SOURCE_DIR};/usr/${project}/${version}")
 
-  # for the RPMs
-  set(CPACK_PACKAGE_DEFAULT_LOCATION "/usr")
-  set(CPACK_GENERATOR "RPM")
-  set(CPACK_RPM_PACKAGE_VERSION "${version}")
-  set(CPACK_SOURCE_GENERATOR "RPM")
-  set(CPACK_SOURCE_RPM "ON")
-  set(CPACK_SOURCE_RPM_PACKAGE_ARCHITECTURE "noarch")
-  set(CPACK_SOURCE_RPM_PACKAGE_NAME "${project}-source")
+    # for the RPMs
+    set(CPACK_PACKAGE_DEFAULT_LOCATION "/usr")
+    set(CPACK_GENERATOR "RPM")
+    set(CPACK_RPM_PACKAGE_VERSION "${version}")
+    set(CPACK_SOURCE_GENERATOR "RPM")
+    set(CPACK_SOURCE_RPM "ON")
+    set(CPACK_SOURCE_RPM_PACKAGE_ARCHITECTURE "noarch")
+    set(CPACK_SOURCE_RPM_PACKAGE_NAME "${project}-source")
 
-  include(CPack)
+    include(CPack)
+  endif()
 
 endmacro()
 
