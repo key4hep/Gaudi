2b8b7ef875d6cefc932b0a035e36a18ad65ea298
Charles Leggett <leggett@cern.ch>
2016-01-15 23:45:54 +0100

    uncomment critical bits of ThreadInitTask

--- GaudiHive/src/ThreadInitTask.cpp
+++ GaudiHive/src/ThreadInitTask.cpp
@@ -19,54 +19,54 @@ ThreadInitTask::execute() {
       << " at " << this
       << std::dec << endmsg;
 
-  // if (m_tools.retrieve().isFailure()) {
-  //   log << MSG::ERROR << "unable to retrieve ToolHandleArray " << m_tools
-  // 	<< endmsg;
-  //   m_execFailed = true;
-  // } else {
+  if (m_tools.retrieve().isFailure()) {
+    log << MSG::ERROR << "unable to retrieve ToolHandleArray " << m_tools
+	<< endmsg;
+    m_execFailed = true;
+  } else {
 
-  //   if (m_tools.size() == 0) {
-  //     log << MSG::INFO << "no entries in Tool Array"
-  // 	<< endmsg;
-  //   } else {
+    if (m_tools.size() == 0) {
+      log << MSG::INFO << "no entries in Tool Array"
+	<< endmsg;
+    } else {
 
-  //     log << MSG::DEBUG << "executing in thread 0x" << std::hex << pthread_self()
-  // 	  << std::dec << endmsg;      
+      log << MSG::DEBUG << "executing in thread 0x" << std::hex << pthread_self()
+	  << std::dec << endmsg;      
 
-  //   for ( auto t : m_tools ) {
+    for ( auto t : m_tools ) {
 
-  // 	try {
+	try {
     
-  // 	log << MSG::DEBUG << "calling IThreadInitTool " << t << endmsg;
-  // 	if (!m_terminate) {
-  // 	  t->initThread();
-  //     } else {
-  // 	  t->terminateThread();
-  //     }
+	log << MSG::DEBUG << "calling IThreadInitTool " << t << endmsg;
+	if (!m_terminate) {
+	  t->initThread();
+      } else {
+	  t->terminateThread();
+      }
 
-  // 	} catch( const GaudiException& exc ) {
-  // 	  log << MSG::ERROR << "ThreadInitTool " << t << " in thread 0x"
-  // 	      << std::hex << pthread_self() << std::dec << " threw GaudiException: " 
-  // 	      << exc << endmsg;
-  // 	  m_execFailed = true;
-  // 	} catch (const std::exception& exc ) {
-  // 	  log << MSG::ERROR << "ThreadInitTool " << t << " in thread 0x"
-  // 	      << std::hex << pthread_self() << std::dec << " threw std::exception: " 
-  // 	      << exc.what() << endmsg;
-  // 	  m_execFailed = true;
-  // 	} catch(...) {
-  // 	  log << MSG::ERROR << "ThreadInitTool " << t << " in thread 0x"
-  // 	      << std::hex << pthread_self() << std::dec << " threw unknown exception" 
-  // 	      << endmsg;
-  // 	  m_execFailed = true;
-  // 	}
+	} catch( const GaudiException& exc ) {
+	  log << MSG::ERROR << "ThreadInitTool " << t << " in thread 0x"
+	      << std::hex << pthread_self() << std::dec << " threw GaudiException: " 
+	      << exc << endmsg;
+	  m_execFailed = true;
+	} catch (const std::exception& exc ) {
+	  log << MSG::ERROR << "ThreadInitTool " << t << " in thread 0x"
+	      << std::hex << pthread_self() << std::dec << " threw std::exception: " 
+	      << exc.what() << endmsg;
+	  m_execFailed = true;
+	} catch(...) {
+	  log << MSG::ERROR << "ThreadInitTool " << t << " in thread 0x"
+	      << std::hex << pthread_self() << std::dec << " threw unknown exception" 
+	      << endmsg;
+	  m_execFailed = true;
+	}
 
-  //   }
+    }
 
-  //     m_tools.release().ignore();            
+      m_tools.release().ignore();            
     
-  //   }
-  // }
+    }
+  }
 
 
   if (m_barrier != 0) {
