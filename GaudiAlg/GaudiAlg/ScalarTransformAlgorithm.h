66d68bab5bb272619dc3c57a888111b21a40cc30
Gerhard Raven <Gerhard.Raven@nikhef.nl>
2016-05-20 18:20:53 +0200

    fix insert adapter to properly distinguish between std::vector::push_back and KeyedContainer::insert

--- GaudiAlg/GaudiAlg/ScalarTransformAlgorithm.h
+++ GaudiAlg/GaudiAlg/ScalarTransformAlgorithm.h
@@ -10,6 +10,9 @@ template <typename ExternalSignature, typename ScalarFunctor> class ScalarTransf
 
 namespace detail {
 
+   // we want 'user code' to never get pointers as input -- so adapt such that
+   // if we get a pointer, we derefence it before calling the 'user code'.
+
    template <typename In, typename Fun>
    auto adapt_in( const Fun& f, const In& in,
                   typename std::enable_if< !std::is_pointer<In>::value>::type* = nullptr )
@@ -18,6 +21,9 @@ namespace detail {
    template <typename In, typename Fun>
    auto adapt_in( const Fun& f, const In* in ) { return f(*in); }
 
+   // we always receive a T, but sometimes we must output a T* -- so adapt if neccessary...
+   // (backwards compatibility: until we get a 'move aware' AnyDataHandle)
+
    template <typename Out>
    Out adapt_out( Out&& in ) { return std::forward<Out>(in); }
 
@@ -25,11 +31,24 @@ namespace detail {
    Out adapt_out( typename std::remove_pointer<Out>::type&& in )
    { return new typename std::remove_pointer<Out>::type( std::forward<typename std::remove_pointer<Out>::type>(in) ); }
 
-   template <typename Container, typename Value>
-   void adapt_insert(Container& c, Value&& v) { return c.push_back( std::forward<Value>(v) ); }
+   // adapt to differences between eg. std::vector (which has push_back) and KeyedContainer (which has insert)
 
-   template <typename Value>
-   void adapt_insert(KeyedContainer<Value>& c, Value&& v) { return c.insert( std::forward<Value>(v) ); }
+   template <typename> struct void_t { typedef void type; };
+   template <typename T> using is_valid_t = typename void_t<T>::type;
+
+   template <typename T, typename SFINAE = void> struct has_push_back : std::false_type {};
+   template <typename T>                         struct has_push_back<T, is_valid_t<decltype(std::declval<T>().push_back( std::declval<typename T::value_type>() ))>> : std::true_type {};
+
+   template <typename T, typename SFINAE = void> struct has_insert : std::false_type {};
+   template <typename T>                         struct has_insert<T, is_valid_t<decltype(std::declval<T>().insert( std::declval<typename T::value_type>() ))>> : std::true_type {};
+
+   template <typename Container, typename Value>
+   typename std::enable_if< has_push_back<Container>::value, void >::type
+   adapt_insert(Container& c, Value&& v) { c.push_back( std::forward<Value>(v) ); }
+
+   template <typename Container, typename Value>
+   typename std::enable_if< has_insert<Container>::value, void >::type
+   adapt_insert(Container& c, Value&& v) { c.insert( std::forward<Value>(v) ); }
 }
 
 template <typename Out, typename In, typename ScalarFunctor>
