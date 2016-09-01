9b68c11801b3626a4a212bfdf7e4efcc3360fd79
Charles Leggett <leggett@cern.ch>
2016-09-01 20:42:06 +0200

    Since zero can be a valid number for various EventID related parameters, eg run# or event#, we need invalid defaults to be able to distinguish between EventIDs that are set with lumiblock info, vs run# info

--- GaudiKernel/src/Lib/EventIDBase.cpp
+++ GaudiKernel/src/Lib/EventIDBase.cpp
@@ -9,8 +9,15 @@
 
 #include "GaudiKernel/EventIDBase.h"
 
+#include <limits>
+
+const EventIDBase::number_type EventIDBase::UNDEFNUM = 
+  std::numeric_limits<EventIDBase::number_type>::max();
+const EventIDBase::event_number_t EventIDBase::UNDEFEVT = 
+  std::numeric_limits<EventIDBase::event_number_t>::max();
+
 EventIDBase::EventIDBase(number_type run_number, 
-                         uint64_t event_number,
+                         event_number_t event_number,
                          number_type time_stamp,
                          number_type time_stamp_ns_offset,
                          number_type lumi_block,
@@ -22,22 +29,43 @@ EventIDBase::EventIDBase(number_type run_number,
   m_time_stamp_ns_offset (time_stamp_ns_offset),
   m_lumiBlock            (lumi_block),
   m_bunch_crossing_id    (bunch_crossing_id)
-{}
+{
+  if (m_run_number != UNDEFNUM && m_event_number != UNDEFEVT) {
+    setRE();
+  }
+
+  if (m_time_stamp != UNDEFNUM) {
+    setTS();
+    if (m_time_stamp_ns_offset == UNDEFNUM) {
+      m_time_stamp_ns_offset = 0;
+    }
+  }
+
+  if (m_lumiBlock != UNDEFNUM && m_event_number != UNDEFEVT) {
+    setLE();
+  }
+
+}
 
 EventIDBase::~EventIDBase()
 {}
 
 bool
 EventIDBase::isRunEvent() const {
-  return (m_run_number + m_event_number != 0);
+  return ( m_type & RunEvent );
 }
 
 bool
 EventIDBase::isTimeStamp() const {
-  return (m_time_stamp + m_time_stamp_ns_offset != 0);
+  return ( m_type & TimeStamp );
 }
 
 bool
 EventIDBase::isLumiEvent() const {
-  return (m_lumiBlock != 0);
+  return ( m_type & LumiEvent );
+}
+
+bool
+EventIDBase::isValid() const {
+  return ( m_type != Invalid );
 }
