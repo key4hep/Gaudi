5dfb53e00895f00fe419825ed8038e378b541d49
Marco Clemencic <marco.clemencic@cern.ch>
2016-09-09 12:01:59 +0200

    removed workaround not needed after 26506b5a

--- GaudiExamples/tests/qmtest/ExtPropValidator.py
+++ GaudiExamples/tests/qmtest/ExtPropValidator.py
@@ -42,16 +42,7 @@ def validate(stdout,stderr,result,causes):
            'GaudiUtils::Map<string,string,map<string,string> >': {'a': '1', 'b': '2'}
         }
 
-    # Hack for win32: 'string' become 'basic_string...', so I have to
-    #                 modify the keys of the dictionary
-    import os
     import re
-
-    if re.search(r'winxp|gcc6', os.environ.get('CMTCONFIG', '')):
-        for k in expected.keys():
-            if 'string' in k:
-                expected[k.replace('string','basic_string<char,char_traits<char> >').replace('>>','> >')] = expected[k]
-                del expected[k]
 
     signature = re.compile(r'xProps\s*SUCCESS\s*My Properties :')
     prop_sign = re.compile(r'''xProps\s*SUCCESS\s*'([^']*)':(.*)''')
