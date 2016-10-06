6b004118f17b0f88691456dfb5eeceafd72505a4
Illya Shapoval <Illya.Shapoval@cern.ch>
2016-10-06 18:37:52 +0200

    adding ATLAS MC reconstruction scenario to GaudiHive portfolio of execution flow simulators

---
+++ GaudiHive/options/AtlasMCRecoScenario.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env gaudirun.py
+
+from Gaudi.Configuration import *
+from Configurables import HiveWhiteBoard, HiveSlimEventLoopMgr, ForwardSchedulerSvc, AlgResourcePool
+
+from GaudiHive.precedence import precedence  # custom precedence rules, composed of CPUCrunchers
+
+# metaconfig
+evtslots = 1
+evtMax = 3
+algosInFlight = 8
+
+
+InertMessageSvc(OutputLevel=INFO)
+
+whiteboard   = HiveWhiteBoard("EventDataSvc",
+                              EventSlots = evtslots,
+                              OutputLevel = INFO)
+
+slimeventloopmgr = HiveSlimEventLoopMgr(OutputLevel=DEBUG)
+
+scheduler = ForwardSchedulerSvc(MaxEventsInFlight = evtslots,
+                                MaxAlgosInFlight = algosInFlight,
+                                ThreadPoolSize = algosInFlight,
+                                OutputLevel = DEBUG,
+                                useGraphFlowManagement = True,
+                                DataFlowManagerNext = True,
+                                #Optimizer = "DRE",
+                                PreemptiveIOBoundTasks = False,
+                                DumpIntraEventDynamics = False)
+
+AlgResourcePool(OutputLevel = DEBUG)
+
+#timeValue = precedence.UniformTimeValue(algoAvgTime=0.2)
+timeValue = precedence.RealTimeValue(path = "atlas/mcreco/averageTiming.mcreco.TriggerOff.json",
+                                     defaultTime = 0.0)
+ifIObound = precedence.UniformBooleanValue(False)
+#ifIObound = precedence.RndBiased10BooleanValue()
+
+sequencer = precedence.CruncherSequence(timeValue, ifIObound, sleepFraction=0.0,
+                                        cfgPath = "atlas/mcreco/cf.mcreco.TriggerOff.graphml",
+                                        dfgPath = "atlas/mcreco/df.mcreco.TriggerOff.3rdEvent.graphml",
+                                        topSequencer = 'AthSequencer/AthMasterSeq',
+                                        showStat=True).get()
+
+ApplicationMgr( EvtMax = evtMax,
+                EvtSel = 'NONE',
+                ExtSvc = [whiteboard],
+                EventLoop = slimeventloopmgr,
+                TopAlg = [sequencer],
+                MessageSvcType = "InertMessageSvc",
+                OutputLevel = DEBUG)
