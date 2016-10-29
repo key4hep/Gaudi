dbf7b3e7006922069780531ebcfd5ced12607865
Chitsanu Khurewathanakul <chitsanu.khurewathanakul@cern.ch>
2016-10-29 18:47:51 +0200

    Equality check for DataObjectHandleBase.py

--- GaudiKernel/python/GaudiKernel/DataObjectHandleBase.py
+++ GaudiKernel/python/GaudiKernel/DataObjectHandleBase.py
@@ -5,16 +5,30 @@ __doc__ = """The python module holding python bindings to DataObjectHandle"""
 
 class DataObjectHandleBase(object):
     
-    __slots__ = ('Path' )
+    __slots__ = ('Path', )
 
     #define accessTypes
     
-    def __init__(self, *args):
+    def __init__(self, path):
         object.__init__(self)
-        
-        if len(args) != 1: raise RuntimeError("must have one argument!")
-        self.Path = args[0]
-            
+        self.Path = path
+
+    def __eq__(self, other):
+        """
+        Need especially Configurable.isPropertySet when checked against default.
+        """
+        if isinstance(other, DataObjectHandleBase):
+            return self.Path == other.Path
+        if isinstance(other, basestring):
+            return self.Path == other
+        raise ValueError('Unknown equality check: type=%r, repr=%r'%(type(other), other))
+
+    def __ne__(self, other):
+        """
+        This is mandatory if __eq__ is defined.
+        """
+        return not self==other
+
     def __str__(self):
         return self.Path
                
