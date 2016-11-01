51e2e97c0e3335fa736ccb4329d1272104b8c020
Sebastien Ponce <sebastien.ponce@cern.ch>
2016-11-01 10:01:27 +0100

    Replaced typedefs by using statement

--- GaudiKernel/GaudiKernel/GetDataHelpers.h
+++ GaudiKernel/GaudiKernel/GetDataHelpers.h
@@ -22,27 +22,27 @@ namespace Gaudi
      */
     template <class TYPE>
     struct _GetType
-    { typedef TYPE* return_type ; };
+    { using return_type = TYPE* ; };
     // ========================================================================
     /// the template specialization for pointers
     template <class TYPE>
     struct _GetType<TYPE*>
-    { typedef TYPE* return_type ; };
+    { using return_type = TYPE* ; };
     // ========================================================================
     /// the template specialization for references
     template <class TYPE>
     struct _GetType<TYPE&>
-    { typedef TYPE* return_type ; };
+    { using return_type = TYPE* ; };
     // ========================================================================
     /// the template specialization for "ranges"
     template <class CONTAINER>
     struct _GetType<Gaudi::Range_<CONTAINER> >
-    { typedef Gaudi::Range_<CONTAINER>      return_type ; };
+    { using return_type = Gaudi::Range_<CONTAINER>; };
     // ========================================================================
     /// the template specialization for "named ranges"
     template <class CONTAINER>
     struct _GetType<Gaudi::NamedRange_<CONTAINER> >
-    { typedef Gaudi::NamedRange_<CONTAINER> return_type ; };
+    { using return_type = Gaudi::NamedRange_<CONTAINER> ; };
     // ========================================================================
 
 
@@ -57,9 +57,9 @@ namespace Gaudi
     template <class TYPE>
     class GetData {
     public:
-      typedef TYPE                                   Type        ;
+      using Type = TYPE;
       /// the actual return type
-      typedef typename _GetType<Type>::return_type   return_type ;
+      using return_type = typename _GetType<Type>::return_type;
     public:
       /** the only one essential method
        *  @param base  ref to the Handle base to retrieve the data from
@@ -92,12 +92,12 @@ namespace Gaudi
     template <class TYPE, typename return_type>
     inline return_type getData_operator(DataObject *obj) {
       /// the actual types in the TES
-      typedef typename std::conditional<std::is_base_of<DataObject,typename TYPE::Selection>::value,
-                                        typename TYPE::Selection,
-                                        AnyDataWrapper<typename TYPE::Selection>>::type selection_type;
-      typedef typename std::conditional<std::is_base_of<DataObject,typename TYPE::Container>::value,
-                                        typename TYPE::Container,
-                                        AnyDataWrapper<typename TYPE::Container>>::type container_type;
+      using selection_type = typename std::conditional<std::is_base_of<DataObject,typename TYPE::Selection>::value,
+                                                       typename TYPE::Selection,
+                                                       AnyDataWrapper<typename TYPE::Selection>>::type;
+      using container_type = typename std::conditional<std::is_base_of<DataObject,typename TYPE::Container>::value,
+                                                       typename TYPE::Container,
+                                                       AnyDataWrapper<typename TYPE::Container>>::type;
       const auto tmp = dynamic_cast<selection_type*>(obj);
       if (tmp) {
         auto& selection = unwrap_data<typename TYPE::Selection, selection_type>(tmp);
@@ -114,10 +114,10 @@ namespace Gaudi
 
     /// the template specialization for named ranges
     template <class TYPE>
-    struct GetData<Gaudi::NamedRange_<std::vector<const TYPE*> > > {
+    struct GetData<Gaudi::NamedRange_<std::vector<const TYPE*>>> {
       /// the actual return type
-      typedef Gaudi::NamedRange_<std::vector<const TYPE*> > Type;
-      typedef typename _GetType<Type>::return_type return_type ;
+      using Type = Gaudi::NamedRange_<std::vector<const TYPE*>>;
+      using return_type = typename _GetType<Type>::return_type;
       ///       *  @param DataObjectHandleBase base
       inline return_type operator()(DataObject *obj) const {
         return getData_operator<TYPE, return_type>(obj);
@@ -128,8 +128,8 @@ namespace Gaudi
     template <class TYPE>
     struct GetData<Gaudi::Range_<std::vector<const TYPE*> > > {
       /// the actual return type
-      typedef Gaudi::Range_<std::vector<const TYPE*> > Type;
-      typedef typename _GetType<Type>::return_type return_type ;
+      using Type = Gaudi::Range_<std::vector<const TYPE*>>;
+      using return_type = typename _GetType<Type>::return_type;
       ///       *  @param DataObjectHandleBase base
       inline return_type operator()(DataObject *obj) const {
         return getData_operator<TYPE, return_type>(obj);
