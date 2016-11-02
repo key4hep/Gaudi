2baf2f86f9c3041d6559009315dcd38a0cffecbb
Marco Clemencic <marco.clemencic@cern.ch>
2016-11-02 18:02:40 +0100

    use __CLING__ macro instead of custom __cling__

--- GaudiGSL/GaudiMath/GSLFunAdapters.h
+++ GaudiGSL/GaudiMath/GSLFunAdapters.h
@@ -22,7 +22,7 @@
 // ============================================================================
 #include "GaudiKernel/Kernel.h"
 
-#if defined(__clang__) || defined(__cling__)
+#if defined(__clang__) || defined(__CLING__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Winconsistent-missing-override"
 #elif defined(__GNUC__) && __GNUC__ >= 5
@@ -164,7 +164,7 @@ namespace Genfun
   } // end of namespace GaudiMath Implemnentation
 } // end of namespace Genfun
 
-#if defined(__clang__) || defined(__cling__)
+#if defined(__clang__) || defined(__CLING__)
 #pragma clang diagnostic pop
 #elif defined(__GNUC__) && __GNUC__ >= 5
 #pragma GCC diagnostic pop
