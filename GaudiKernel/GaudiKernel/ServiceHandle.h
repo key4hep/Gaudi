3b813d040c4852cf4f35a2ddb170c952eab0f155
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2016-11-10 09:41:52 +0000

    Add   T * operator->() const for ServiceHandle

--- GaudiKernel/GaudiKernel/ServiceHandle.h
+++ GaudiKernel/GaudiKernel/ServiceHandle.h
@@ -73,7 +73,10 @@ public:
 //   }
 
   /// Allow non const access to the service, even from a const handle...
-  T * get() const { return this->nonConst( get() ); }
+  T * get() const { return this->nonConst( GaudiHandle<T>::get() ); }
+
+  /// Allow non const access to the service, even from a const handle...
+  T * operator->() const { return this->nonConst( GaudiHandle<T>::operator->() ); }
 
 protected:
  /** Do the real retrieval of the Service. */
