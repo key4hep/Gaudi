3c4f32e9ff6d75d9bbac3ce9341956cfd0b7549c
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2016-11-26 22:23:58 +0100

    Extracted the Gaudi::createGuidAsString function into a separate source file. To make it possible to build MultiFileCatalog without building XMLFileCatalog.

---
+++ GaudiUtils/src/component/createGuidAsString.h
@@ -0,0 +1,15 @@
+// Dear emacs, this is -*- c++ -*-
+#ifndef GAUDIUTILS_CREATEGUIDASSTRING_H
+#define GAUDIUTILS_CREATEGUIDASSTRING_H
+
+// System include(s):
+#include <string>
+
+namespace Gaudi {
+
+   /// Helper function creating file identifier using the UUID mechanism
+   std::string createGuidAsString();
+        
+} // namespace Gaudi
+
+#endif // GAUDIUTILS_CREATEGUIDASSTRING_H
