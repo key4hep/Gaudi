f8841911b472d97971932d1d7142b393eee6972f
Charles Leggett <leggett@cern.ch>
2017-01-05 23:30:26 +0100

    Fix DataHandleHolderVisitor
    When adding the extra input and output deps to an Algorithm's
    dependency tree, this was converting the DataObjID to a string,
    then back to a DataObjID, which has the effect of stripping the
    separation of ('ClassID','key') into 'ClassID/key', which screws
    up the ATLAS syntax.
    
    Also, it's looping twice over the input/outputDataObjs(), adding
    the contents back to itself, which seem redundant, unless I'm
    missing something.
    
    Also, the use of tuples is needlessly complicated, making the code
    much more difficult to comprehend. I don't think we're saving any
    CPU cycles this way (in fact I'm betting we're loosing a bunch in
    the creation of the tuple), and instead requiring an extra 15 lines
    of comments to explain what it's doing! The simple loops in the
    original version made the code  much, much more readable.

--- GaudiKernel/src/Lib/DataHandleHolderVisitor.cpp
+++ GaudiKernel/src/Lib/DataHandleHolderVisitor.cpp
@@ -44,14 +44,15 @@ void DHHVisitor::visit(const IDataHandleHolder* v) {
    // objects, so we need another loop here.
    // NOTE: perhaps a view from range v3 can be used to also avoid this second loop.
    for (auto& hs : {make_tuple(v->extraInputDeps(), std::ref(m_ido), std::ref(m_ign_i)),
-                    make_tuple(v->extraOutputDeps(), std::ref(m_odo), std::ref(m_ign_o)),
-                    make_tuple(v->inputDataObjs(), std::ref(m_ido), std::ref(m_ign_i)),
-                    make_tuple(v->outputDataObjs(), std::ref(m_odo), std::ref(m_ign_o))}) {            
+                    make_tuple(v->extraOutputDeps(), std::ref(m_odo), std::ref(m_ign_o))
+                    // make_tuple(v->inputDataObjs(), std::ref(m_ido), std::ref(m_ign_i)),
+                    // make_tuple(v->outputDataObjs(), std::ref(m_odo), std::ref(m_ign_o))
+         }) {
       for (const auto& h : std::get<0>(hs)) {
          if (! h.key().empty()) { 
-            std::get<1>(hs).emplace(h.fullKey());
+            std::get<1>(hs).emplace(h);
          } else {
-            std::get<2>(hs).emplace(h.fullKey());
+            std::get<2>(hs).emplace(h);
          }
       }
    }
