105f78986ad3af40a6eadba51fa6073d6f353911
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2017-01-20 15:15:39 +0000

    resolve conflicts with upstream master

--- GaudiAlg/GaudiAlg/IGenericTool.h
+++ GaudiAlg/GaudiAlg/IGenericTool.h
@@ -18,25 +18,4 @@ public:
   virtual void execute() = 0;
 
 };
-
-/** General purpose interface class for tools that "do something"
- *  in a thread safe way.
- *
- *  @author Marco Clemencic
- *  @date   2016-12-09
- */
-class GAUDI_API IGenericThreadSafeTool: virtual public IGenericTool {
-public:
-  /// InterfaceID
-  DeclareInterfaceID(IGenericThreadSafeTool, 1, 0);
-
-  /// Do the action (thread safe).
-  virtual void execute() const = 0;
-
-  /// Allow use of IGenericThreadSafeTool as IGenericTool.
-  void execute() override final {
-    const_cast<const IGenericThreadSafeTool*>(this)->execute();
-  }
-};
-
 #endif // KERNEL_INORMALIZETOOL_H
