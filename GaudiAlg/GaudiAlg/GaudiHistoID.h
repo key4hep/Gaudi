bb81c84620d706577e67b43e5432ec25703d00b0
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2017-02-02 19:24:31 +0000

    Optimisation of GaudiAlg:ID class

--- GaudiAlg/GaudiAlg/GaudiHistoID.h
+++ GaudiAlg/GaudiAlg/GaudiHistoID.h
@@ -53,23 +53,33 @@ namespace GaudiAlg
   public:
     // ========================================================================
     /// Implicit constructor from a numeric ID
-    ID  ( const NumericID    id = -1 ) ; 
+    ID  ( const NumericID    id = -1 ) 
+      : m_nID  ( id ) 
+      , m_hash ( boost::hash_value ( id    ) ) { }
+    /// Implicit 'move' constructor from a literal ID
+    ID  (       LiteralID&&  id      ) 
+      : m_aID  ( std::move(id) ) 
+      , m_hash ( boost::hash_value ( m_aID ) ) { }
+    /// Implicit 'copy' constructor from a literal ID
+    ID  ( const LiteralID&   id      ) 
+      : m_aID  ( id ) 
+      , m_hash ( boost::hash_value ( m_aID ) ) { }
     /// Implicit constructor from a literal ID
-    ID  ( const LiteralID&   id      ) ; 
-    /// Implicit constructor from a literal ID
-    ID  ( const char*        id      ) ; 
+    ID  ( const char*        id      ) 
+      : m_aID  ( id ) 
+      , m_hash ( boost::hash_value ( m_aID ) ) { }
     /// Destructor
     ~ID ( ) = default;
     /// Is this ID numeric
-    inline bool numeric   () const { return -1 != m_nID ;    }
+    inline bool numeric   () const noexcept { return -1 != m_nID ;    }
     /// Is this ID numeric
-    inline bool literal   () const { return !m_aID.empty() ; }
+    inline bool literal   () const noexcept { return !m_aID.empty() ; }
     /// Is this ID undefined
-    inline bool undefined () const { return !numeric() && !literal(); }
+    inline bool undefined () const noexcept { return !numeric() && !literal(); }
     /// Returns the ID as a LiteralID
-    inline const LiteralID& literalID() const { return m_aID; }
+    inline const LiteralID& literalID() const noexcept { return m_aID; }
     /// Returns the numerical ID
-    inline NumericID        numericID() const { return m_nID; }
+    inline NumericID        numericID() const noexcept { return m_nID; }
     /// Return ID as string, for both numeric and literal IDs
     GAUDI_API LiteralID idAsString() const ;
     /// cast operator to std::string
@@ -78,7 +88,7 @@ namespace GaudiAlg
      *  Implementation depends on type of ID
      *  @return boolean indicating if the IDs are equal
      */
-    inline bool operator==( const ID& id ) const
+    inline bool operator==( const ID& id ) const noexcept
     {
       return 
         hash    () != id.hash    () ? false                           :  
@@ -92,7 +102,7 @@ namespace GaudiAlg
      *  Implementation depends on type of ID
      *  @return boolean indicating the order of the IDs
      */
-    inline bool operator<( const ID& id ) const
+    inline bool operator<( const ID& id ) const noexcept
     {
       return
         //hash () < id.hash () ? true  :
@@ -107,26 +117,26 @@ namespace GaudiAlg
   public:
     // ========================================================================
     /// good ID?
-    bool operator!() const { return undefined() ; }
+    bool operator!() const noexcept { return undefined() ; }
     // ========================================================================
   public:
     // ========================================================================
     /// return hash value (for python)
-    inline size_t   hash   () const { return m_hash    ; }
+    inline size_t   hash   () const noexcept { return m_hash    ; }
     /// return hash value (for python)
-    inline size_t __hash__ () const { return   hash () ; }
+    inline size_t __hash__ () const noexcept { return   hash () ; }
     // ========================================================================
   private:
     // ========================================================================
     /// Internal numeric ID
-    NumericID   m_nID ;                            //       Internal numeric ID
+    NumericID   m_nID{-1} ;                        //       Internal numeric ID
     /// Internal alpha-numeric ID
     LiteralID   m_aID ;                            // Internal alpha-numeric ID
     // ========================================================================
   private:
     // ========================================================================
     /// the hash value of ID
-    size_t m_hash  ;                                    // the hash value of ID
+    size_t m_hash{0}  ;                                 // the hash value of ID
     // ========================================================================
   };
   // ==========================================================================
