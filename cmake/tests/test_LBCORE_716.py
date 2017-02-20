60cf27ca76bf340764727a7e3f3f4bc3fa95bbb8
Marco Clemencic <marco.clemencic@cern.ch>
2017-02-20 12:47:13 +0100

    make sure BINARY_TAG is set in test_LBCORE_716

--- cmake/tests/test_LBCORE_716.py
+++ cmake/tests/test_LBCORE_716.py
@@ -18,10 +18,10 @@ def clean():
 
 def build():
     global build_log, build_returncode, build_err
+    os.environ['BINARY_TAG'] = 'x86_64-slc0-gcc99-opt'
     build_proc = Popen(['make', 'VERBOSE=1'], cwd=base_dir,
                        stdout=PIPE, stderr=PIPE)
-    build_log, _err = build_proc.communicate()
-    build_err=_err
+    build_log, build_err = build_proc.communicate()
     build_returncode = build_proc.returncode
 
 def setup():
