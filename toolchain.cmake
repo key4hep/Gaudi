c2f1b33e38bd6921305ee55c8dabf74e5185f703
Marco Clemencic <marco.clemencic@cern.ch>
2017-03-18 09:15:14 +0100

    fixed LCG version override from environment

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,7 +1,8 @@
 # Special wrapper to load the declared version of the heptools toolchain.
 set(heptools_version 88)
 
-if(ENV{HEPTOOLS_VERSION})
+cmake_minimum_required(VERSION 2.8.5)
+if($ENV{HEPTOOLS_VERSION})
   set(heptools_version $ENV{HEPTOOLS_VERSION})
 endif()
 
