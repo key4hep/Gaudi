18bd86c062539bda33ec1251fcab7c4edeb76c7d
Marco Clemencic <marco.clemencic@cern.ch>
2017-03-29 15:35:45 +0200

    Add backward compatibility variables for CMake toolchain

--- cmake/GaudiDefaultToolchain.cmake
+++ cmake/GaudiDefaultToolchain.cmake
@@ -14,3 +14,17 @@ else()
 endif()
 
 set_paths_from_projects(${tools} ${projects})
+
+# set legacy variables for backward compatibility
+if(NOT LCG_SYSTEM)
+  set(LCG_SYSTEM ${BINARY_TAG_ARCH}-${BINARY_TAG_OS}-${BINARY_TAG_COMP}
+      CACHE STRING "Platform id of the target system or a compatible one.")
+  mark_as_advanced(LCG_SYSTEM LCG_platform LCG_system)
+endif()
+set(LCG_ARCH "${BINARY_TAG_ARCH}")
+set(LCG_HOST_ARCH "${HOST_BINARY_TAG_ARCH}")
+set(LCG_COMP "${BINARY_TAG_COMP_NAME}")
+string(REPLACE "." "" LCG_COMPVERS "${BINARY_TAG_COMP_VERSION}")
+set(LCG_BUILD_TYPE ${BINARY_TAG_TYPE})
+set(LCG_platform ${LCG_SYSTEM}-${BINARY_TAG_TYPE})
+set(LCG_system   ${LCG_SYSTEM}-opt)
