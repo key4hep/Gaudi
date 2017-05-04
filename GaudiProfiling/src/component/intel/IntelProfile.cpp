8bc4b59483d85a65133f4d90469da8b591c6e15b
Sebastien Ponce <sponce@pmpe07.cern.ch>
2017-05-04 12:14:16 +0200

    Introduced new Algorithm to help profiling Gaudi with Intel tools
    Called IntelProfile, it replaces/complements the IntelProfilerAuditor that is not usable in a threaded context (it relies on Incicdents).
    It allows to start and stop the intel profiling at given events.

---
+++ GaudiProfiling/src/component/intel/IntelProfile.cpp
@@ -0,0 +1,35 @@
+// Include files
+#include <iostream>
+
+// local
+#include "IntelProfile.h"
+
+//-----------------------------------------------------------------------------
+// Implementation file for class : IntelProfile
+//-----------------------------------------------------------------------------
+
+// Declaration of the Algorithm Factory
+DECLARE_ALGORITHM_FACTORY( IntelProfile )
+
+//=============================================================================
+// Main execution
+//=============================================================================
+StatusCode IntelProfile::execute() {
+
+  // Increase event number
+  m_eventNumber += 1;
+
+  if (m_eventNumber == m_nStartFromEvent) {
+    warning() << "Starting Intel profile at event "
+              <<  m_eventNumber << endmsg;
+    __itt_resume();
+  }
+
+  if (m_eventNumber ==  m_nStopAtEvent) {
+    warning() << "Stopping Intel profile at event "
+              <<  m_eventNumber << endmsg;
+    __itt_pause();
+  }
+
+  return StatusCode::SUCCESS;
+}
