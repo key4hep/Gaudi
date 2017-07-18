347a94c2120eedc72fa3d95ee0e5acd3a69a3a39
Gerhard Raven <gerhard.raven@nikhef.nl>
2017-07-18 10:42:09 +0200

    add support for Gaudi::Property<GaudiMath::Interpolation::Type>

--- GaudiGSL/GaudiMath/Interpolation.h
+++ GaudiGSL/GaudiMath/Interpolation.h
@@ -1,22 +1,31 @@
-#ifndef GAUDIMATH_INTERPOLATION_H 
+#ifndef GAUDIMATH_INTERPOLATION_H
 #define GAUDIMATH_INTERPOLATION_H 1
 // ============================================================================
-// incldue files 
+// include files
 // ============================================================================
+#include <iosfwd>
+#include <string>
 
-namespace GaudiMath 
+class StatusCode;
+
+namespace GaudiMath
 {
   namespace Interpolation
   {
     enum Type
       {
-        Linear     = 0   ,  // linear interpolation 
-        Polynomial       ,  // polinomial interpolation 
-        Cspline          ,  // Cubic spline with natural  boundary conditions 
-        Cspline_Periodic ,  // Cubic spline with periodic boundary conditions 
-        Akima            ,  // Akima spline with natural  boundary conditions 
-        Akima_Periodic      // Akima spline with periodic boundary conditions 
+        Linear     = 0   ,  // linear interpolation
+        Polynomial       ,  // polinomial interpolation
+        Cspline          ,  // Cubic spline with natural  boundary conditions
+        Cspline_Periodic ,  // Cubic spline with periodic boundary conditions
+        Akima            ,  // Akima spline with natural  boundary conditions
+        Akima_Periodic      // Akima spline with periodic boundary conditions
       };
+
+     StatusCode parse(Type&, const std::string&);
+     std::ostream& toStream(const Type&, std::ostream&);
+     inline std::ostream& operator<<(std::ostream& os, const Type& t) { return toStream(t,os); }
+
   } // end of namespace Interpolation
 } //end of namespace GaudiMath
 
