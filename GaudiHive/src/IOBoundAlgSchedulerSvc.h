key4hep/Gaudi

commit 99edb6c4b45cc58ec729247d689066af16ee500b

Dropped backward compatibility with XyzFactory classes
- require public constructor for components
- removed XyzFactory classes
- enabled warning for XyzFactory headers
- some clean up and modernization

GaudiHive/src/IOBoundAlgSchedulerSvc.h
@@ -22,14 +22,10 @@
  *  @author  Illya Shapoval
  *  @version 1.0
  */
-class IOBoundAlgSchedulerSvc : public extends1<Service, IAccelerator>
+class IOBoundAlgSchedulerSvc : public extends<Service, IAccelerator>
 {
 public:
-  /// Constructor
-  IOBoundAlgSchedulerSvc( const std::string& name, ISvcLocator* svc );
-
-  /// Destructor
-  ~IOBoundAlgSchedulerSvc() override;
+  using extends::extends;
 
   /// Initialise
   StatusCode initialize() override;

commit d297a9c1225987c4fd0203e35ed3d645a7b8d1bd

Fix bug introduced with 99edb6c4 (mr !420)
https://coverity.cern.ch/reports.htm#v13572/p10041/fileInstanceId=763879735&defectInstanceId=396360581&mergedDefectId=98201

GaudiHive/src/IOBoundAlgSchedulerSvc.h
@@ -46,7 +46,7 @@ private:
   StatusCode deactivate();
 
   /// Flag to track if the scheduler is active or not
-  bool m_isActive;
+  bool m_isActive = false;
 
   /// The thread in which the activate function runs
   std::thread m_thread;