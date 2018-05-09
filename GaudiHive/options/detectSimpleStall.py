3f9b4012c406e5d49b3b2df8442d836f92e2ec48
Illya Shapoval <ishapoval@lbl.gov>
2018-05-08 19:53:51 -0700

    add options file simulating a simple scheduling stall

---
+++ GaudiHive/options/detectSimpleStall.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env gaudirun.py
+
+from Gaudi.Configuration import *
+from Configurables import (HiveWhiteBoard, HiveSlimEventLoopMgr,
+                           AvalancheSchedulerSvc, AlgResourcePool,
+                           CPUCruncher,TimelineSvc,PrecedenceSvc)
+
+
+evtslots = 1
+evtMax = 1
+cardinality = 1
+threads = 1
+
+
+whiteboard = HiveWhiteBoard("EventDataSvc",
+                            EventSlots=evtslots)
+
+slimeventloopmgr = HiveSlimEventLoopMgr(SchedulerName="AvalancheSchedulerSvc",
+                                        OutputLevel=DEBUG)
+
+scheduler = AvalancheSchedulerSvc(ThreadPoolSize=threads,
+                                  OutputLevel=VERBOSE)
+
+AlgResourcePool(OutputLevel=DEBUG)
+
+# Set up of CPU crunchers -------------------------------------------------------
+
+a1 = CPUCruncher("A1")
+# to skip algorithm 'A2', force early exit from the branch
+# This leads to data flow stall
+a1.InvertDecision = True
+
+a2 = CPUCruncher("A2") # this algorithm is not run due to early exit from the group it belongs to
+a2.outKeys = ['/Event/a2']
+
+a3 = CPUCruncher("A3")
+a3.inpKeys = ['/Event/a2']
+
+# Assemble control flow graph
+branch = GaudiSequencer("EarlyExitBranch", ModeOR=False,
+                         ShortCircuit=True, Sequential=True)
+branch.Members = [a1, a2]
+
+for algo in [a1,a2,a3]:
+    algo.shortCalib = True
+    algo.Cardinality = cardinality
+    algo.avgRuntime = .1
+
+# Application Manager ----------------------------------------------------------
+
+ApplicationMgr(EvtMax=evtMax,
+               EvtSel='NONE',
+               ExtSvc=[whiteboard],
+               EventLoop=slimeventloopmgr,
+               TopAlg=[branch,a3],
+               MessageSvcType="InertMessageSvc",
+               OutputLevel=INFO)
