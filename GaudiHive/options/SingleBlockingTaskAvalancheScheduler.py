053bb8e9ccf6877c097ba6b3bbf9ee015d122984
Illya Shapoval <ishapoval@lbl.gov>
2018-05-25 18:34:50 -0700

    add test for asynchronous scheduling of a blocking task

---
+++ GaudiHive/options/SingleBlockingTaskAvalancheScheduler.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env gaudirun.py
+
+"""
+The simplest possible configuration for asynchronous scheduling of single blocking algorithm.
+"""
+
+from Gaudi.Configuration import *
+from Configurables import HiveWhiteBoard, HiveSlimEventLoopMgr, AvalancheSchedulerSvc, CPUCruncher
+
+# metaconfig
+evtMax = 7
+evtslots = 3
+algosInFlight = 0
+blockingAlgosInFlight = 3
+
+
+whiteboard = HiveWhiteBoard("EventDataSvc",
+                            EventSlots=evtslots,
+                            OutputLevel=INFO)
+
+slimeventloopmgr = HiveSlimEventLoopMgr(SchedulerName="AvalancheSchedulerSvc",
+                                        OutputLevel=INFO)
+
+AvalancheSchedulerSvc(ThreadPoolSize=algosInFlight,
+                      PreemptiveIOBoundTasks=True,
+                      MaxIOBoundAlgosInFlight=blockingAlgosInFlight,
+                      OutputLevel=VERBOSE)
+
+
+blockingAlg = CPUCruncher(name="BlockingAlg",
+                          shortCalib=True,
+                          avgRuntime=2.,
+                          Cardinality=3,
+                          IsIOBound=True,  # tag algorithm as blocking
+                          SleepFraction=0.7,  # simulate blocking nature
+                          OutputLevel=DEBUG)
+
+ApplicationMgr(EvtMax=evtMax,
+               EvtSel='NONE',
+               ExtSvc=[whiteboard],
+               EventLoop=slimeventloopmgr,
+               TopAlg=[blockingAlg],
+               MessageSvcType="InertMessageSvc",
+               OutputLevel=INFO)
