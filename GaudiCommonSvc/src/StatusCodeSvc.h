0825c547da2b87673521821f67a318e1d26bec8d
Frank Winklmeier <frank.winklmeier@cern.ch>
2019-04-16 12:23:28 +0200

    StatusCodeSvc: thread-safety fixes
    Make the `StatusCodeSvc` thread-safe:
    - use `tbb::concurrent_unordered_map` for storing of counts
    - apply the filters during printout in finalize instead of modifying the
      map during initialize
    - sort the output in finalize to ensure the same order as in the
      previous version using `std::map`

--- GaudiCommonSvc/src/StatusCodeSvc.h
+++ GaudiCommonSvc/src/StatusCodeSvc.h
@@ -1,11 +1,11 @@
-
 #ifndef GAUDISVC_STATUSCODESVC_H
 #define GAUDISVC_STATUSCODESVC_H
 
 #include "GaudiKernel/IStatusCodeSvc.h"
 #include "GaudiKernel/Service.h"
 
-#include <map>
+#include "tbb/concurrent_unordered_map.h"
+
 #include <set>
 #include <string>
 
@@ -22,22 +22,25 @@ public:
 
 private:
   struct StatCodeDat final {
-    std::string fnc;
-    std::string lib;
-    int         count;
+    // the atomic forces us to define these otherwise trivial constructors:
+    StatCodeDat( const std::string f, const std::string l ) : fnc( f ), lib( l ) {}
+    StatCodeDat( StatCodeDat&& o ) : fnc( o.fnc ), lib( o.lib ), count( o.count.load() ) {}
+    StatCodeDat( const StatCodeDat& o ) : fnc( o.fnc ), lib( o.lib ), count( o.count.load() ) {}
+    std::string               fnc;
+    std::string               lib;
+    std::atomic<unsigned int> count{1};
   };
 
-  void parseFilter( const std::string& str, std::string& fnc, std::string& lib );
-  void filterFnc( const std::string& );
-  void filterLib( const std::string& );
+  void parseFilter( const std::string& str, std::string& fnc, std::string& lib ) const;
+  bool failsFilter( const std::string& fnc, const std::string& lib ) const;
 
   Gaudi::Property<std::vector<std::string>> m_pFilter{this, "Filter"};
   Gaudi::Property<bool>                     m_abort{this, "AbortOnError", false};
   Gaudi::Property<bool>                     m_suppress{this, "SuppressCheck", false};
   Gaudi::Property<bool>                     m_dict{this, "IgnoreDicts", true};
 
-  std::map<std::string, StatCodeDat> m_dat;
-  std::set<std::string>              m_filterfnc, m_filterlib;
+  tbb::concurrent_unordered_map<std::string, StatCodeDat> m_dat;
+  std::set<std::string>                                   m_filterfnc, m_filterlib;
 };
 
 #endif
