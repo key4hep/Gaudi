23ca1ba993636aae4636ddadba5f20e4b8b667f6
Marco Clemencic <marco.clemencic@cern.ch>
2019-07-04 11:16:20 +0200

    Do not check for 'xsaves' to detect Skylake architecture

--- cmake/get_host_binary_tag.py
+++ cmake/get_host_binary_tag.py
@@ -18,64 +18,64 @@ from collections import OrderedDict
 
 # available flags per architecture
 # source: https://gitlab.cern.ch/lhcb-core/LbPlatformUtils/blob/master/LbPlatformUtils/architectures.py
-ARCH_DEFS = OrderedDict(
-    [('cannonlake',
-      set([
-          'pclmulqdq', 'avx', 'pku', 'umip', 'avx512dq', 'bmi1', 'fsgsbase',
-          'avx512ifma', 'avx512bw', 'xsaves', 'clflushopt', 'sse4_2', 'sse4_1',
-          'lm', 'adx', '3dnowprefetch', 'sha_ni', 'fma', 'mmx', 'avx512cd',
-          'avx512f', 'pni', 'rdseed', 'popcnt', 'sse', 'f16c', 'xsavec', 'aes',
-          'avx2', 'sse2', 'avx512vbmi', 'bmi2', 'ssse3', 'movbe', 'rdrand',
-          'avx512vl'
-      ])),
-     ('skylake_avx512',
-      set([
-          'pclmulqdq', 'avx', 'pku', 'avx512dq', 'fsgsbase', 'avx512bw',
-          'xsaves', 'clflushopt', 'sse4_2', 'sse4_1', 'lm', 'adx',
-          '3dnowprefetch', 'fma', 'mmx', 'avx512cd', 'avx512f', 'clwb', 'pni',
-          'rdseed', 'popcnt', 'sse', 'f16c', 'xsavec', 'aes', 'avx2', 'sse2',
-          'bmi1', 'bmi2', 'ssse3', 'movbe', 'rdrand', 'avx512vl'
-      ])),
-     ('skylake',
-      set([
-          'avx', 'fsgsbase', 'xsaves', 'clflushopt', 'sse4_2', 'sse4_1', 'lm',
-          'adx', '3dnowprefetch', 'fma', 'mmx', 'pclmulqdq', 'pni', 'rdseed',
-          'popcnt', 'sse', 'f16c', 'xsavec', 'aes', 'avx2', 'sse2', 'bmi1',
-          'bmi2', 'ssse3', 'movbe', 'rdrand'
-      ])),
-     ('broadwell',
-      set([
-          'avx', 'fsgsbase', 'sse4_2', 'sse4_1', 'lm', 'adx', '3dnowprefetch',
-          'fma', 'mmx', 'pclmulqdq', 'pni', 'rdseed', 'popcnt', 'sse', 'f16c',
-          'aes', 'avx2', 'sse2', 'bmi1', 'bmi2', 'ssse3', 'movbe', 'rdrand'
-      ])),
-     ('haswell',
-      set([
-          'avx', 'fsgsbase', 'sse4_2', 'sse4_1', 'lm', 'fma', 'mmx', 'aes',
-          'pni', 'popcnt', 'sse', 'f16c', 'pclmulqdq', 'avx2', 'sse2', 'bmi1',
-          'bmi2', 'ssse3', 'movbe', 'rdrand'
-      ])),
-     ('ivybridge',
-      set([
-          'pni', 'aes', 'sse4_2', 'mmx', 'sse2', 'sse4_1', 'lm', 'pclmulqdq',
-          'ssse3', 'fsgsbase', 'popcnt', 'rdrand', 'sse', 'avx', 'f16c'
-      ])),
-     ('sandybridge',
-      set([
-          'pni', 'aes', 'sse4_2', 'mmx', 'sse2', 'sse4_1', 'lm', 'pclmulqdq',
-          'ssse3', 'popcnt', 'sse', 'avx'
-      ])),
-     ('westmere',
-      set([
-          'pni', 'aes', 'sse4_2', 'mmx', 'sse2', 'sse4_1', 'lm', 'pclmulqdq',
-          'ssse3', 'popcnt', 'sse'
-      ])),
-     ('nehalem',
-      set([
-          'pni', 'sse4_2', 'mmx', 'sse2', 'sse4_1', 'lm', 'ssse3', 'popcnt',
-          'sse'
-      ])), ('core2', set(['pni', 'mmx', 'sse2', 'lm', 'ssse3', 'sse'])),
-     ('x86_64', set([]))])
+ARCH_DEFS = OrderedDict([
+    ('cannonlake',
+     set([
+         'pclmulqdq', 'avx', 'pku', 'umip', 'avx512dq', 'bmi1', 'fsgsbase',
+         'avx512ifma', 'avx512bw', 'clflushopt', 'sse4_2', 'sse4_1', 'lm',
+         'adx', '3dnowprefetch', 'sha_ni', 'fma', 'mmx', 'avx512cd', 'avx512f',
+         'pni', 'rdseed', 'popcnt', 'sse', 'f16c', 'xsavec', 'aes', 'avx2',
+         'sse2', 'avx512vbmi', 'bmi2', 'ssse3', 'movbe', 'rdrand', 'avx512vl'
+     ])),
+    ('skylake_avx512',
+     set([
+         'pclmulqdq', 'avx', 'pku', 'avx512dq', 'fsgsbase', 'avx512bw',
+         'clflushopt', 'sse4_2', 'sse4_1', 'lm', 'adx', '3dnowprefetch', 'fma',
+         'mmx', 'avx512cd', 'avx512f', 'clwb', 'pni', 'rdseed', 'popcnt',
+         'sse', 'f16c', 'xsavec', 'aes', 'avx2', 'sse2', 'bmi1', 'bmi2',
+         'ssse3', 'movbe', 'rdrand', 'avx512vl'
+     ])),
+    ('skylake',
+     set([
+         'avx', 'fsgsbase', 'clflushopt', 'sse4_2', 'sse4_1', 'lm', 'adx',
+         '3dnowprefetch', 'fma', 'mmx', 'pclmulqdq', 'pni', 'rdseed', 'popcnt',
+         'sse', 'f16c', 'xsavec', 'aes', 'avx2', 'sse2', 'bmi1', 'bmi2',
+         'ssse3', 'movbe', 'rdrand'
+     ])),
+    ('broadwell',
+     set([
+         'avx', 'fsgsbase', 'sse4_2', 'sse4_1', 'lm', 'adx', '3dnowprefetch',
+         'fma', 'mmx', 'pclmulqdq', 'pni', 'rdseed', 'popcnt', 'sse', 'f16c',
+         'aes', 'avx2', 'sse2', 'bmi1', 'bmi2', 'ssse3', 'movbe', 'rdrand'
+     ])),
+    ('haswell',
+     set([
+         'avx', 'fsgsbase', 'sse4_2', 'sse4_1', 'lm', 'fma', 'mmx', 'aes',
+         'pni', 'popcnt', 'sse', 'f16c', 'pclmulqdq', 'avx2', 'sse2', 'bmi1',
+         'bmi2', 'ssse3', 'movbe', 'rdrand'
+     ])),
+    ('ivybridge',
+     set([
+         'pni', 'aes', 'sse4_2', 'mmx', 'sse2', 'sse4_1', 'lm', 'pclmulqdq',
+         'ssse3', 'fsgsbase', 'popcnt', 'rdrand', 'sse', 'avx', 'f16c'
+     ])),
+    ('sandybridge',
+     set([
+         'pni', 'aes', 'sse4_2', 'mmx', 'sse2', 'sse4_1', 'lm', 'pclmulqdq',
+         'ssse3', 'popcnt', 'sse', 'avx'
+     ])),
+    ('westmere',
+     set([
+         'pni', 'aes', 'sse4_2', 'mmx', 'sse2', 'sse4_1', 'lm', 'pclmulqdq',
+         'ssse3', 'popcnt', 'sse'
+     ])),
+    ('nehalem',
+     set([
+         'pni', 'sse4_2', 'mmx', 'sse2', 'sse4_1', 'lm', 'ssse3', 'popcnt',
+         'sse'
+     ])), ('core2', set(['pni', 'mmx', 'sse2', 'lm', 'ssse3', 'sse'])),
+    ('x86_64', set([]))
+])
 
 
 def _Linux_os():
