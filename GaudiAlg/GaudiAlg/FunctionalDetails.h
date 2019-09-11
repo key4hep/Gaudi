0bdf0ead5222949ecb897306b6a965f88cfa58d1
Chris Jones <jonesc@hep.phy.cam.ac.uk>
2019-09-11 16:23:39 +0100

    FunctionalDetails.h - Better workaround for range v3 view->views namespace change

--- GaudiAlg/GaudiAlg/FunctionalDetails.h
+++ GaudiAlg/GaudiAlg/FunctionalDetails.h
@@ -18,6 +18,12 @@
 #include <range/v3/version.hpp>
 #include <range/v3/view/const.hpp>
 #include <range/v3/view/zip.hpp>
+// upstream has renamed namespace ranges::view ranges::views
+#if RANGE_V3_VERSION < 900
+namespace ranges::views {
+  using namespace ranges::view;
+}
+#endif
 
 namespace Gaudi::Functional::details {
 
@@ -73,11 +79,7 @@ namespace Gaudi::Functional::details {
 #ifndef NDEBUG
       verifySizes( args... );
 #endif
-#if RANGE_V3_VERSION < 900
-      return ranges::view::zip( std::forward<Args>( args )... );
-#else
       return ranges::views::zip( std::forward<Args>( args )... );
-#endif
     }
 
     /// Zips multiple containers together to form a single const range
@@ -86,11 +88,7 @@ namespace Gaudi::Functional::details {
 #ifndef NDEBUG
       verifySizes( args... );
 #endif
-#if RANGE_V3_VERSION < 900
-      return ranges::view::const_( ranges::view::zip( std::forward<Args>( args )... ) );
-#else
       return ranges::views::const_( ranges::views::zip( std::forward<Args>( args )... ) );
-#endif
     }
   } // namespace zip
 
