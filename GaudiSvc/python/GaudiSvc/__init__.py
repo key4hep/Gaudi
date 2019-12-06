0469f84741f9beb99465bdd931dbee359d1606ac
Marco Clemencic <marco.clemencic@cern.ch>
2019-12-06 16:12:30 +0100

    workaround for ROOT-10474

--- GaudiSvc/python/GaudiSvc/__init__.py
+++ GaudiSvc/python/GaudiSvc/__init__.py
@@ -7,6 +7,22 @@ def getMetaData(path):
     if not f:
         raise RuntimeError('cannot open {0}'.format(path))
     try:
-        return dict((i.first, i.second) for i in f.info)
+        # return dict((i.first, i.second) for i in f.info)
+        # FIXME: workaround for https://sft.its.cern.ch/jira/browse/ROOT-10474
+        import cppyy
+        cppyy.gbl.gInterpreter.Declare(r'''
+        #include <map>
+        #include <string>
+        #include <sstream>
+        auto get_keys(const std::map<std::string, std::string>& m) {
+            std::stringstream s;
+            for(auto& i: m) {
+                s << i.first << '\n';
+            }
+            return s.str();
+        }
+        ''')
+        keys = str(cppyy.gbl.get_keys(f.info)).strip().split('\n')
+        return dict((k, f.info[k]) for k in keys)
     except AttributeError:
         return {}
