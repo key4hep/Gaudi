2b7fd33ad8bcf87c3abf60d747c09e2f92fc8733
Marco Clemencic <marco.clemencic@cern.ch>
2019-12-16 17:01:14 +0100

    Fixed GaudiPluginService copyright and license

--- GaudiPluginService/tests/src/LegacyUseCasesLib.cpp
+++ GaudiPluginService/tests/src/LegacyUseCasesLib.cpp
@@ -1,13 +1,13 @@
-/*****************************************************************************\
-* (c) Copyright 2013 CERN                                                     *
-*                                                                             *
-* This software is distributed under the terms of the GNU General Public      *
-* Licence version 3 (GPL Version 3), copied verbatim in the file "LICENCE".   *
-*                                                                             *
-* In applying this licence, CERN does not waive the privileges and immunities *
-* granted to it by virtue of its status as an Intergovernmental Organization  *
-* or submit itself to any jurisdiction.                                       *
-\*****************************************************************************/
+/***********************************************************************************\
+* (c) Copyright 2013-2019 CERN for the benefit of the LHCb and ATLAS collaborations *
+*                                                                                   *
+* This software is distributed under the terms of the Apache version 2 licence,     *
+* copied verbatim in the file "LICENSE".                                            *
+*                                                                                   *
+* In applying this licence, CERN does not waive the privileges and immunities       *
+* granted to it by virtue of its status as an Intergovernmental Organization        *
+* or submit itself to any jurisdiction.                                             *
+\***********************************************************************************/
 /**
  * Compile-time test for all known PluginService use-cases
  *
