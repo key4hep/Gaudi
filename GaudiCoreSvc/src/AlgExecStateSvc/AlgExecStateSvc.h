b8d2f0a4c2cefb36b5c2df530e1013482bf2dba4
Frank Winklmeier <frank.winklmeier@cern.ch>
2020-01-29 17:34:12 +0100

    AlgExecStateSvc: Remove a fatal message and minor cleanups
    Remove the fatal message in `checkInit`. It already throws an exception
    and this way we can leave it up to the caller to print an appropriate
    message.
    
    Some other changes:
    * `std::map` -> `std::unordered_map`
    * Add a few `const`
    * Use `ConcurrencyFlags` to retrieve the number of slots instead of
      trying to find the `IHiveWhiteBoard`.

--- GaudiCoreSvc/src/AlgExecStateSvc/AlgExecStateSvc.h
+++ GaudiCoreSvc/src/AlgExecStateSvc/AlgExecStateSvc.h
@@ -1,5 +1,5 @@
 /***********************************************************************************\
-* (c) Copyright 1998-2019 CERN for the benefit of the LHCb and ATLAS collaborations *
+* (c) Copyright 1998-2020 CERN for the benefit of the LHCb and ATLAS collaborations *
 *                                                                                   *
 * This software is distributed under the terms of the Apache version 2 licence,     *
 * copied verbatim in the file "LICENSE".                                            *
@@ -15,8 +15,8 @@
 #include "GaudiKernel/IAlgorithm.h"
 #include "GaudiKernel/Service.h"
 
-#include <map>
 #include <mutex>
+#include <unordered_map>
 #include <vector>
 
 /** @class AlgExecStateSvc
@@ -58,12 +58,12 @@ private:
   std::vector<EventStatus::Status> m_eventStatus;
   std::vector<Gaudi::StringKey>    m_preInitAlgs;
 
-  std::map<Gaudi::StringKey, std::atomic<unsigned int>> m_errorCount;
+  std::unordered_map<Gaudi::StringKey, std::atomic<unsigned int>> m_errorCount;
 
   void           init();
   void           checkInit() const;
   std::once_flag m_initFlag;
-  bool           m_isInit = false;
+  bool           m_isInit{false};
 
   std::mutex m_mut;
 };
