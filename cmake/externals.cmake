a0eb6159870edca525df23e49b11eea2e715d554
Marco Clemencic <marco.clemencic@cern.ch>
2020-03-04 16:38:31 +0100

    Add (interface) dependency on fmtlib

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -1,5 +1,5 @@
 #####################################################################################
-# (c) Copyright 1998-2019 CERN for the benefit of the LHCb and ATLAS collaborations #
+# (c) Copyright 1998-2020 CERN for the benefit of the LHCb and ATLAS collaborations #
 #                                                                                   #
 # This software is distributed under the terms of the Apache version 2 licence,     #
 # copied verbatim in the file "LICENSE".                                            #
@@ -73,3 +73,6 @@ if(RANGEV3_INCLUDE_DIR)
   set(RANGEV3_FOUND TRUE)
   set_property(GLOBAL APPEND PROPERTY GAUDI_REQUIRED_PATHS ${RANGEV3_INCLUDE_DIRS})
 endif()
+
+# fmtlib: https://fmt.dev
+find_package(fmt REQUIRED)
