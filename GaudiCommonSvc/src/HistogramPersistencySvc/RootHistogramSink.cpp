bbf2de2711e6809691380ae8601feebbb2b87581
Sebastien Ponce <sebastien.ponce@cern.ch>
2020-09-23 17:41:54 +0200

    Better version of ROOT Sink for Histos, credits to Gerhard

--- GaudiCommonSvc/src/HistogramPersistencySvc/RootHistogramSink.cpp
+++ GaudiCommonSvc/src/HistogramPersistencySvc/RootHistogramSink.cpp
@@ -40,59 +40,77 @@ namespace {
              jAxis.at("maxValue").get<double>() };
   }
 
-  template<typename ROOTHisto, typename WEIGHT, std::size_t... index, typename UpdFunc>
-  void saveRootHistoInternal(const std::string& component, const std::string& name, nlohmann::json& j,
-                             std::index_sequence<index...>, UpdFunc f) {
-    auto id = fmt::format("{}/{}", component, name );
+  template <typename Traits, std::size_t... index>
+  void saveRootHistoInternal( const std::string& component, const std::string& name, nlohmann::json& j,
+                              std::index_sequence<index...> ) {
+    auto id = fmt::format( "{}/{}", component, name );
     // extract data from json
-    auto jsonAxis = j.at("axis");
-    auto axis = std::array{ toAxis( jsonAxis[index] )... };
-    auto weights = j.at("bins").get<WEIGHT>();
+    auto jsonAxis = j.at( "axis" );
+    auto axis     = std::array{toAxis( jsonAxis[index] )...};
+    auto weights  = j.at( "bins" ).get<std::vector<typename Traits::WeightType>>();
     // compute total number of bins, multiplying bins per axis
     auto totNBins = ((axis[index].nBins+2) * ...);
     assert(weights.size() == totNBins);
     // Create Root histogram calling constructors with the args tuple
-    auto histo = std::make_from_tuple<ROOTHisto>( std::tuple_cat( std::tuple{ id.c_str(), id.c_str() },
-                                                                  std::tuple{ axis[index].nBins, axis[index].minValue, axis[index].maxValue} ... ) );
+    auto histo = std::make_from_tuple<typename Traits::Histo>(
+        std::tuple_cat( std::tuple{id.c_str(), id.c_str()},
+                        std::tuple{axis[index].nBins, axis[index].minValue, axis[index].maxValue}... ) );
     // fill Histo
-    for (unsigned int i = 0; i < totNBins; i++) f(histo, i, weights);
+    for ( unsigned int i = 0; i < totNBins; i++ ) Traits::fill( histo, i, weights[i] );
     // write to file
     histo.Write();
   }
 
-  template<unsigned int ND, typename ROOTHisto>
-  void saveRootHisto(const std::string& component, const std::string& name, nlohmann::json& j) {
-    saveRootHistoInternal<ROOTHisto, std::vector<double>>
-      (component, name, j, std::make_index_sequence<ND>(),
-       [](ROOTHisto& histo, unsigned int i, auto& weights) { histo.SetBinContent(i, weights[i]); });
-  }
+  template <bool isProfile, typename RootHisto>
+  struct Traits;
 
-  /// Wrapper around TProfileX to be able to fill it
-  template <typename TP>
-  struct ProfileWrapper : TP {
-    using TP::TP;
-    void setBinNEntries(Int_t i, Int_t n) {
-      this->fBinEntries.fArray[i] = n;
-    }
-    void setBinW2(Int_t i, Double_t v) {
-      this->fSumw2.fArray[i] = v;
+  template <typename RootHisto>
+  struct Traits<false, RootHisto> {
+    using Histo      = RootHisto;
+    using WeightType = double;
+    static auto fill( Histo& histo, unsigned int i, const WeightType& weight ) {
+      histo.SetBinContent( i, weight );
     }
   };
+  template <typename RootHisto>
+  struct Traits<true, RootHisto> {
+    /// Wrapper around TProfileX to be able to fill it
+    template <typename TP>
+    struct ProfileWrapper : TP {
+      using TP::TP;
+      void setBinNEntries( Int_t i, Int_t n ) { this->fBinEntries.fArray[i] = n; }
+      void setBinW2( Int_t i, Double_t v ) { this->fSumw2.fArray[i] = v; }
+    };
+    using Histo      = ProfileWrapper<RootHisto>;
+    using WeightType = std::tuple<std::tuple<unsigned int, double>, double>;
+    static constexpr auto fill( Histo& histo, unsigned int i, const WeightType& weight ) {
+      auto [c, sumWeight2]       = weight;
+      auto [nEntries, sumWeight] = c;
+      histo.setBinNEntries( i, nEntries );
+      histo.SetBinContent( i, sumWeight );
+      histo.setBinW2( i, sumWeight2 );
+    };
+  };
 
-  template<unsigned int ND, typename ROOTHisto>
-  void saveRootProfHisto(const std::string& component, const std::string& name, nlohmann::json& j) {
-    using Histo = ProfileWrapper<ROOTHisto>;
-    saveRootHistoInternal<Histo, std::vector<std::tuple<std::tuple<unsigned int, double>, double>>>
-      (component, name, j, std::make_index_sequence<ND>(),
-       [](Histo& histo, unsigned int i, auto& weights) {
-         auto [c, sumWeight2] = weights[i];
-         auto [nEntries, sumWeight] = c;
-         histo.setBinNEntries(i, nEntries);
-         histo.SetBinContent(i, sumWeight);
-         histo.setBinW2(i, sumWeight2);
-       });
+  template <unsigned int N, bool isProfile, typename ROOTHisto>
+  void saveRootHisto( const std::string& component, const std::string& name, nlohmann::json& j ) {
+    saveRootHistoInternal<Traits<isProfile, ROOTHisto>>( component, name, j, std::make_index_sequence<N>() );
   }
 
+  using namespace std::string_literals;
+  static const auto registry =
+    std::map{std::pair{std::pair{"histogram:Histogram"s, 1}, &saveRootHisto<1, false, TH1D>},
+             std::pair{std::pair{"histogram:WeightedHistogram"s, 1}, &saveRootHisto<1, false, TH1D>},
+             std::pair{std::pair{"histogram:Histogram"s, 2}, &saveRootHisto<2, false, TH2D>},
+             std::pair{std::pair{"histogram:WeightedHistogram"s, 2}, &saveRootHisto<2, false, TH2D>},
+             std::pair{std::pair{"histogram:Histogram"s, 3}, &saveRootHisto<3, false, TH3D>},
+             std::pair{std::pair{"histogram:WeightedHistogram"s, 3}, &saveRootHisto<3, false, TH3D>},
+             std::pair{std::pair{"histogram:ProfileHistogram"s, 1}, &saveRootHisto<1, true, TProfile>},
+             std::pair{std::pair{"histogram:WeightedProfileHistogram"s, 1}, &saveRootHisto<1, true, TProfile>},
+             std::pair{std::pair{"histogram:ProfileHistogram"s, 2}, &saveRootHisto<2, true, TProfile2D>},
+             std::pair{std::pair{"histogram:WeightedProfileHistogram"s, 2}, &saveRootHisto<2, true, TProfile2D>},
+             std::pair{std::pair{"histogram:ProfileHistogram"s, 3}, &saveRootHisto<3, true, TProfile3D>},
+             std::pair{std::pair{"histogram:WeightedProfileHistogram"s, 3}, &saveRootHisto<3, true, TProfile3D>}};
 }
 
 namespace Gaudi::Histograming::Sink {
@@ -113,48 +131,20 @@ namespace Gaudi::Histograming::Sink {
     StatusCode stop() override {
       auto ok = Service::stop();
       if ( !ok ) return ok;
-      std::sort(begin(m_monitoringEntities), end(m_monitoringEntities),
-                []( const auto& a, const auto& b ) {
-                  return a.component == b.component ? a.name > b.name : a.component > b.component ;
-                } );
-      TFile histoFile("testHisto.root", "RECREATE");
-      std::for_each( begin( m_monitoringEntities ), end( m_monitoringEntities ),
-                     []( auto& ent ) {
-                       auto j = ent.toJSON();
-                       auto dim = j.at("dimension").template get<unsigned int>();
-                       if (dim > 3) throw GaudiException("Unable to save ROOT histogram for dimension > 3", "Histogram::Sink::Root", StatusCode::FAILURE);
-                       if (dim == 0) throw GaudiException("Invalid histogram json with dimension 0", "Histogram::Sink::Root", StatusCode::FAILURE);
-                       auto type = j.at("type").template get<std::string>();
-                       if ( type == "histogram:Histogram" ||
-                            type == "histogram:WeightedHistogram" ) {
-                         switch (dim) {
-                         case 1:
-                           saveRootHisto<1, TH1D>(ent.component, ent.name, j);
-                           break;
-                         case 2:
-                           saveRootHisto<2, TH2D>(ent.component, ent.name, j);
-                           break;
-                         case 3 :
-                           saveRootHisto<3, TH3D>(ent.component, ent.name, j);
-                           break;
-                         }
-                       } else if ( type == "histogram:ProfileHistogram" ||
-                                   type == "histogram:WeightedProfileHistogram" ) {
-                         switch (dim) {
-                         case 1:
-                           saveRootProfHisto<1, TProfile>(ent.component, ent.name, j);
-                           break;
-                         case 2:
-                           saveRootProfHisto<2, TProfile2D>(ent.component, ent.name, j);
-                           break;
-                         case 3 :
-                           saveRootProfHisto<3, TProfile3D>(ent.component, ent.name, j);
-                           break;
-                         }
-                       } else {
-                         throw GaudiException("Unknown histogram type : " + type, "Histogram::Sink::Root", StatusCode::FAILURE);
-                       }
-                     } );
+      std::sort( begin( m_monitoringEntities ), end( m_monitoringEntities ), []( const auto& a, const auto& b ) {
+        return a.component == b.component ? a.name > b.name : a.component > b.component;
+      } );
+      TFile histoFile( "testHisto.root", "RECREATE" );
+      std::for_each( begin( m_monitoringEntities ), end( m_monitoringEntities ), []( auto& ent ) {
+        auto j   = ent.toJSON();
+        auto dim = j.at( "dimension" ).template get<unsigned int>();
+        auto type  = j.at( "type" ).template get<std::string>();
+        auto saver = registry.find( {type, dim} );
+        if ( saver == registry.end() )
+          throw GaudiException( "Unknown type : " + type + " dim : " + std::to_string( dim ), "Histogram::Sink::Root",
+                                StatusCode::FAILURE );
+        ( *saver->second )( ent.component, ent.name, j );
+      } );
       histoFile.Close();
       return ok;
     }
