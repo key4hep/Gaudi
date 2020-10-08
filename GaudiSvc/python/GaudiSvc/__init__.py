f076dbbe94624fa38da9c169ee47c0745d796e1a
Marco Clemencic <marco.clemencic@cern.ch>
2020-10-08 17:19:14 +0200

    Adapt to new cppyy and remove workaround for ROOT-10474

--- GaudiSvc/python/GaudiSvc/__init__.py
+++ GaudiSvc/python/GaudiSvc/__init__.py
@@ -12,27 +12,11 @@ def getMetaData(path):
     '''
     Extract job metadata recorded by Gaudi::MetaDataSvc from a file.
     '''
-    from cppyy.gbl import TFile
+    from ROOT import TFile
     f = TFile.Open(path)
     if not f:
         raise RuntimeError('cannot open {0}'.format(path))
     try:
-        # return dict((i.first, i.second) for i in f.info)
-        # FIXME: workaround for https://sft.its.cern.ch/jira/browse/ROOT-10474
-        import cppyy
-        cppyy.gbl.gInterpreter.Declare(r'''
-        #include <map>
-        #include <string>
-        #include <sstream>
-        auto get_keys(const std::map<std::string, std::string>& m) {
-            std::stringstream s;
-            for(auto& i: m) {
-                s << i.first << '\n';
-            }
-            return s.str();
-        }
-        ''')
-        keys = str(cppyy.gbl.get_keys(f.info)).strip().split('\n')
-        return dict((k, f.info[k]) for k in keys)
+        return dict(f.info)
     except AttributeError:
         return {}
