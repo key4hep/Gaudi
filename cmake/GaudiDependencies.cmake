1cdec5ff81c6a9d2e95fcf08c53cd7aa6f537d46
Marco Clemencic <marco.clemencic@cern.ch>
2020-11-23 09:13:00 +0000

    Use Python_FIND_STRATEGY LOCATION
    this allows to point CMake to a version of Python of our choice,
    despite what is available on the PATH.

--- cmake/GaudiDependencies.cmake
+++ cmake/GaudiDependencies.cmake
@@ -106,7 +106,7 @@ foreach(component IN ITEMS system filesystem regex thread python unit_test_frame
   mark_as_advanced(boost_${component}_DIR)
 endforeach()
 
-set(Python_FIND_STRATEGY VERSION) # Find latest version available
+set(Python_FIND_STRATEGY LOCATION) # Trust search hints to find the wanted Python
 if(GAUDI_USE_PYTHON_MAJOR STREQUAL "2")
   set(_gaudi_Python_MIN_VERSION 2.7.15)
 elseif(GAUDI_USE_PYTHON_MAJOR STREQUAL "3")
