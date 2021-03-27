0a7a46c511139f97c0c20bb20fe607ede7487d61
Marco Clemencic <marco.clemencic@cern.ch>
2021-03-27 08:14:43 +0100

    Do not mess with FeatureSummary_DEFAULT_PKG_TYPE in downstream projects

--- cmake/GaudiDependencies.cmake
+++ cmake/GaudiDependencies.cmake
@@ -87,8 +87,6 @@ if(GAUDI_DEPENDENCIES_FIND_QUIETLY)
 endif()
 
 include(FeatureSummary)
-get_property(SAVED_FeatureSummary_REQUIRED_PKG_TYPES GLOBAL PROPERTY FeatureSummary_REQUIRED_PKG_TYPES)
-set_property(GLOBAL PROPERTY FeatureSummary_DEFAULT_PKG_TYPE REQUIRED)
 
 # Note: this must be set before the first `find_package`
 set(THREADS_PREFER_PTHREAD_FLAG YES)
@@ -98,6 +96,7 @@ set(OLD_BUILD_SHARED_LIBS ${BUILD_SHARED_LIBS}) # FIXME: the day BoostConfig.cma
 set(BUILD_SHARED_LIBS ON)
 find_package(Boost 1.70 ${__quiet} CONFIG COMPONENTS system filesystem regex
   thread python unit_test_framework program_options log log_setup graph)
+set_package_properties(Boost PROPERTIES TYPE REQUIRED)
 set(BUILD_SHARED_LIBS ${OLD_BUILD_SHARED_LIBS})
 mark_as_advanced(Boost_DIR) # FIXME: the day Boost correctly marks as advanced its variables
 foreach(component IN ITEMS system filesystem regex thread python unit_test_framework
@@ -114,9 +113,11 @@ else()
   message(FATAL_ERROR "Invalid value for GAUDI_USE_PYTHON_MAJOR (${GAUDI_USE_PYTHON_MAJOR}), only 2 and 3 are supported")
 endif()
 find_package(Python ${_gaudi_Python_MIN_VERSION} ${__quiet} COMPONENTS Interpreter Development)
+set_package_properties(Python PROPERTIES TYPE REQUIRED)
 
 find_package(ROOT 6.18 ${__quiet} CONFIG COMPONENTS Core RIO Hist Thread Matrix
   MathCore Net XMLIO Tree TreePlayer Graf3d Graf Gpad)
+set_package_properties(ROOT PROPERTIES TYPE REQUIRED)
 mark_as_advanced(ROOT_DIR ROOT_genmap_CMD ROOT_rootdraw_CMD)
 # FIXME: the day ROOTConfig.cmake displays at least that it was found remove these lines
 if(NOT GAUDI_DEPENDENCIES_FIND_QUIETLY)
@@ -126,10 +127,12 @@ endif()
 # FIXME: if you are using the normal version of TBB, it has a config file so
 #   remove cmake/FindTBB.cmake and add CONFIG to the next line.
 find_package(TBB 2019.0.11007.2 ${__quiet})
+set_package_properties(TBB PROPERTIES TYPE REQUIRED)
 
 set(_gaudi_ZLIB_MIN_VERSION 1.2.11)
 foreach(dep IN ITEMS UUID Threads ZLIB Rangev3 cppgsl fmt nlohmann_json)
   find_package(${dep} ${_gaudi_${dep}_MIN_VERSION} ${__quiet})
+  set_package_properties(${dep} PROPERTIES TYPE REQUIRED)
 endforeach()
 
 set(_gaudi_CLHEP_MIN_VERSION 2.4.0.1)
@@ -163,7 +166,9 @@ foreach(dep IN LISTS deps)
     if(CMAKE_FIND_PACKAGE_NAME) # if the lookup is performed from GaudiConfig.cmake
       # then, all enabled "optional" dependencies become RECOMMENDED
       # (except AIDA that is required downstream if used at build time)
-      if(NOT dep STREQUAL "AIDA")
+      if(dep STREQUAL "AIDA")
+        set_package_properties(${dep} PROPERTIES TYPE REQUIRED)
+      else()
         set_package_properties(${dep} PROPERTIES TYPE RECOMMENDED)
       endif()
     endif()
@@ -190,6 +195,3 @@ endif()
 # Print a summary of the lookup
 feature_summary(FATAL_ON_MISSING_REQUIRED_PACKAGES
   WHAT ALL)
-
-set_property(GLOBAL PROPERTY FeatureSummary_DEFAULT_PKG_TYPE ${SAVED_FeatureSummary_REQUIRED_PKG_TYPES})
-unset(SAVED_FeatureSummary_REQUIRED_PKG_TYPES)
