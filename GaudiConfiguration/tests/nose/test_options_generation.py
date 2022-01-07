e21973ddd415674a0408caf723d23b869f8a77d5
Marco Clemencic <marco.clemencic@cern.ch>
2022-01-07 11:15:04 +0100

    add test to validate is_property_set logic

--- GaudiConfiguration/tests/nose/test_options_generation.py
+++ GaudiConfiguration/tests/nose/test_options_generation.py
@@ -1,5 +1,5 @@
 #####################################################################################
-# (c) Copyright 1998-2019 CERN for the benefit of the LHCb and ATLAS collaborations #
+# (c) Copyright 1998-2022 CERN for the benefit of the LHCb and ATLAS collaborations #
 #                                                                                   #
 # This software is distributed under the terms of the Apache version 2 licence,     #
 # copied verbatim in the file "LICENSE".                                            #
@@ -65,6 +65,20 @@ def test_props():
         "Dummy.Int": "42",
         "Dummy.String": "'default'",
         "Dummy.Tool": "'TestConf::SimpleOptsAlgTool/Dummy.MyTool'",
+    }
+
+
+@with_setup(setup_func, teardown_func)
+def test_special_cases_for_is_set():
+    p = SimpleOptsAlg("Dummy")
+    assert p.__opt_properties__() == {}
+
+    p.String  # just accessing is not setting, so it should not appear
+    # setting to default should show as explicitly set
+    p.Int = p._descriptors["Int"].default
+    print(p.__opt_properties__())
+    assert p.__opt_properties__() == {
+        "Dummy.Int": "0",
     }
 
 
