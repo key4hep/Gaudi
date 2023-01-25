892bb42db3ddc603404341da46fee3dbff8bb931
Marco Clemencic <marco.clemencic@cern.ch>
2023-01-25 14:57:15 +0100

    Add CI job to build a minimal Gaudi (no optional feature)

---
+++ ci-utils/preset-full.cmake
@@ -0,0 +1,12 @@
+# Enable everyting
+set(GAUDI_USE_AIDA             ON  CACHE BOOL "")
+set(GAUDI_USE_XERCESC          ON  CACHE BOOL "")
+set(GAUDI_USE_CLHEP            ON  CACHE BOOL "")
+set(GAUDI_USE_HEPPDT           ON  CACHE BOOL "")
+set(GAUDI_USE_CPPUNIT          ON  CACHE BOOL "")
+set(GAUDI_USE_UNWIND           ON  CACHE BOOL "")
+set(GAUDI_USE_GPERFTOOLS       ON  CACHE BOOL "")
+set(GAUDI_USE_DOXYGEN          ON  CACHE BOOL "")
+set(GAUDI_USE_INTELAMPLIFIER   ON  CACHE BOOL "")
+set(GAUDI_USE_JEMALLOC         ON  CACHE BOOL "")
+set(BUILD_TESTING              ON  CACHE BOOL "")
