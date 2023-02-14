4a58e64530cb58dda61892ec24e1405b4f45da81
Marco Clemencic <marco.clemencic@cern.ch>
2023-02-14 13:13:52 +0100

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,9 @@ copyright = "1998-2023, CERN for the benefit of the LHCb and ATLAS collaboration
 author = "The Gaudi Developers"
 
 # The short X.Y version
-version = "36.9.1"
+version = "36.10"
 # The full version, including alpha/beta/rc tags
-release = "v36r9p1"
+release = "v36r10"
 
 # -- General configuration ---------------------------------------------------
 
