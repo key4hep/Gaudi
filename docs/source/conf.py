3f5d8b9884e6565fec05302bbad8b952a92a5464
Marco Clemencic <marco.clemencic@cern.ch>
2023-02-27 18:05:40 +0100

    update version and change log

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,9 @@ copyright = "1998-2023, CERN for the benefit of the LHCb and ATLAS collaboration
 author = "The Gaudi Developers"
 
 # The short X.Y version
-version = "36.10"
+version = "36.11"
 # The full version, including alpha/beta/rc tags
-release = "v36r10"
+release = "v36r11"
 
 # -- General configuration ---------------------------------------------------
 
