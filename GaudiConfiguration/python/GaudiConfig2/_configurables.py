f99b91efb2de68e2185ec1b585d83ede71579f51
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-03-10 13:44:17 +0100

    GaudiConfig2: add clone for Configurable
    Add a `clone` method to the Configurable class and tests.

--- GaudiConfiguration/python/GaudiConfig2/_configurables.py
+++ GaudiConfiguration/python/GaudiConfig2/_configurables.py
@@ -1,5 +1,5 @@
 #####################################################################################
-# (c) Copyright 1998-2022 CERN for the benefit of the LHCb and ATLAS collaborations #
+# (c) Copyright 1998-2023 CERN for the benefit of the LHCb and ATLAS collaborations #
 #                                                                                   #
 # This software is distributed under the terms of the Apache version 2 licence,     #
 # copied verbatim in the file "LICENSE".                                            #
@@ -272,6 +272,10 @@ class Configurable(ConfigMetaHelper):
     @classmethod
     def getDefaultProperty(cls, name):
         return cls._descriptors[name].default
+
+    def clone(self, newname=None):
+        """Clone instance with all its properties."""
+        return self.__class__(newname, **self._properties)
 
     def merge(self, other):
         """
