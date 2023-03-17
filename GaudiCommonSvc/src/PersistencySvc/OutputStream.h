adc807ee701e266fef9c47869a7ef3bc4bc9d242
Chris Burr <christopher.burr@cern.ch>
2023-03-17 15:17:52 +0100

    Open files in OutputStream start to ensure they're always created
    Co-authored-by: Nicole Skidmore <nicola.skidmore@cern.ch>
    Co-authored-by: Marco Clemencic <marco.clemencic@cern.ch>

--- GaudiCommonSvc/src/PersistencySvc/OutputStream.h
+++ GaudiCommonSvc/src/PersistencySvc/OutputStream.h
@@ -1,5 +1,5 @@
 /***********************************************************************************\
-* (c) Copyright 1998-2019 CERN for the benefit of the LHCb and ATLAS collaborations *
+* (c) Copyright 1998-2023 CERN for the benefit of the LHCb and ATLAS collaborations *
 *                                                                                   *
 * This software is distributed under the terms of the Apache version 2 licence,     *
 * copied verbatim in the file "LICENSE".                                            *
@@ -139,6 +139,8 @@ protected:
   virtual bool hasInput() const;
 
 public:
+  /// Start OutputStream
+  StatusCode start() override;
   /// Initialize OutputStream
   StatusCode initialize() override;
   /// Terminate OutputStream
