582139cf4ae626ebd29d5370617ef2fce71c392e
Marco Clemencic <marco.clemencic@cern.ch>
2023-06-08 14:45:54 +0200

    Fix compatibility with fmtlib 8

--- GaudiKernel/include/Gaudi/PropertyFmt.h
+++ GaudiKernel/include/Gaudi/PropertyFmt.h
@@ -69,7 +69,11 @@ struct fmt::formatter<Gaudi::Property<T, V, H>> : formatter<T> {
     }
     return formatter<T>::parse( ctx );
   }
-  auto format( const Gaudi::Property<T, V, H>& p, format_context& ctx ) const {
+  auto format( const Gaudi::Property<T, V, H>& p, format_context& ctx )
+#if FMT_VERSION >= 90000
+      const
+#endif
+  {
     if ( debug ) {
       if constexpr ( std::is_same_v<T, std::string> ) {
         std::stringstream s;
