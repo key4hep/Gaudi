fe681b4a7c48de9d9e161e760bf3ac7e6d7ce18b
Frank Winklmeier <frank.winklmeier@cern.ch>
2023-11-03 18:17:32 +0100

    GaudiExamples: remove nose depedency

--- GaudiExamples/tests/scripts/test_compat_aliases.py
+++ GaudiExamples/tests/scripts/test_compat_aliases.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #####################################################################################
-# (c) Copyright 1998-2019 CERN for the benefit of the LHCb and ATLAS collaborations #
+# (c) Copyright 1998-2023 CERN for the benefit of the LHCb and ATLAS collaborations #
 #                                                                                   #
 # This software is distributed under the terms of the Apache version 2 licence,     #
 # copied verbatim in the file "LICENSE".                                            #
@@ -52,9 +52,4 @@ def test_aliases():
 
 
 if __name__ == "__main__":
-    import sys
-
-    import nose
-
-    sys.stderr = sys.stdout  # redirect nose output to stdout
-    nose.main(defaultTest=sys.argv[0])
+    test_aliases()
