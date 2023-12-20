a5e491cc19b051e48a485b49958ea5fe71c31498
Marco Clemencic <marco.clemencic@cern.ch>
2023-12-20 14:53:28 +0100

    Backward compatibility for users  of GaudiKernel/ParticleProperty.h

---
+++ GaudiPartProp/include/GaudiKernel/ParticleProperty.h
@@ -0,0 +1,17 @@
+/***********************************************************************************\
+* (c) Copyright 2023 CERN for the benefit of the LHCb and ATLAS collaborations      *
+*                                                                                   *
+* This software is distributed under the terms of the Apache version 2 licence,     *
+* copied verbatim in the file "LICENSE".                                            *
+*                                                                                   *
+* In applying this licence, CERN does not waive the privileges and immunities       *
+* granted to it by virtue of its status as an Intergovernmental Organization        *
+* or submit itself to any jurisdiction.                                             *
+\***********************************************************************************/
+#pragma once
+
+// Backward compatibility layer for users of legacy GaudiKernel/ParticleProperty.h
+
+#include <Gaudi/ParticleProperty.h>
+
+using Gaudi::ParticleProperty;
