2ee5d0b1878c48a192c0bf9779fe48e5afd9aadb
Frank Winklmeier <frank.winklmeier@cern.ch>
2024-02-07 11:08:14 +0100

    GaudiPython: sort `set` properties before storing in catalogue
    Store `set` properties as "sorted" in the job options catalogue to make
    its content reproducible. This fix is only needed for the legacy
    Configurables as GaudiConfig2 already handles this correctly.

--- GaudiPython/python/GaudiPython/Bindings.py
+++ GaudiPython/python/GaudiPython/Bindings.py
@@ -1,5 +1,5 @@
 #####################################################################################
-# (c) Copyright 1998-2023 CERN for the benefit of the LHCb and ATLAS collaborations #
+# (c) Copyright 1998-2024 CERN for the benefit of the LHCb and ATLAS collaborations #
 #                                                                                   #
 # This software is distributed under the terms of the Apache version 2 licence,     #
 # copied verbatim in the file "LICENSE".                                            #
@@ -311,6 +311,9 @@ class iProperty(object):
             value = str(value.toStringProperty())
         elif hasattr(value, "toString"):
             value = str(value.toString())
+        elif isinstance(value, set) and value:
+            # We want a reproducible (sorted) representation in the catalogue
+            value = "{" + repr(sorted(value))[1:-1] + "}"
         else:
             value = str(value)
 
