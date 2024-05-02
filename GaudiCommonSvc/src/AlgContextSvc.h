b888947ced81cabeff9cade28d2b4aac7afe6edf
Marco Clemencic <marco.clemencic@cern.ch>
2024-05-02 12:31:08 +0200

    Prevent segfaults in AlgContextSvc::algorithms()
    Make usure a valid instance of IAlgContextSvc::Algorithms is returned even it it was never
    created for the current thread

--- GaudiCommonSvc/src/AlgContextSvc.h
+++ GaudiCommonSvc/src/AlgContextSvc.h
@@ -1,5 +1,5 @@
 /***********************************************************************************\
-* (c) Copyright 1998-2019 CERN for the benefit of the LHCb and ATLAS collaborations *
+* (c) Copyright 1998-2024 CERN for the benefit of the LHCb and ATLAS collaborations *
 *                                                                                   *
 * This software is distributed under the terms of the Apache version 2 licence,     *
 * copied verbatim in the file "LICENSE".                                            *
@@ -49,7 +49,13 @@ public:
   /// accessor to current algorithm: @see IAlgContextSvc
   IAlgorithm* currentAlg() const override;
   /// get the stack of executed algorithms @see IAlgContextSvc
-  const IAlgContextSvc::Algorithms& algorithms() const override { return *m_algorithms; }
+  const IAlgContextSvc::Algorithms& algorithms() const override {
+    if ( !m_algorithms.get() ) {
+      static IAlgContextSvc::Algorithms empty;
+      return empty;
+    }
+    return *m_algorithms;
+  }
 
 public:
   /// handle incident @see IIncidentListener
