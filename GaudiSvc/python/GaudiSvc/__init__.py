ba9ca94f5ae3e1629605b3713bf6dae4f2f5a0e1
Marco Clemencic <marco.clemencic@cern.ch>
2024-07-12 15:38:32 +0200

    Adapt GaudiSvc.getMetaData to ROOT 6.32

--- GaudiSvc/python/GaudiSvc/__init__.py
+++ GaudiSvc/python/GaudiSvc/__init__.py
@@ -1,5 +1,5 @@
 #####################################################################################
-# (c) Copyright 1998-2019 CERN for the benefit of the LHCb and ATLAS collaborations #
+# (c) Copyright 1998-2024 CERN for the benefit of the LHCb and ATLAS collaborations #
 #                                                                                   #
 # This software is distributed under the terms of the Apache version 2 licence,     #
 # copied verbatim in the file "LICENSE".                                            #
@@ -12,12 +12,20 @@ def getMetaData(path):
     """
     Extract job metadata recorded by Gaudi::MetaDataSvc from a file.
     """
+    import re
+
+    import ROOT
     from ROOT import TFile
+
+    version = tuple(int(i, 10) for i in re.split(r"\D", ROOT.__version__))
 
     f = TFile.Open(path)
     if not f:
         raise RuntimeError("cannot open {0}".format(path))
     try:
-        return dict(f.info)
+        if version < (6, 32):
+            return dict(f.info)
+        else:
+            return {k.decode("utf-8"): v.decode("utf-8") for k, v in f["info"]}
     except AttributeError:
         return {}
