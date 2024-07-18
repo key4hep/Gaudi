f5dd99d83741a43839ae9df160edd550cabb0476
Frank Winklmeier <frank.winklmeier@cern.ch>
2024-07-18 15:46:12 +0200

    MessageSvc: suppress cppcheck warning

--- GaudiCoreSvc/src/MessageSvc/MessageSvc.cpp
+++ GaudiCoreSvc/src/MessageSvc/MessageSvc.cpp
@@ -1,5 +1,5 @@
 /***********************************************************************************\
-* (c) Copyright 1998-2019 CERN for the benefit of the LHCb and ATLAS collaborations *
+* (c) Copyright 1998-2024 CERN for the benefit of the LHCb and ATLAS collaborations *
 *                                                                                   *
 * This software is distributed under the terms of the Apache version 2 licence,     *
 * copied verbatim in the file "LICENSE".                                            *
@@ -390,7 +390,7 @@ void MessageSvc::i_reportMessage( const Message& msg, int outputLevel ) {
     }
   }
 
-  if ( cmsg != &msg ) { delete cmsg; }
+  if ( cmsg != &msg ) { delete cmsg; } // cppcheck-suppress autovarInvalidDeallocation; false positive
 }
 
 // #############################################################################
