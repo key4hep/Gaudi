33bfa7ba5e25b5095cf506393d09ca74a4f6b4f6
Marco Clemencic <marco.clemencic@cern.ch>
2024-09-05 17:19:33 +0200

    Use throw_format_error instead of deprecated error_handler

--- GaudiKernel/include/Gaudi/PropertyFmt.h
+++ GaudiKernel/include/Gaudi/PropertyFmt.h
@@ -60,11 +60,15 @@ template <typename T, typename V, typename H>
 struct fmt::formatter<Gaudi::Property<T, V, H>> : formatter<T> {
   bool           debug = false;
   constexpr auto parse( format_parse_context& ctx ) -> format_parse_context::iterator {
+#if FMT_VERSION < 100100
+    // throw_format_error is part of the public API of fmtlib as of 10.1.0
+    using detail::throw_format_error;
+#endif
     auto it = ctx.begin(), end = ctx.end();
     if ( it != end && *it == '?' ) {
       debug = true;
       ++it;
-      if ( it != end && *it != '}' ) detail::error_handler().on_error( "invalid format" );
+      if ( it != end && *it != '}' ) throw_format_error( "invalid format" );
       return it;
     }
     return formatter<T>::parse( ctx );
