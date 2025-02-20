5dcbbf9d98d716e55d90d555b0dba014dcb34d02
Marco Clemencic <marco.clemencic@cern.ch>
2025-02-20 13:52:49 +0100

    Update version and change log for v39r3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,9 +22,9 @@ copyright = "1998-2023, CERN for the benefit of the LHCb and ATLAS collaboration
 author = "The Gaudi Developers"
 
 # The short X.Y version
-version = "39.2"
+version = "39.3"
 # The full version, including alpha/beta/rc tags
-release = "v39r2"
+release = "v39r3"
 
 # -- General configuration ---------------------------------------------------
 
