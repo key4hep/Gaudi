b355b4a5d255dd1d424b32e33204c248f4b17349
Ruggero Turra <ruggero.turra@cern.ch>
2025-06-12 17:40:55 +0200

    fix copyright

--- GaudiMP/scripts/compareRootHistos.py
+++ GaudiMP/scripts/compareRootHistos.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 #####################################################################################
-# (c) Copyright 1998-2023 CERN for the benefit of the LHCb and ATLAS collaborations #
+# (c) Copyright 1998-2025 CERN for the benefit of the LHCb and ATLAS collaborations #
 #                                                                                   #
 # This software is distributed under the terms of the Apache version 2 licence,     #
 # copied verbatim in the file "LICENSE".                                            #
