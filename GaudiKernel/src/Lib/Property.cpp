e2a9ba220d563d16f3866cc7cc244956a0304c6d
Chris Burr <christopher.burr@cern.ch>
2026-01-13 00:00:00 +0000

    fix: RTTI comparison for string type in WeakPropertyRef
    On macOS with shared libraries, comparing type_info pointers can fail
    because the type_info for std::string may have different addresses in
    different compilation units. Use operator== on type_info objects instead
    of comparing pointers.

--- GaudiKernel/src/Lib/Property.cpp
+++ GaudiKernel/src/Lib/Property.cpp
@@ -1,5 +1,5 @@
 /***********************************************************************************\
-* (c) Copyright 1998-2025 CERN for the benefit of the LHCb and ATLAS collaborations *
+* (c) Copyright 1998-2026 CERN for the benefit of the LHCb and ATLAS collaborations *
 *                                                                                   *
 * This software is distributed under the terms of the Apache version 2 licence,     *
 * copied verbatim in the file "LICENSE".                                            *
@@ -461,7 +461,7 @@ StatusCode Gaudi::Utils::setProperty( IInterface* component, const std::string& 
 
 Gaudi::Details::WeakPropertyRef::operator std::string() const {
   using Gaudi::Utils::toString;
-  return m_property ? ( ( m_property->type_info() == &typeid( std::string ) ) ? toString( m_property->toString() )
+  return m_property ? ( ( *m_property->type_info() == typeid( std::string ) ) ? toString( m_property->toString() )
                                                                               : m_property->toString() )
                     : m_value;
 }
