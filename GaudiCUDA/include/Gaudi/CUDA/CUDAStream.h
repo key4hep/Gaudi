6d2e1e4f9ff017aeab8d450864325c278fb1dbc9
Beojan Stanislaus <beojan.stanislaus@cern.ch>
2026-01-21 00:12:46 -0800

    Make CUDA dependency non-public

--- GaudiCUDA/include/Gaudi/CUDA/CUDAStream.h
+++ GaudiCUDA/include/Gaudi/CUDA/CUDAStream.h
@@ -1,5 +1,5 @@
 /***********************************************************************************\
-* (c) Copyright 2023-2024 CERN for the benefit of the LHCb and ATLAS collaborations *
+* (c) Copyright 2023-2026 CERN for the benefit of the LHCb and ATLAS collaborations *
 *                                                                                   *
 * This software is distributed under the terms of the Apache version 2 licence,     *
 * copied verbatim in the file "LICENSE".                                            *
@@ -12,10 +12,9 @@
 // Gaudi
 #include <Gaudi/AsynchronousAlgorithm.h>
 
-// CUDA
-#ifndef __CUDACC__
-#  include <cuda_runtime.h>
-#endif
+// Forward declaration hack so we can have a cudaStream_t member here
+struct CUstream_st;
+typedef struct CUstream_st* cudaStream_t;
 
 namespace Gaudi::CUDA {
   class Stream {
@@ -30,18 +29,18 @@ namespace Gaudi::CUDA {
     ~Stream();
 
     /// Access the internal cudaStream_t
-    inline operator cudaStream_t() { return m_stream; }
+    operator cudaStream_t();
 
     /// Access the parent algorithm
-    inline const Gaudi::AsynchronousAlgorithm* parent() { return m_parent; }
+    const Gaudi::AsynchronousAlgorithm* parent();
 
     /// Yield fiber until stream is done
     StatusCode await();
 
     /// INTERNAL: Register a dependency
-    void registerDependency() { ++m_dependents; }
+    void registerDependency();
 
     /// INTERNAL: Remove a dependency
-    void removeDependency() { --m_dependents; }
+    void removeDependency();
   };
 } // namespace Gaudi::CUDA
