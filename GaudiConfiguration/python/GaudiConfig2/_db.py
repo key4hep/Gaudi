cba8de2df78895b927fc26122fc259924c46c917
Marco Clemencic <marco.clemencic@cern.ch>
2026-01-31 23:35:46 +0100

    Search for confdb2 DBs in GAUDI_PLUGIN_PATH first

--- GaudiConfiguration/python/GaudiConfig2/_db.py
+++ GaudiConfiguration/python/GaudiConfig2/_db.py
@@ -1,5 +1,5 @@
 #####################################################################################
-# (c) Copyright 1998-2025 CERN for the benefit of the LHCb and ATLAS collaborations #
+# (c) Copyright 1998-2026 CERN for the benefit of the LHCb and ATLAS collaborations #
 #                                                                                   #
 # This software is distributed under the terms of the Apache version 2 licence,     #
 # copied verbatim in the file "LICENSE".                                            #
@@ -21,7 +21,7 @@ class ConfDB2(object):
 
         self._dbs = {}
         pathvars = (
-            ["DYLD_LIBRARY_PATH"]
+            ["GAUDI_PLUGIN_PATH", "DYLD_LIBRARY_PATH"]
             if sys.platform == "darwin"
             else ["GAUDI_PLUGIN_PATH", "LD_LIBRARY_PATH"]
         )
