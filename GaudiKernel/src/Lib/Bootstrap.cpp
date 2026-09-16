d707641280aa00c41363492d3deef9c81063d79c
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2026-09-16 14:06:22 +0200

    Replace deprecated ROOT RVersion header

--- GaudiKernel/src/Lib/Bootstrap.cpp
+++ GaudiKernel/src/Lib/Bootstrap.cpp
@@ -1,5 +1,5 @@
 /***********************************************************************************\
-* (c) Copyright 1998-2025 CERN for the benefit of the LHCb and ATLAS collaborations *
+* (c) Copyright 1998-2026 CERN for the benefit of the LHCb and ATLAS collaborations *
 *                                                                                   *
 * This software is distributed under the terms of the Apache version 2 licence,     *
 * copied verbatim in the file "LICENSE".                                            *
@@ -26,7 +26,7 @@
 
 #include <GaudiKernel/IEventProcessor.h>
 
-#include <RVersion.h>
+#include <ROOT/RVersion.hxx>
 
 #include <Python.h>
 
